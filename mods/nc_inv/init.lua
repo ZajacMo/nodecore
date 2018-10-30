7fb3f1be0ff711c14ec92e77ab2683098c1ff391
Aaron Suen <warr1024@gmail.com>
2018-10-29 23:43:34 -0400

    More hud stuff, limit inventory.

---
+++ mods/nc_inv/init.lua
@@ -0,0 +1,7 @@
+minetest.register_on_joinplayer(function(player)
+		player:get_inventory():set_size("main", 8)
+		player:set_inventory_formspec("size[8,1]list[current_player;main;0,0;8,1;]")
+		player:hud_set_hotbar_itemcount(8)
+		player:hud_set_hotbar_image("nc_hud_bg.png")
+		player:hud_set_hotbar_selected_image("nc_hud_sel.png")
+	end)
