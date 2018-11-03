90574771c68307ab9d3bf20e29a894ef16a6958a
Aaron Suen <warr1024@gmail.com>
2018-11-03 19:18:28 -0400

    Simplify trees, since logs currently cannot be moved.
    Presumably this will change in the future, but it's also not
    a given that we should have a falling or rotating version of the
    tree (or that it should also split to planks).

--- mods/nc_woodwork/plank.lua
+++ mods/nc_woodwork/plank.lua
@@ -1,6 +1,6 @@
 -- LUALOCALS < ---------------------------------------------------------
 local ItemStack, ipairs, minetest, nodecore
-    = ItemStack, ipairs, minetest, nodecore
+= ItemStack, ipairs, minetest, nodecore
 -- LUALOCALS > ---------------------------------------------------------
 
 local modname = minetest.get_current_modname()
@@ -14,40 +14,36 @@ minetest.register_node(plank, {
 		}
 	})
 
-local upright = {[0]=true,[1]=true,[2]=true,[3]=true,[20]=true,[21]=true,[22]=true,[23]=true}
-for _, name in ipairs({"nc_tree:tree", "nc_tree:log"}) do
-	nodecore.extend_node(name, function(copy, orig)
-			local op = orig.on_pummel or function() end
-			copy.on_pummel = function(pos, node, stats, ...)
-				if not upright[node.param2]
-				or (stats.pointed.above.y - stats.pointed.under.y) ~= 1
-				or stats.duration < 5 then
-					return op(pos, node, stats, ...)
-				end
-				local wielded = stats.puncher:get_wielded_item()
-				local caps = wielded:get_tool_capabilities()
-				local choppy = caps and caps.groupcaps and caps.groupcaps.choppy
-				if not choppy then
-					return op(pos, node, stats, ...)
-				end
-				minetest.remove_node(pos)
-				local defer = 0
-				for _, v in ipairs({
-						{x = pos.x + 1, y = pos.y, z = pos.z},
-						{x = pos.x - 1, y = pos.y, z = pos.z},
-						{x = pos.x, y = pos.y, z = pos.z + 1},
-						{x = pos.x, y = pos.y, z = pos.z - 1}
-						}) do
-					if minetest.registered_nodes[minetest.get_node(v)
-					.name].buildable_to then
-						minetest.item_drop(ItemStack(plank), nil, v)
-					else
-						defer = defer + 1
-					end
-				end
-				if defer > 0 then
-					minetest.item_drop(ItemStack(plank .. " " .. defer), nil, pos)
+nodecore.extend_node("nc_tree:tree", function(copy, orig)
+		local op = orig.on_pummel or function() end
+		copy.on_pummel = function(pos, node, stats, ...)
+			if (stats.pointed.above.y - stats.pointed.under.y) ~= 1
+			or stats.duration < 5 then
+				return op(pos, node, stats, ...)
+			end
+			local wielded = stats.puncher:get_wielded_item()
+			local caps = wielded:get_tool_capabilities()
+			local choppy = caps and caps.groupcaps and caps.groupcaps.choppy
+			if not choppy then
+				return op(pos, node, stats, ...)
+			end
+			minetest.remove_node(pos)
+			local defer = 0
+			for _, v in ipairs({
+					{x = pos.x + 1, y = pos.y, z = pos.z},
+					{x = pos.x - 1, y = pos.y, z = pos.z},
+					{x = pos.x, y = pos.y, z = pos.z + 1},
+					{x = pos.x, y = pos.y, z = pos.z - 1}
+					}) do
+				if minetest.registered_nodes[minetest.get_node(v)
+				.name].buildable_to then
+					minetest.item_drop(ItemStack(plank), nil, v)
+				else
+					defer = defer + 1
 				end
 			end
-		end)
-end
+			if defer > 0 then
+				minetest.item_drop(ItemStack(plank .. " " .. defer), nil, pos)
+			end
+		end
+	end)
