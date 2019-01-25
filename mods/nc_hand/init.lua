4bf1f7ae85abd96c9fa2b233e22e90d756397b5e
Aaron Suen <warr1024@gmail.com>
2019-01-25 01:17:08 -0500

    Remove the cheat tool again.

--- mods/nc_hand/init.lua
+++ mods/nc_hand/init.lua
@@ -14,17 +14,3 @@ minetest.register_item(":", {
 				thumpy = 1
 			})
 	})
-
-minetest.register_tool("nc_hand:cheat", {
-		inventory_image = "nc_hand.png^[invert:rgb",
-		wield_image = "nc_hand.png^[invert:rgb",
-		wield_scale = {x=1, y=1, z=2.5},
-		tool_capabilities = nodecore.toolcaps({
-				uses = 0,
-				crumbly = 100,
-				cracky = 100,
-				snappy = 100,
-				choppy = 100,
-				thumpy = 100
-			})
-	})
