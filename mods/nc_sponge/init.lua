294fb0285f041a108b70e48020169b0c0cf6c9c9
Aaron Suen <warr1024@gmail.com>
2019-02-19 00:50:59 -0500

    Some initial basic sponge wetting/drying logic.

--- mods/nc_sponge/init.lua
+++ mods/nc_sponge/init.lua
@@ -1,12 +1,14 @@
 -- LUALOCALS < ---------------------------------------------------------
-local minetest
-    = minetest
+local math, minetest, nodecore
+    = math, minetest, nodecore
+local math_random
+    = math.random
 -- LUALOCALS > ---------------------------------------------------------
 
 local modname = minetest.get_current_modname()
 
 minetest.register_node(modname .. ":sponge", {
-		description = "Sponge (Dry)",
+		description = "Sponge",
 		drawtype = "allfaces_optional",
 		tiles = {modname ..".png"},
 		paramtype = "light",
@@ -18,7 +20,7 @@ minetest.register_node(modname .. ":sponge", {
 	})
 
 minetest.register_node(modname .. ":sponge_wet", {
-		description = "Sponge (Wet)",
+		description = "Wet Sponge",
 		drawtype = "allfaces_optional",
 		tiles = {modname ..".png^(nc_terrain_water.png^[opacity:96)"},
 		paramtype = "light",
@@ -27,4 +29,43 @@ minetest.register_node(modname .. ":sponge_wet", {
 			coolant = 1,
 			falling_node = 1
 		}
+	})
+
+nodecore.register_limited_abm({
+		label = "Sponge Wettening",
+		interval = 1,
+		chance = 10,
+		limited_max = 100,
+		nodenames = {modname .. ":sponge"},
+		neighbors = {"group:water"},
+		action = function(pos)
+			return minetest.set_node(pos,
+				{name = modname .. ":sponge_wet"})
+		end
+	})
+
+nodecore.register_limited_abm({
+		label = "Sponge Drying in Sunlight",
+		interval = 1,
+		chance = 20,
+		limited_max = 100,
+		nodenames = {modname .. ":sponge_wet"},
+		action = function(pos)
+			local light = minetest.get_node_light({x = pos.x, y = pos.y + 1, z = pos.z})
+			if light >= 15 and math_random(1, 10) == 1 then
+				return minetest.set_node(pos, {name = modname .. ":sponge"})
+			end
+		end
+	})
+
+nodecore.register_limited_abm({
+		label = "Sponge Drying near Fire",
+		interval = 1,
+		chance = 20,
+		limited_max = 100,
+		nodenames = {modname .. ":sponge_wet"},
+		neighbors = {"group:igniter"},
+		action = function(pos)
+			return minetest.set_node(pos, {name = modname .. ":sponge"})
+		end
 	})
