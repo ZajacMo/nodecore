aa714cd218a85612acd64c1da4733e34beb65b51
Aaron Suen <warr1024@gmail.com>
2019-02-21 10:45:24 -0500

    Make log tracing optional, and off by default.
    This can be very annoying/distracting in singleplayer.

--- mods/nc_api/util_logtrace.lua
+++ mods/nc_api/util_logtrace.lua
@@ -7,6 +7,17 @@ local table_concat
 
 minetest.register_privilege("logtrace", "Receive server debug/trace messages.")
 
+core.register_chatcommand("logtrace", {
+		description = "Toggle debug trace messages",
+		privs = {logtrace = true},
+		func = function(name)
+			local player = minetest.get_player_by_name(name)
+			if not player then return end
+			local old = player:get_attribute("logtrace") or ""
+			return player:set_attribute("logtrace", (old == "") and "1" or "")
+		end,
+	})
+
 function nodecore.logtrace(...)
 	local t = {"#", ...}
 	for i, v in ipairs(t) do
@@ -18,7 +29,10 @@ function nodecore.logtrace(...)
 	for _, p in pairs(minetest.get_connected_players()) do
 		local n = p:get_player_name()
 		if minetest.get_player_privs(n).logtrace then
-			minetest.chat_send_player(n, msg)
+			local a = p:get_attribute("logtrace")
+			if a and a ~= "" then
+				minetest.chat_send_player(n, msg)
+			end
 		end
 	end
 end
