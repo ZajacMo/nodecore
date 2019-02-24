e2cf72282cc59753a9c54f047f49c98921614ed3
Aaron Suen <warr1024@gmail.com>
2019-02-23 20:06:14 -0500

    Disallow shelf access through top/bottom.

--- mods/nc_woodwork/shelf.lua
+++ mods/nc_woodwork/shelf.lua
@@ -35,15 +35,16 @@ minetest.register_node(modname .. ":shelf", {
 			nodecore.visinv_update_ents(pos)
 		end,
 		on_rightclick = function(pos, node, clicker, stack, pointed_thing)
+			if pointed_thing.above.y ~= pointed_thing.under.y then return end
 			if not stack or stack:is_empty() then return end
 			local def = minetest.registered_items[stack:get_name()]
 			if (not def) or (def.groups and def.groups.visinv) then
 				return minetest.item_place_node(stack, clicker, pointed_thing)
-
 			end
 			return nodecore.stack_add(pos, stack)
 		end,
-		on_punch = function(pos, node, puncher)
+		on_punch = function(pos, node, puncher, pointed_thing)
+			if pointed_thing.above.y ~= pointed_thing.under.y then return end
 			return nodecore.stack_giveto(pos, puncher)
 		end,
 		on_dig = function(pos, node, digger, ...)
