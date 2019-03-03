287b5a1014db956f0deddd0ec02f0ccf6782ca3d
Aaron Suen <warr1024@gmail.com>
2019-03-02 23:39:56 -0500

    Tweak belt position.

--- mods/nc_player_wield/init.lua
+++ mods/nc_player_wield/init.lua
@@ -109,14 +109,14 @@ minetest.register_on_joinplayer(function(player)
 		local function cslot(n, x, z)
 			return addslot(n, nil, x * 1.6,
 				(isold and -4 or 5.5) + x / 2,
-				z * 2)
+				z * 2.1)
 		end
 
 		cslot(1, 1, 1)
-		cslot(2, 0, 1)
+		cslot(2, 0, 1.2)
 		cslot(3, -1, 1)
 		cslot(4, -2, 0)
 		cslot(5, -1, -1)
-		cslot(6, 0, -1)
+		cslot(6, 0, -1.2)
 		cslot(7, 1, -1)
 	end)
