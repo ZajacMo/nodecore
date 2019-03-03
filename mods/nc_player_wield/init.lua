dfb04e9eeead5c2739d92a9d15cd36924de07100
Aaron Suen <warr1024@gmail.com>
2019-03-02 23:14:41 -0500

    Adapted wield view for MT 5.0+.
    There is some wierd issue in 5.0+, besides the different model
    coordinate system.  If I try to create and attach all ents in the
    very first tick on player join, only like 3 of them work.  Queue
    them up and apply them gradually, one per world tick, to try to
    work around this.  Need to figure out what limitation I'm hitting
    that's causing this...

--- mods/nc_player_wield/init.lua
+++ mods/nc_player_wield/init.lua
@@ -1,6 +1,6 @@
 -- LUALOCALS < ---------------------------------------------------------
-local minetest
-    = minetest
+local ipairs, minetest, string
+    = ipairs, minetest, string
 -- LUALOCALS > ---------------------------------------------------------
 
 local modname = minetest.get_current_modname()
@@ -51,26 +51,65 @@ minetest.register_entity(modname .. ":ent", {
 		end
 	})
 
+local attq = {}
+local qpos = 0
+
+minetest.register_globalstep(function()
+		if not attq then return end
+		qpos = qpos + 1
+		if qpos > #attq then
+			attq = nil
+			qpos = nil
+			return
+		end
+		local v = attq[qpos]
+		local player = minetest.get_player_by_name(v.pname)
+		if not player then return end
+		local obj = minetest.add_entity(v.pos, modname .. ":ent")
+		obj:set_attach(player, v.bone,
+			{x = v.x, y = v.y, z = v.z},
+			{x = v.rx, y = v.ry, z = v.rz})
+		local ent = obj:get_luaentity()
+		ent.pname = v.pname
+		ent.slot = v.slot
+	end)
+
 
 minetest.register_on_joinplayer(function(player)
 		local pname = player:get_player_name()
 		local pos = player:get_pos()
 
+		local myq = {}
+
+		minetest.after(1, function()
+				if not attq then
+					attq = {}
+					qpos = 0
+				end
+				for _, v in ipairs(myq) do attq[#attq + 1] = v end
+			end)
+
 		local function addslot(n, b, x, y, z, rx, ry, rz)
-			local obj = minetest.add_entity(pos, modname .. ":ent")
-			obj:set_attach(player, b,
-				{x = x, y = y, z = z},
-				{x = rx or 0, y = ry or 0, z = rz or 0})
-			local ent = obj:get_luaentity()
-			ent.pname = pname
-			ent.slot = n
+			myq[#myq + 1] = {
+				slot = n,
+				pname = pname,
+				pos = pos,
+				bone = b,
+				x = x,
+				y = y,
+				z = z,
+				rx = rx or 0,
+				ry = ry or 0,
+				rz = rz or 0}
 		end
 
 		addslot(0, "Arm_Right", -2, 7, 0, 0, 180, 90)
 
+		local isold = minetest.get_version().string:sub(1, 2) == "0."
 		local function cslot(n, x, z)
-			return addslot(n, nil, x * 1.5, -4 + x / 2, z * 2)
-			--return addslot(n, "Chest", x * 1.5, -2.5 + x / 2, z * 2)
+			return addslot(n, nil, x * 1.5,
+				(isold and -4 or 5.5) + x / 2,
+				z * 2)
 		end
 
 		cslot(1, 1, 1)
