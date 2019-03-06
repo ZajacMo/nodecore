ac322cf4fd6e2260b9567f70ace0f1fcccc8a02f
Aaron Suen <warr1024@gmail.com>
2019-03-06 00:18:52 -0500

    Revert changes from last 2 commits.
    They seemed to have no effect at all on android issue.
    Conclusion: this is probably an engine bug, especially since I
    can't get it to misbehave at all on low-DPI desktop.

--- mods/nc_player_hud/init.lua
+++ mods/nc_player_hud/init.lua
@@ -32,9 +32,9 @@ local function make_wield_bar(x, y, t)
 		text = t or "",
 		number = (x == 0 and y == 0) and 0xFFFFFF or 0,
 		direction = 0,
-		scale = {x = 320, y = 16},
-		offset = {x = 25 + x, y = -(48 + 24 - y)},
-		alignment = {x = 1, y = -0.5}
+		size = {x = 24, y = 24},
+		offset = {x = 25 + x, y = -(48 + 24 + 8 - y)},
+		alignment = {x = 1, y = 0.5}
 	}
 end
 
