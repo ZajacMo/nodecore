cbb7f823c3548d46d94be8d066894886be2d3a1d
Aaron Suen <warr1024@gmail.com>
2019-03-07 10:08:22 -0500

    Fix node dig prediction, allow limited zoom.

--- mods/nc_player_setup/setup.lua
+++ mods/nc_player_setup/setup.lua
@@ -5,5 +5,15 @@ local minetest
 
 minetest.register_on_joinplayer(function(player)
 		player:get_inventory():set_size("main", 8)
-		player:set_properties({stepheight = 1.4})
+
+		player:set_properties({
+				makes_footstep_sound = true,
+				
+				-- No-jump stair climbing on all platforms, with
+				-- a little extra for climbing up from stack nodes
+				stepheight = 1.4,
+
+				-- Allow slight zoom for screenshots
+				zoom_fov = 60
+			})
 	end)
