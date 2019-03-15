38833a90a711ff2f2272ea691d7c722745413085
Aaron Suen <warr1024@gmail.com>
2019-03-14 22:04:44 -0400

    Limit punch sound interval.

--- mods/nc_api/item_punch_sounds.lua
+++ mods/nc_api/item_punch_sounds.lua
@@ -13,12 +13,19 @@ function nodecore.node_sound(pos, kind)
 	return minetest.sound_play(t.name, t)
 end
 
+local lasthit = {}
+
 minetest.register_on_punchnode(function(pos, node, puncher, pointed)
 		local def = minetest.registered_items[node.name] or {}
 		if not def.groups or not def.sounds then return end
 		if not puncher then return end
+		local pname = puncher:get_player_name()
+		local now = minetest.get_us_time() / 1000000
+		local last = lasthit[pname] or 0
+		if now - last < 0.25 then return end
 		local wield = puncher:get_wielded_item()
 		if not nodecore.toolspeed(wield, def.groups) then
 			nodecore.node_sound(pos, "dig")
+			lasthit[pname] = now
 		end
 	end)
