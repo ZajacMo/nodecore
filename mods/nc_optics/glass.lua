1b395e35896995959546b6d11bd2b6e133ebbc72
Aaron Suen <warr1024@gmail.com>
2019-03-14 21:42:11 -0400

    Lava-like ambiance for molten glass too.

--- mods/nc_optics/glass.lua
+++ mods/nc_optics/glass.lua
@@ -67,3 +67,23 @@ minetest.register_node(modname .. ":glass_hot_flowing",
 			drawtype = "flowingliquid",
 			paramtype2 = "flowingliquid"
 			}, moltdef))
+
+
+nodecore.register_ambiance({
+		label = "Lava Source Ambiance",
+		nodenames = {modname .. ":glass_hot_source"},
+		neigbors = {"air"},
+		interval = 1,
+		chance = 10,
+		sound_name = "nc_terrain_bubbly",
+		sound_gain = 0.2
+	})
+nodecore.register_ambiance({
+		label = "Lava Flow Ambiance",
+		nodenames = {modname .. ":glass_hot_flowing"},
+		neigbors = {"air"},
+		interval = 1,
+		chance = 10,
+		sound_name = "nc_terrain_bubbly",
+		sound_gain = 0.2
+	})
