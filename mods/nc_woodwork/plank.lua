5c05a3c6dde82ed576171d285a3eedd37a960086
Aaron Suen <warr1024@gmail.com>
2019-03-16 16:54:35 -0400

    Avoid spewing so many individual items.

--- mods/nc_woodwork/plank.lua
+++ mods/nc_woodwork/plank.lua
@@ -39,6 +39,6 @@ nodecore.register_craft({
 			{match = plank, replace = "air"}
 		},
 		items = {
-			{name = "nc_tree:stick", count = 8, scatter = 5}
+			{name = "nc_tree:stick 2", count = 4, scatter = 5}
 		}
 	})
