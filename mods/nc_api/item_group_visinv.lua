45160f72701954fa404c0a4886dd0436eafbebb5
Aaron Suen <warr1024@gmail.com>
2019-03-24 18:42:32 -0400

    Oops, fix regression in stack dig node sounds.

--- mods/nc_api/item_group_visinv.lua
+++ mods/nc_api/item_group_visinv.lua
@@ -142,6 +142,7 @@ nodecore.register_limited_abm({
 local digpos
 local old_node_dig = minetest.node_dig
 minetest.node_dig = function(pos, ...)
+	nodecore.stack_sounds(pos, "dug")
 	local function helper(...)
 		digpos = nil
 		return ...
