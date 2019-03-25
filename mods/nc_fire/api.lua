cfb5e729c8266a711fdef9cbb3816770094d5bd9
Aaron Suen <warr1024@gmail.com>
2019-03-24 21:08:39 -0400

    Sounds for igniting/extinguishing fires.

--- mods/nc_fire/api.lua
+++ mods/nc_fire/api.lua
@@ -20,8 +20,13 @@ function nodecore.ignite(pos, node)
 			nodecore.item_eject(p or pos, stack, 1)
 		end
 	end
-	minetest.set_node(pos, {name = modname .. ((fuel > 0)
-				and (":ember" .. fuel) or ":fire")})
+	if fuel > 0 then
+		minetest.set_node(pos, {name = modname .. ":ember" .. fuel})
+		minetest.sound_play("nc_fire_ignite", {gain = 1, pos = pos})
+		minetest.sound_play("nc_fire_flamy", {gain = 3, pos = pos})
+	else
+		minetest.set_node(pos, {name = modname .. ":fire"})
+	end
 	minetest.after(0, function() minetest.check_for_falling(pos) end)
 end
 
@@ -29,7 +34,11 @@ function nodecore.snuff(pos, node, groups)
 	local ember = nodecore.node_group("ember", pos, node)
 	if not ember then return end
 	ember = ember - 1
-	minetest.set_node(pos, {name = modname .. ((ember > 0)
-				and (":ember" .. ember) or ":ash")})
+	if ember > 0 then
+		minetest.set_node(pos, {name = modname .. ":ember" .. ember})
+	else
+		minetest.set_node(pos, {name = modname .. ":ash"})
+		minetest.sound_play("nc_fire_snuff", {gain = 1, pos = pos})
+	end
 	minetest.after(0, function() minetest.check_for_falling(pos) end)
 end
