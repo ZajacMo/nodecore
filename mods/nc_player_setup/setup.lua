c83e3288d79e2e57e3ad8c038daf30285156f720
Aaron Suen <warr1024@gmail.com>
2019-03-29 23:53:27 -0400

    stepheight obsoleted by auto-jumping...?
    Well, we'll have to see if that works out...

--- mods/nc_player_setup/setup.lua
+++ mods/nc_player_setup/setup.lua
@@ -11,10 +11,6 @@ minetest.register_on_joinplayer(function(player)
 		player:set_properties({
 				makes_footstep_sound = true,
 
-				-- No-jump stair climbing on all platforms, with
-				-- a little extra for climbing up from stack nodes
-				stepheight = 1.4,
-
 				-- Allow slight zoom for screenshots
 				zoom_fov = 60
 			})
