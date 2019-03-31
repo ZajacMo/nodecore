2d16d89812f427e33f103a6f1a0935074fa7e0af
Aaron Suen <warr1024@gmail.com>
2019-03-31 09:05:33 -0400

    Experimental cdbrelease hook.

---
+++ .cdbrelease.lua
@@ -0,0 +1,11 @@
+-- LUALOCALS < ---------------------------------------------------------
+-- SKIP: package
+local dofile
+    = dofile
+-- LUALOCALS > ---------------------------------------------------------
+
+return {
+	user = "Warr1024",
+	package = "nodecore",
+	version = dofile("./mods/nc_api/version.lua")
+}
