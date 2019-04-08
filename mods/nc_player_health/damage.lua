40c239a798ca4165648cf50de886457564718408
Aaron Suen <warr1024@gmail.com>
2019-04-08 08:26:44 -0400

    Defer social damage sounds until after all modifiers.
    This allows external mods that alter damage to prevent damage
    sounds from playing for other nearby players if they modify the
    damage amount.

--- mods/nc_player_health/damage.lua
+++ mods/nc_player_health/damage.lua
@@ -1,18 +1,22 @@
 -- LUALOCALS < ---------------------------------------------------------
 local minetest, nodecore
-    = minetest, nodecore
+= minetest, nodecore
 -- LUALOCALS > ---------------------------------------------------------
 
 minetest.register_on_player_hpchange(function(player, hp)
+		local orig = player:get_hp()
 		if hp < 0 then
-			nodecore.sound_play_except("player_damage", {
-					pos = player:get_pos(),
-					gain = 0.5
-					}, player)
+			minetest.after(0, function()
+					local now = player:get_hp()
+					if now >= orig then return end
+					nodecore.sound_play_except("player_damage", {
+							pos = player:get_pos(),
+							gain = 0.5
+							}, player)
+				end)
 		end
-		local t = player:get_hp()
-		if hp + t <= 0 then
-			hp = 1 - t
+		if hp + orig <= 0 then
+			hp = 1 - orig
 			player:get_meta():set_float("dhp", -1)
 		end
 		return hp
