a0670867c1f899ff4cd37ebc23d708caad2538aa
Aaron Suen <warr1024@gmail.com>
2019-04-14 00:09:03 -0400

    Experimental item switching sounds.

--- mods/nc_player_hand/init.lua
+++ mods/nc_player_hand/init.lua
@@ -1,6 +1,6 @@
 -- LUALOCALS < ---------------------------------------------------------
-local minetest, nodecore, type
-    = minetest, nodecore, type
+local minetest, nodecore, pairs, type
+    = minetest, nodecore, pairs, type
 -- LUALOCALS > ---------------------------------------------------------
 
 minetest.register_item(":", {
@@ -14,3 +14,36 @@ minetest.register_item(":", {
 				thumpy = 1
 			})
 	})
+
+local function wieldsound(player, idx, gain, delay)
+	local n = player:get_inventory():get_stack("main", idx):get_name()
+	local def = minetest.registered_items[n]
+	if def and def.sounds then
+		local t = {}
+		for k, v in pairs(def.sounds.dig) do t[k] = v end
+		t.pos = player:get_pos()
+		t.gain = gain
+		if delay then
+			return minetest.after(delay, function()
+					return minetest.sound_play(t.name, t)
+				end)
+		else
+			return minetest.sound_play(t.name, t)
+		end
+	end
+end
+
+local wields = {}
+minetest.register_globalstep(function(dtime)
+		for _, player in pairs(minetest.get_connected_players()) do
+			local pname = player:get_player_name()
+
+			local idx = player:get_wield_index()
+			local old = wields[pname]
+			if idx ~= old then
+				if old then wieldsound(player, old, 0.07) end
+				wieldsound(player, idx, 0.15, 0.1)
+				wields[pname] = idx
+			end
+		end
+	end)
