7431453ad79e1b0567566cc87a95c11dc19ef839
Aaron Suen <warr1024@gmail.com>
2019-08-13 22:02:02 -0400

    Door rotation logic.
    Shockingly it WORKS.
    
    Does not yet interact reasonably with a lot of terrain (should
    arguably push liquids, falling nodes, items, etc.) or entities
    a(i.e. should arguably push players).

--- mods/nc_doors/init.lua
+++ mods/nc_doors/init.lua
@@ -5,6 +5,72 @@ local minetest, nodecore, pairs, vector
 
 local modname = minetest.get_current_modname()
 
+local function hingeaxis(pos, node)
+	local fd = node and node.param2 or 0
+	fd = nodecore.facedirs[fd]
+	fd = vector.multiply(vector.add(fd.f, fd.r), 0.5)
+	return {
+		x = fd.x == 0 and 0 or pos.x + fd.x,
+		y = fd.y == 0 and 0 or pos.y + fd.y,
+		z = fd.z == 0 and 0 or pos.z + fd.z
+	}
+end
+
+local function doorop(pos, node, _, _, pointed)
+	if (not pointed.above) or (not pointed.under) then return end
+	
+	local force = vector.subtract(pointed.under, pointed.above)
+	local fd = nodecore.facedirs[node.param2 or 0]
+	local rotdir
+	if vector.equals(force, fd.k) or vector.equals(force, fd.r) then
+		rotdir = "r"
+	elseif vector.equals(force, fd.l) or vector.equals(force, fd.f) then
+		rotdir = "f"
+	else return end
+
+	local found = {}
+	local hinge = hingeaxis(pos, node)
+	if nodecore.scan_flood(pos, 128, function(p, d)
+		local n = minetest.get_node_or_nil(p)
+		if not n then return true end
+		if (not nodecore.match(n, {groups = {door = true}}))
+		or (not vector.equals(hingeaxis(p, n), hinge)) then return false end
+		found[minetest.pos_to_string(p)] = {pos = p, node = n}
+	end) then return end
+
+	for k, v in pairs(found) do
+		local fd = nodecore.facedirs[v.node.param2 or 0]
+		local to = vector.add(v.pos, fd[rotdir])
+		if (not found[minetest.pos_to_string(to)])
+		and (not nodecore.buildable_to(to))
+		then return end
+		v.to = to
+		v.fd = fd
+	end
+
+	local toset = {}
+	for k, v in pairs(found) do
+		toset[k] = {pos = v.pos, name = "air"}
+	end
+	for k, v in pairs(found) do
+		for i, fd in pairs(nodecore.facedirs) do
+			if vector.equals(fd.t, v.fd.t)
+			and vector.equals(fd.r, rotdir == "r" and v.fd.f or v.fd.k) then
+				toset[minetest.pos_to_string(v.to)] = {
+					pos = v.to,
+					name = v.node.name,
+					param2 = i
+				}
+				break
+			end
+		end
+	end
+
+	for k, v in pairs(toset) do
+		minetest.set_node(v.pos, v)
+	end
+end
+
 local tilemods = {
 	{idx = 1, part = "end", tran = "R180"},
 	{idx = 2, part = "end", tran = "FX"},
@@ -42,11 +108,14 @@ function nodecore.register_door(basemod, basenode, ext)
 		.. "_mask.png^[transform" .. v.tran .. "))"
 	end
 
+	local groups = nodecore.underride({door = 1}, basedef.groups)
 	local doordef = nodecore.underride({
 		name = modname .. ":door_" .. basenode,
 		tiles = tiles,
 		drop = "nc_lode:rod_tempered",
-		drop_in_place = paneldef.name
+		drop_in_place = paneldef.name,
+		on_rightclick = doorop,
+		groups = groups
 	}, paneldef)
 
 	minetest.register_node(doordef.name, doordef)
@@ -55,6 +124,7 @@ function nodecore.register_door(basemod, basenode, ext)
 		label = "drill door " .. basenode:lower(),
 		action = "pummel",
 		toolgroups = {thumpy = 3},
+		normal = {y = 1},
 		nodes = {
 			{
 				match = "nc_lode:rod_tempered",
@@ -71,6 +141,7 @@ function nodecore.register_door(basemod, basenode, ext)
 		label = "door pin " .. basenode:lower(),
 		action = "pummel",
 		toolgroups = {thumpy = 1},
+		normal = {y = 1},
 		nodes = {
 			{
 				match = "nc_lode:rod_tempered",
