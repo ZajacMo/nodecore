bba8edfc39291bb390578b68a035a035c629d1ed
Aaron Suen <warr1024@gmail.com>
2019-08-23 21:42:48 -0400

    Lower sponge water TTL.

--- mods/nc_sponge/squeeze.lua
+++ mods/nc_sponge/squeeze.lua
@@ -73,6 +73,6 @@ nodecore.register_craft({
 		minetest.set_node(p, {name = "nc_terrain:water_source"})
 		nodecore.node_sound(p, "place")
 		local obj = minetest.add_entity(p, modname .. ":waterguard",
-			minetest.serialize({ttl = 5}))
+			minetest.serialize({ttl = 3}))
 	end
 })
