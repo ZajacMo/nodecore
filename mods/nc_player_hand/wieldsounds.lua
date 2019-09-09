9e20b3ce66895532b119660b57d564b560e47488
Aaron Suen <warr1024@gmail.com>
2019-09-09 08:11:06 -0400

    Sneaking actually makes player make less sound.

--- mods/nc_player_hand/wieldsounds.lua
+++ mods/nc_player_hand/wieldsounds.lua
@@ -10,7 +10,8 @@ local function wieldsound(player, idx, gain)
 		local t = {}
 		for k, v in pairs(def.sounds.dig) do t[k] = v end
 		t.pos = player:get_pos()
-		t.gain = gain
+		t.gain = gain or 1
+		if player:get_player_control().sneak then t.gain = t.gain / 4 end
 		return function() minetest.sound_play(t.name, t) end
 	end
 	return function() end
--- mods/nc_player_setup/setup.lua
+++ mods/nc_player_setup/setup.lua
@@ -1,7 +1,9 @@
 -- LUALOCALS < ---------------------------------------------------------
-local minetest
-    = minetest
+local minetest, pairs
+    = minetest, pairs
 -- LUALOCALS > ---------------------------------------------------------
+
+local footsteps = {}
 
 minetest.register_on_joinplayer(function(player)
 		local inv = player:get_inventory()
@@ -18,6 +20,7 @@ minetest.register_on_joinplayer(function(player)
 				-- Allow slight zoom for screenshots
 				zoom_fov = 60
 			})
+		footsteps[player:get_player_name()] = true
 	end)
 
 minetest.register_allow_player_inventory_action(function(_, action)
@@ -25,3 +28,16 @@ minetest.register_allow_player_inventory_action(function(_, action)
 	end)
 
 minetest.unregister_chatcommand("kill")
+
+minetest.register_globalstep(function()
+		for _, player in pairs(minetest.get_connected_players()) do
+			local name = player:get_player_name()
+			local value = not player:get_player_control().sneak
+			if footsteps[name] ~= value then
+				player:set_properties({
+						makes_footstep_sound = value
+					})
+				footsteps[name] = value
+			end
+		end
+	end)
