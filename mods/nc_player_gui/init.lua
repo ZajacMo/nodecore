3e5a3b5f8cf3119eb4f15441634903d4724848d5
Aaron Suen <warr1024@gmail.com>
2019-10-01 19:27:43 -0400

    Remove the now-incorrect "torches are not a thing" tip.
    We may need to provide some more information about torches later,
    but for now, we simply won't disclaim their existence.

--- mods/nc_player_gui/init.lua
+++ mods/nc_player_gui/init.lua
@@ -71,7 +71,6 @@ nodecore.register_inventory_tab({
 			"- Stuck in a pit? Pummel surfaces barehanded to find places to climb.",
 			"- Can't dig trees or grass? Search for sticks in the canopy.",
 			"- Ores may be hidden, but revealed by subtle clues in terrain.",
-			"- \"Torches\" are not a thing; use fire as your first light source.",
 			"- \"Furnaces\" are not a thing; discover smelting with open flames.",
 			"- Trouble lighting a fire? Try using longer sticks, more tinder.",
 			"- The game is challenging by design, sometimes frustrating. DON'T GIVE UP!"
