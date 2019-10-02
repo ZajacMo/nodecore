23140977813573b4674c9768d550ed24a8b2deca
Aaron Suen <warr1024@gmail.com>
2019-10-01 21:14:14 -0400

    Tweak torch wield texture, extend avg life 50%.

--- mods/nc_torch/node.lua
+++ mods/nc_torch/node.lua
@@ -29,7 +29,7 @@ minetest.register_node(modname .. ":torch", {
 		on_ignite = function(pos)
 			minetest.set_node(pos, {name = modname .. ":torch_lit"})
 			minetest.sound_play("nc_fire_ignite", {gain = 1, pos = pos})
-			local expire = minetest.get_gametime() + nodecore.boxmuller() * 5 + 40
+			local expire = minetest.get_gametime() + nodecore.boxmuller() * 5 + 60
 			minetest.get_meta(pos):set_float("expire", expire)
 			return true
 		end
