1fbad4ad70e6f63cb4dd10cadb1cfb267129a3a9
Aaron Suen <warr1024@gmail.com>
2019-10-06 11:29:46 -0400

    Buff torch firestarting ability.

--- mods/nc_torch/node.lua
+++ mods/nc_torch/node.lua
@@ -23,7 +23,7 @@ minetest.register_node(modname .. ":torch", {
 			snappy = 1,
 			falling_repose = 1,
 			flammable = 1,
-			firestick = 1,
+			firestick = 3,
 			stack_as_node = 1
 		},
 		sounds = nodecore.sounds("nc_tree_sticky"),
