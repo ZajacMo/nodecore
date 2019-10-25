ac77b41af589928347c957154a23edfd92c9a467
Aaron Suen <warr1024@gmail.com>
2019-10-24 20:16:37 -0400

    Revert "Scaling overhaul."
    While this makes scaling a little easier to do, it still doesn't
    fix the fundamental problem of scaling being distracting and too
    easy to trigger accidentally.
    
    The "pummel" version of scaling caused some confusion when one
    player accidentally triggered it trying to figure out how to get
    lode out of lode ore.  The current one triggers accidentally all
    the time just from tabbing out of a game with it running (e.g. it
    isn't paused in multiplayer) leaving the character staring at a
    wall.  While such a player WOULD have opportunity to find
    handholds in the wall, we aren't differentiating between a player
    who's scrutinizing a surface from one staring off into space.
    
    The scaling needs to be made harder to trigger accidentally, like
    reinstating the need for an empty hand.  Using a right-click
    action may also help distinguish it from pummelng recipes.
    
    Removing this from the alpha branch so players don't get too
    accustomed to this functionality and then have it change AGAIN.
    
    This reverts commit 943604cf9588a316dce19e0a60a4219b52c4d905.

--- mods/nc_scaling/init.lua
+++ mods/nc_scaling/init.lua
@@ -1,19 +1,41 @@
 -- LUALOCALS < ---------------------------------------------------------
-local ItemStack, math, minetest, nodecore, pairs, vector
-    = ItemStack, math, minetest, nodecore, pairs, vector
-local math_random
-    = math.random
+local minetest, nodecore, pairs, vector
+    = minetest, nodecore, pairs, vector
 -- LUALOCALS > ---------------------------------------------------------
 
 nodecore.amcoremod()
 
 local modname = minetest.get_current_modname()
 
-minetest.register_node(modname .. ":root", {
+minetest.register_node(modname .. ":steps", {
 		paramtype = "light",
+		paramtype2 = "facedir",
+		sunlight_propagates = true,
+		tiles = {
+			"nc_scaling_blank.png",
+			"nc_scaling_blank.png",
+			"nc_scaling_blank.png",
+			"nc_scaling_blank.png",
+			"nc_scaling_blank.png",
+			"nc_scaling_steps.png"
+		},
+		use_texture_alpha = true,
+		drawtype = "nodebox",
+		node_box = nodecore.fixedbox(-0.5, -0.5, 31/64, 0.5, 0.5, 0.5),
+		walkable = false,
+		climbable = true,
+		pointable = false,
+		buildable_to = true,
+		air_equivalent = true,
+		light_source = 1,
+		groups = {[modname] = 1}
+	})
+
+minetest.register_node(modname .. ":hang", {
+		paramtype = "light",
+		paramtype2 = "facedir",
 		sunlight_propagates = true,
 		drawtype = "airlike",
-		light_source = 1,
 		walkable = false,
 		climbable = true,
 		pointable = false,
@@ -22,48 +44,23 @@ minetest.register_node(modname .. ":root", {
 		groups = {[modname] = 1}
 	})
 
-minetest.register_node(modname .. ":branch", {
+minetest.register_node(modname .. ":see", {
 		paramtype = "light",
 		sunlight_propagates = true,
-		drawtype = "airlike",
+		tiles = {
+			"nc_scaling_steps.png",
+			"nc_scaling_blank.png"
+		},
+		use_texture_alpha = true,
+		drawtype = "nodebox",
+		node_box = nodecore.fixedbox(-0.5, -0.5, -0.5, 0.5, -31/64, 0.5),
 		walkable = false,
-		climbable = true,
 		pointable = false,
 		buildable_to = true,
 		air_equivalent = true,
-		groups = {[modname] = 1}
-	})
-
-minetest.register_node(modname .. ":floor", {
-		paramtype = "light",
-		sunlight_propagates = true,
-		drawtype = "airlike",
 		light_source = 1,
-		walkable = false,
-		pointable = false,
-		buildable_to = true,
-		air_equivalent = true,
 		groups = {[modname] = 1}
 	})
-
-local function solid(pos)
-	local node = minetest.get_node(pos)
-	local def = minetest.registered_nodes[node.name]
-	if def and def.walkable and (not nodecore.toolspeed(
-			ItemStack(""), def.groups)) then return true end
-end
-
-local function findrel(pos, test)
-	if test({x = pos.x, y = pos.y + 1, z = pos.z}) then
-		return "ceil"
-	end
-	if test({x = pos.x + 1, y = pos.y, z = pos.z})
-	or test({x = pos.x - 1, y = pos.y, z = pos.z})
-	or test({x = pos.x, y = pos.y, z = pos.z + 1})
-	or test({x = pos.x, y = pos.y, z = pos.z - 1}) then
-		return "wall"
-	end
-end
 
 local function closenough(pos, player)
 	local pp = player:get_pos()
@@ -71,161 +68,159 @@ local function closenough(pos, player)
 	return vector.distance(pos, pp) <= 5
 end
 
-local function anyclosenough(pos)
-	for _, p in pairs(minetest.get_connected_players()) do
-		if closenough(pos, p) then return true end
-	end
-end
-
-local function sparkle(pos)
-	minetest.add_particlespawner({
-			texture = modname .. "_particle.png",
-			collisiondetection = false,
-			amount = 10,
-			time = 5,
-			minpos = {x = pos.x - 0.4, y = pos.y - 0.4, z = pos.z - 0.4},
-			maxpos = {x = pos.x + 0.4, y = pos.y + 0.4, z = pos.z + 0.4},
-			minvel = {x = -0.02, y = -0.02, z = -0.02},
-			maxvel = {x = 0.02, y = 0.02, z = 0.02},
-			minexptime = 2,
-			maxexptime = 8,
-			minsize = 0.25,
-			maxsize = 0.5,
-			glow = -4
-		})
-end
-
 nodecore.register_limited_abm({
-		label = "Scaling Decay (Root)",
-		interval = 5,
+		label = "Scaling Decay",
+		interval = 1,
 		chance = 1,
 		limited_max = 100,
-		nodenames = {modname .. ":root"},
+		nodenames = {"group:" .. modname},
 		action = function(pos)
-			if (not findrel(pos, solid)) or (not anyclosenough(pos)) then
+			local data = minetest.get_meta(pos):get_string("data")
+			if (not data) or (data == "") then
 				return minetest.remove_node(pos)
 			end
-			return sparkle(pos)
+			data = minetest.deserialize(data)
+			if minetest.get_node(data.pos).name ~= data.node then
+				return minetest.remove_node(pos)
+			end
+			for _, p in pairs(minetest.get_connected_players()) do
+				if closenough(pos, p) then return end
+			end
+			return minetest.remove_node(pos)
 		end
 	})
 
-nodecore.register_limited_abm({
-		label = "Scaling Decay (Branch)",
-		interval = 5,
-		chance = 1,
-		limited_max = 100,
-		nodenames = {modname .. ":branch"},
-		action = function(pos)
-			local meta = minetest.get_meta(pos)
-			local root = meta:get_string("root")
-			if (not root) or (root == "") then
-				return minetest.remove_node(pos)
-			end
-			root = minetest.string_to_pos(root)
-			if (minetest.get_node(root).name ~= modname .. ":root")
-			or (not anyclosenough(pos)) then
-				return minetest.remove_node(pos)
+local function stepcheck(pos, data)
+	if not closenough(pos, data.crafter) then return end
+	if not data.wield:is_empty() then return end
+	local node = minetest.get_node(pos)
+	local def = minetest.registered_nodes[node.name]
+	return not nodecore.toolspeed(data.wield, def.groups)
+end
+
+local function setstep(pos, node, mdata, param2)
+	node = {name = modname .. ":" .. node}
+	if param2 then node.param2 = param2 end
+	minetest.set_node(pos, node)
+	minetest.get_meta(pos):set_string("data",
+		minetest.serialize(mdata))
+end
+
+local function hangcheck(pos, dx, dy, dz, mdata)
+	pos = {
+		x = pos.x + dx,
+		y = pos.y + dy,
+		z = pos.z + dz
+	}
+	if minetest.get_node(pos).name ~= "air" then return end
+	setstep(pos, "hang", mdata)
+end
+
+local function stepafter(dx, dy, dz)
+	return function(pos, data)
+		local p = data.rel(dx, dy, dz)
+
+		local d = vector.subtract(p, pos)
+		local fd = 0
+		for i = 0, #nodecore.facedirs do
+			if vector.equals(nodecore.facedirs[i].f, d) then
+				fd = i
+				break
 			end
 		end
+
+		nodecore.node_sound(pos, "place")
+
+		local mdata = {
+			pos = pos,
+			node = data.node.name
+		}
+		setstep(p, "steps", mdata, fd)
+
+		-- invisible support below to simulate hanging by fingertips
+		hangcheck(p, 0, -1, 0, mdata)
+		if dy ~= 0 then
+			-- invisible support to the sides to simulate swinging
+			-- outward to grab onto the side of an overhang when
+			-- hanging below it
+			hangcheck(p, 1, -1, 0, mdata)
+			hangcheck(p, -1, -1, 0, mdata)
+			hangcheck(p, 0, -1, 1, mdata)
+			hangcheck(p, 0, -1, -1, mdata)
+		end
+	end
+end
+
+local pumparticles = {
+	minsize = 1,
+	maxsize = 5,
+	forcetexture = "nc_scaling_particle.png",
+	glow = 1
+}
+
+nodecore.register_craft({
+		label = "scale sheer walls",
+		action = "pummel",
+		priority = -100,
+		pumparticles = pumparticles,
+		duration = 5,
+		normal = {x = 1},
+		check = stepcheck,
+		nodes = {
+			{
+				match = {walkable = true}
+			},
+			{
+				x = 1,
+				match = {any = {"air", modname .. ":hang"}}
+			}
+		},
+		after = stepafter(1, 0, 0)
 	})
 
-nodecore.register_limited_abm({
-		label = "Scaling Decay (Floor)",
-		interval = 5,
-		chance = 1,
-		limited_max = 100,
-		nodenames = {modname .. ":floor"},
-		action = function(pos)
-			if (not solid({x = pos.x, y = pos.y - 1, z = pos.z}))
-			or (not anyclosenough(pos)) then
-				return minetest.remove_node(pos)
-			end
-		end
+nodecore.register_craft({
+		label = "scale sheer ceilings",
+		action = "pummel",
+		priority = -100,
+		pumparticles = pumparticles,
+		duration = 10,
+		normal = {y = -1},
+		check = stepcheck,
+		nodes = {
+			{
+				match = {walkable = true}
+			},
+			{
+				y = -1,
+				match = "air"
+			}
+		},
+		after = stepafter(0, -1, 0)
 	})
 
-local function addbranch(pos, dx, dy, dz)
-	local npos = {x = pos.x + dx, y = pos.y + dy, z = pos.z + dz}
-	if minetest.get_node(npos).name ~= "air" then return end
-	minetest.set_node(npos, {name = modname .. ":branch"})
-	minetest.get_meta(npos):set_string("root", minetest.pos_to_string(pos))
-end
-
-local function addanchor(pos)
-	local rel = findrel(pos, solid)
-	if rel then
-		minetest.set_node(pos, {name = modname .. ":root"})
-		sparkle(pos)
-		addbranch(pos, 0, -1, 0)
-		if rel == "ceil" then
-			addbranch(pos, 1, -1, 0)
-			addbranch(pos, -1, -1, 0)
-			addbranch(pos, 0, -1, 1)
-			addbranch(pos, 0, -1, -1)
+nodecore.register_craft({
+		label = "scale sheer floors",
+		action = "pummel",
+		priority = -100,
+		pumparticles = pumparticles,
+		duration = 5,
+		normal = {y = 1},
+		check = stepcheck,
+		nodes = {
+			{
+				match = {walkable = true}
+			},
+			{
+				y = 1,
+				match = {any = {"air", modname .. ":hang"}},
+				replace = modname .. ":see"
+			}
+		},
+		after = function(pos)
+			minetest.get_meta({x = pos.x, y = pos.y + 1, z = pos.z})
+			:set_string("data", minetest.serialize({
+						pos = pos,
+						node = minetest.get_node(pos).name
+					}))
 		end
-	elseif solid({x = pos.x, y = pos.y - 1, z = pos.z}) then
-		minetest.set_node(pos, {name = modname .. ":floor"})
-	end
-end
-
-local cache = {}
-
-local function cacheclean()
-	local exp = minetest.get_us_time() - 10 * 1000000
-	minetest.after(10, cacheclean)
-	for _, pc in pairs(cache) do
-		for pkey, pp in pairs(pc) do
-			if (pp.last or 0) < exp then
-				pc[pkey] = nil
-			end
-		end
-	end
-end
-cacheclean()
-
-local function hit(pos, player)
-	local nn = minetest.get_node(pos).name
-	if nn ~= "air" and nn ~= modname .. ":branch" then return end
-	local pname = player:get_player_name()
-	local pc = cache[pname]
-	if not pc then
-		pc = {}
-		cache[pname] = pc
-	end
-	local key = minetest.pos_to_string(pos)
-	local pp = pc[key]
-	if not pp then
-		pp = {}
-		pc[key] = pp
-	end
-	pp.last = minetest.get_us_time()
-	pp.hits = (pp.hits or 0) + 1
-	if pp.hits >= 5 then
-		pc[key] = nil
-		return addanchor(pos)
-	end
-end
-
-local function scanplayer(player)
-	local pos = player:get_pos()
-	pos.y = pos.y + player:get_properties().eye_height
-	local dir = player:get_look_dir()
-	dir.x = dir.x + nodecore.boxmuller() / 3
-	dir.y = dir.y + nodecore.boxmuller() / 3
-	dir.z = dir.z + nodecore.boxmuller() / 3
-	dir = vector.multiply(dir, math_random() * 5)
-	for pointed in minetest.raycast(pos, vector.add(pos, dir)) do
-		if pointed.type == "node" then return hit(pointed.above, player) end
-	end
-end
-
-local function scan()
-	minetest.after(0.25, scan)
-	for _, player in pairs(minetest.get_connected_players()) do
-		local ctl = player:get_player_control()
-		if not ctl.right and not ctl.left and not ctl.down and not ctl.up
-		and not ctl.LMB and not ctl.RBM then
-			scanplayer(player)
-		end
-	end
-end
-scan()
+	})
