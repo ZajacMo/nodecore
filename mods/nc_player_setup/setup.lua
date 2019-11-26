a634777ecdc1b837a5a3019ab4ac8d6335ac685d
Aaron Suen <warr1024@gmail.com>
2019-11-25 21:14:22 -0500

    Fix non-interact players dropping injuries.

--- mods/nc_player_setup/setup.lua
+++ mods/nc_player_setup/setup.lua
@@ -36,8 +36,11 @@ local function privdropinv(player)
 	local inv = player:get_inventory()
 	for i, stack in pairs(inv:get_list("main")) do
 		if not stack:is_empty() then
-			nodecore.item_eject(pos, stack, 0.001)
-			inv:set_stack("main", i, "")
+			local def = minetest.registered_items[stack:get_name()]
+			if def and not def.virtual_item then
+				nodecore.item_eject(pos, stack, 0.001)
+				inv:set_stack("main", i, "")
+			end
 		end
 	end
 end
