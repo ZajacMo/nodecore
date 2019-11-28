45c31cd203f69177e4b9c86650ddcb68aeff65b6
Aaron Suen <warr1024@gmail.com>
2019-11-28 11:03:45 -0500

    Lenses can still burn transparent things.
    The sunlight_propagates flag indicates that a node
    can MOSTLY transmit light, like wooden frames, items,
    or torches, but there are no purely transparent things
    that are flammable, and all existing flammable things
    are at least opaque at their centers, where lens light
    would focus.

--- mods/nc_optics/lens.lua
+++ mods/nc_optics/lens.lua
@@ -115,8 +115,7 @@ nodecore.register_limited_abm({
 			local out = vector.add(face.k, pos)
 			local tn = minetest.get_node(out)
 			local tdef = minetest.registered_items[tn.name] or {}
-			local flam = tdef and (not tdef.sunlight_propagates)
-			and tdef.groups and tdef.groups.flammable
+			local flam = tdef and tdef.groups and tdef.groups.flammable
 			if flam then
 				return nodecore.fire_check_ignite(out, tn)
 			end
