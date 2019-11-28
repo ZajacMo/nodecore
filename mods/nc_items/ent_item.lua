d016885364b062869ee56f6180fc34f4083e7b26
Aaron Suen <warr1024@gmail.com>
2019-11-28 08:40:28 -0500

    Attempt to settle items stuck inside nodes.

--- mods/nc_items/ent_item.lua
+++ mods/nc_items/ent_item.lua
@@ -5,25 +5,35 @@ local math_random
     = math.random
 -- LUALOCALS > ---------------------------------------------------------
 
+local function settle(self)
+	local pos = vector.round(self.object:get_pos())
+	local i = ItemStack(self.itemstring)
+	pos = nodecore.scan_flood(pos, 5,
+		function(p)
+			if p.y > pos.y and math_random() < 0.95 then return end
+			if p.y > pos.y + 1 then return end
+			i = nodecore.stack_add(p, i)
+			if i:is_empty() then return p end
+			if nodecore.buildable_to(p) then return p end
+		end)
+	if not pos then return end
+	if not i:is_empty() then nodecore.place_stack(pos, i) end
+	self.itemstring = ""
+	self.object:remove()
+	return true
+end
+
 local bii = minetest.registered_entities["__builtin:item"]
 local newbii = {
 	on_step = function(self, dtime, ...)
 		bii.on_step(self, dtime, ...)
-		if self.moving_state then return end
-		local pos = vector.round(self.object:get_pos())
-		local i = ItemStack(self.itemstring)
-		pos = nodecore.scan_flood(pos, 5,
-			function(p)
-				if p.y > pos.y and math_random() < 0.95 then return end
-				if p.y > pos.y + 1 then return end
-				i = nodecore.stack_add(p, i)
-				if i:is_empty() then return p end
-				if nodecore.buildable_to(p) then return p end
-			end)
-		if not pos then return end
-		if not i:is_empty() then nodecore.place_stack(pos, i) end
-		self.itemstring = ""
-		self.object:remove()
+		if not self.moving_state then
+			return settle(self)
+		end
+	end,
+	disable_physics = function(self, ...)
+		if settle(self) then return end
+		return bii.disable_physics(self, ...)
 	end,
 	on_punch = function(self, whom, ...)
 		if not nodecore.interact(whom) then return end
