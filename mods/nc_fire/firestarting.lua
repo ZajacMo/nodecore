d0afb15f1cd3fe8e2dcd72ffd86b2101d97241bd
Aaron Suen <warr1024@gmail.com>
2019-12-01 13:10:49 -0500

    Add sparks to stick firestarting.
    This should provide a visual clue as to WHERE players
    should put tinder to get it to catch properly, since this
    seems to be a common problem players run into.

--- mods/nc_fire/firestarting.lua
+++ mods/nc_fire/firestarting.lua
@@ -1,6 +1,6 @@
 -- LUALOCALS < ---------------------------------------------------------
-local ItemStack, math, minetest, nodecore
-    = ItemStack, math, minetest, nodecore
+local ItemStack, math, minetest, nodecore, vector
+    = ItemStack, math, minetest, nodecore, vector
 local math_random
     = math.random
 -- LUALOCALS > ---------------------------------------------------------
@@ -27,6 +27,22 @@ nodecore.register_craft({
 
 			if math_random(1, 4) > fs then return end
 			nodecore.fire_ignite(pos)
+			minetest.add_particlespawner({
+					amount = 25,
+					time = 0.02,
+					minpos = vector.subtract(pos, 0.25),
+					maxpos = vector.add(pos, 0.25),
+					minvel = {x = -2, y = 0, z = -2},
+					maxvel = {x = 2, y = 0, z = 2},
+					minacc = {x = -0.1, y = 0, z = -0.1},
+					maxacc = {x = 0.1, y = 0, z = 0.1},
+					minxeptime = 0.4,
+					maxexptime = 0.5,
+					minsize = 0.4,
+					maxsize = 0.5,
+					texture = "nc_fire_spark.png",
+					glow = 7
+				})
 
 			if math_random(1, 4) > fs then return end
 			local dir = nodecore.pickrand(nodecore.dirs())
