658100cd7e4ca81dd48fe9f0ea4d4f1a0115fd7c
Aaron Suen <warr1024@gmail.com>
2019-12-05 22:26:44 -0500

    TouchTip overhaul.
    - Support centered multiline descriptions.
    - Automatically include visinv stack descriptions.
    - Translations per-line.
    - API for custom TouchTips.

--- mods/nc_player_hud/touchtip.lua
+++ mods/nc_player_hud/touchtip.lua
@@ -1,60 +1,111 @@
 -- LUALOCALS < ---------------------------------------------------------
-local minetest, nodecore, pairs
-    = minetest, nodecore, pairs
+local minetest, nodecore, pairs, string
+    = minetest, nodecore, pairs, string
+local string_gmatch, string_rep
+    = string.gmatch, string.rep
 -- LUALOCALS > ---------------------------------------------------------
 
 local tips = {}
 
 local function show(player, text, ttl)
-	text = nodecore.translate(text)
-	ttl = ttl or 2
 	local pname = player:get_player_name()
-	local tip = tips[pname]
-	if tip then
-		if text ~= tip.text then
-			player:hud_change(tip.id, "text", text)
-			tip.text = text
-		end
-		tip.ttl = ttl
-		return
+	local lines = {}
+	for str in string_gmatch(text, "[^\r\n]+") do
+		lines[#lines + 1] = nodecore.translate(str)
 	end
-	tips[pname] = {
-		id = player:hud_add({
-				hud_elem_type = "text",
-				position = {x = 0.5, y = 0.75},
-				text = text,
-				number = 0xFFFFFF,
-				alignment = {x = 0, y = 0},
-				offset = {x = 0, y = 0},
-			}),
-		text = text,
-		ttl = ttl
-	}
+	for i = 1, #lines do
+		lines[i] = string_rep(" \n", i - 1) .. lines[i]
+		.. string_rep("\n ", #lines - i)
+	end
+
+	local tip = tips[pname]
+	if not tip then
+		tip = {lines = {}}
+		tips[pname] = tip
+	end
+
+	tip.ttl = ttl or 2
+
+	for i = 1, #lines do
+		local old = tip.lines[i]
+		if not old then
+			tip.lines[i] = {
+				id = player:hud_add({
+						hud_elem_type = "text",
+						position = {x = 0.5, y = 0.75},
+						text = lines[i],
+						number = 0xFFFFFF,
+						alignment = {x = 0, y = 0},
+						offset = {x = 0, y = 0},
+					}),
+				text = lines[i]
+			}
+		elseif old.text ~= lines[i] then
+			player:hud_change(old.id, "text", lines[i])
+			old.text = lines[i]
+		end
+	end
+
+	for i = #tip.lines, #lines + 1, -1 do
+		local id = tip.lines[i].id
+		player:hud_change(id, "text", "")
+		minetest.after(0, function() player:hud_remove(id) end)
+		tip.lines[i] = nil
+	end
 end
 nodecore.show_touchtip = show
-
-local wields = {}
 
 local function stack_desc(s)
 	if s:is_empty() then return "" end
 
-	local t = s:get_meta():get_string("description")
-	if t and t ~= "" then return t end
-
 	local n = s:get_name()
 	local d = minetest.registered_items[n] or {}
-	return d.description or n
-end
 
-local function wield_name(player)
-	return stack_desc(player:get_wielded_item())
+	local t = s:get_meta():get_string("description")
+	t = t ~= "" and t or d.description or n
+
+	if d.on_stack_touchtip then
+		return d.on_stack_touchtip(s, t) or t
+	end
+	return t
 end
+nodecore.touchtip_stack = stack_desc
+
+local function node_desc(pos, node)
+	node = node or minetest.get_node(pos)
+	local name = node.name
+	local def = minetest.registered_items[name] or {}
+	if def.air_equivalent or def.pointable == false then return end
+
+	local metaname = minetest.get_meta(pos):get_string("description")
+	if metaname and metaname ~= "" then
+		name = metaname
+	elseif def.groups and def.groups.is_stack_only then
+		name = stack_desc(nodecore.stack_get(pos))
+	elseif def.description then
+		name = def.description
+	end
+
+	if def.groups and def.groups.visinv and not def.groups.is_stack_only then
+		local s = nodecore.stack_get(pos)
+		local t = stack_desc(s)
+		if t and t ~= "" then name = name .. "\n" .. t end
+	end
+
+	if def.on_node_touchtip then
+		return def.on_node_touchtip(pos, node, name) or name
+	end
+	return name
+end
+nodecore.touchtip_node = node_desc
+
+local wields = {}
 
 minetest.register_globalstep(function(dtime)
 		for _, player in pairs(minetest.get_connected_players()) do
 			local pname = player:get_player_name()
 
-			local wn = wield_name(player)
+			local wn = stack_desc(player:get_wielded_item())
 			if wn ~= wields[pname] then
 				wields[pname] = wn
 				show(player, wn)
@@ -64,7 +115,9 @@ minetest.register_globalstep(function(dtime)
 			if tip then
 				tip.ttl = tip.ttl - dtime
 				if tip.ttl <= 0 then
-					player:hud_remove(tip.id)
+					for _, l in pairs(tip.lines) do
+						player:hud_remove(l.id)
+					end
 					tips[pname] = nil
 				end
 			end
@@ -72,17 +125,7 @@ minetest.register_globalstep(function(dtime)
 	end)
 
 minetest.register_on_punchnode(function(pos, node, puncher)
-		node = node or minetest.get_node(pos)
-		local name = node.name
-		local def = minetest.registered_items[name] or {}
-		if def.air_equivalent or def.pointable == false then return end
-		if def.groups and def.groups.is_stack_only then
-			name = stack_desc(nodecore.stack_get(pos))
-		elseif def.description then
-			name = def.description
-		end
-
-		show(puncher, name)
+		return show(puncher, nodecore.touchtip_node(pos, node))
 	end)
 
 minetest.register_on_joinplayer(function(player)
