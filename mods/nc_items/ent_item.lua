e0f2cb5b6dd449ac48470da3e3f1e7ea7febe227
Aaron Suen <warr1024@gmail.com>
2019-12-07 11:12:55 -0500

    Item settling overhaul.
    - Always prefer closest place first, then lowest height.
    - Simplify search order; use a pre-built order instead of scan.
    - Randomize search order only once per tick, if used.

--- mods/nc_items/ent_item.lua
+++ mods/nc_items/ent_item.lua
@@ -1,26 +1,82 @@
 -- LUALOCALS < ---------------------------------------------------------
-local ItemStack, math, minetest, nodecore, setmetatable, vector
-    = ItemStack, math, minetest, nodecore, setmetatable, vector
-local math_random
-    = math.random
+local ItemStack, math, minetest, nodecore, pairs, setmetatable, table,
+      vector
+    = ItemStack, math, minetest, nodecore, pairs, setmetatable, table,
+      vector
+local math_random, table_sort
+    = math.random, table.sort
 -- LUALOCALS > ---------------------------------------------------------
+
+local settleused
+local settleorder = {}
+do
+	local groups = {}
+	local keys = {}
+	for x = -5, 5 do
+		for y = -5, 5 do
+			for z = -5, 5 do
+				local p = {
+					x = x,
+					y = y,
+					z = z,
+				}
+				local d = vector.length(p)
+				if d <= 5 then
+					p.v = d + p.y / 100
+					local g = groups[p.v]
+					if not g then
+						g = {}
+						groups[p.v] = g
+						keys[#keys + 1] = p.v
+					end
+					g[#g + 1] = p
+				end
+			end
+		end
+	end
+	table_sort(keys)
+	for _, k in pairs(keys) do
+		settleorder[#settleorder + 1] = groups[k]
+	end
+end
+
+minetest.register_globalstep(function()
+		if not settleused then return end
+		settleused = nil
+		for _, t in pairs(settleorder) do
+			local n = #t
+			for i = 1, n do
+				local j = math_random(1, n)
+				local x = t[i]
+				t[i] = t[j]
+				t[j] = x
+			end
+		end
+	end)
 
 local function settle(self)
 	local pos = vector.round(self.object:get_pos())
-	local i = ItemStack(self.itemstring)
-	pos = nodecore.scan_flood(pos, 5,
-		function(p)
-			if p.y > pos.y and math_random() < 0.95 then return end
-			if p.y > pos.y + 1 then return end
-			i = nodecore.stack_add(p, i)
-			if i:is_empty() then return p end
-			if nodecore.buildable_to(p) then return p end
-		end)
-	if not pos then return end
-	if not i:is_empty() then nodecore.place_stack(pos, i) end
-	self.itemstring = ""
-	self.object:remove()
-	return true
+	local item = ItemStack(self.itemstring)
+	settleused = true
+	for i = 1, #settleorder do
+		local grp = settleorder[i]
+		for j = 1, #grp do
+			local p = vector.add(pos, grp[j])
+			item = nodecore.stack_add(p, item)
+			if item:is_empty() then
+				self.itemstring = ""
+				self.object:remove()
+				return true
+			end
+			if nodecore.buildable_to(p) then
+				nodecore.place_stack(p, item)
+				self.itemstring = ""
+				self.object:remove()
+				return true
+			end
+		end
+	end
+	self.itemstring = item:to_string()
 end
 
 local bii = minetest.registered_entities["__builtin:item"]
