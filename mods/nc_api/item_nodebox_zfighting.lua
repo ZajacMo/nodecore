c29c2ed9d040fdba7beb0809b13c5d3566a9f3f9
Aaron Suen <warr1024@gmail.com>
2019-12-08 18:43:36 -0500

    Fix one more nodebox regression.

--- mods/nc_api/item_nodebox_zfighting.lua
+++ mods/nc_api/item_nodebox_zfighting.lua
@@ -1,27 +1,29 @@
 -- LUALOCALS < ---------------------------------------------------------
-local minetest, nodecore, pairs, type
-    = minetest, nodecore, pairs, type
+local nodecore, pairs, type
+    = nodecore, pairs, type
 -- LUALOCALS > ---------------------------------------------------------
 
 local function scantbl(t, adjto)
+	local u = {}
 	for k, v in pairs(t) do
 		if v == 0.5 then
-			t[k] = adjto
+			u[k] = adjto
 		elseif v == -0.5 then
-			t[k] = -adjto
+			u[k] = -adjto
 		elseif type(v) == "table" then
-			scantbl(v, adjto)
+			u[k] = scantbl(v, adjto)
+		else
+			u[k] = v
 		end
 	end
+	return u
 end
-
-local function clone(t) return minetest.deserialize(minetest.serialize(t)) end
 
 nodecore.register_on_register_item(function(_, def)
 		if def.type ~= "node" then return end
 		if def.node_box and def.node_box.fixed then
-			def.collision_box = def.collision_box or clone(def.node_box)
-			def.selection_box = def.selection_box or clone(def.node_box)
-			scantbl(def.node_box.fixed, def.z_fight_win and 129/256 or 127/256)
+			def.collision_box = def.collision_box or def.node_box
+			def.selection_box = def.selection_box or def.node_box
+			def.node_box = scantbl(def.node_box, def.z_fight_win and 129/256 or 127/256)
 		end
 	end)
