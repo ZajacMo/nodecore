15134ad90cd9b1af86146d81deffed8c5aed7582
Aaron Suen <warr1024@gmail.com>
2019-12-31 08:49:37 -0500

    Fix pulverize priv not covering clearinv cmd.

--- mods/nc_items/pulverize.lua
+++ mods/nc_items/pulverize.lua
@@ -4,13 +4,14 @@ local minetest, pairs
 -- LUALOCALS > ---------------------------------------------------------
 
 minetest.register_privilege("pulverize", {
-		description = "Can use the /pulverize command",
+		description = "Can pulverize inventory items",
 		give_to_singleplayer = false,
 		give_to_admin = false
 	})
 
+local wrap = {pulverize = true, clearinv = true}
 for k, v in pairs(minetest.registered_chatcommands) do
-	if k == "pulverize" then
+	if wrap[k] then
 		v.privs = v.privs or {}
 		v.privs.pulverize = true
 		minetest.override_chatcommand(k, v)
