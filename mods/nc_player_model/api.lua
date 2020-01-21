dda8a58bae3481b710f4564ef38b17932ff310dd
Aaron Suen <warr1024@gmail.com>
2020-01-20 21:32:59 -0500

    Make the base model anim data public too.
    This allows overriding the data to use the same
    logic but a different model, overriding the function
    to use the same data but different logic, or both.

--- mods/nc_player_model/api.lua
+++ mods/nc_player_model/api.lua
@@ -54,7 +54,7 @@ nodecore.player_skin = nodecore.player_skin or function(player)
 	return table_concat(layers, "^"), layers
 end
 
-local anim_data = {
+nodecore.player_anim_data = nodecore.player_anim_data or {
 	stand = {x = 0, y = 0},
 	sit = {x = 1, y = 1},
 	walk = {x = 2, y = 42},
@@ -65,7 +65,7 @@ local anim_data = {
 	swim_down = {x = 163, y = 223, speed = 0.4},
 	swim_mine = {x = 224, y = 281, speed = 0.5}
 }
-for k, v in pairs(anim_data) do
+for k, v in pairs(nodecore.player_anim_data) do
 	v.name = k
 	v.speed = 72 * (v.speed or 1)
 end
@@ -73,7 +73,7 @@ end
 nodecore.player_anim = nodecore.player_anim or function(player)
 	local hp = player:get_hp()
 	if hp <= 0 then
-		return anim_data.lay
+		return nodecore.player_anim_data.lay
 	end
 
 	local ctl = player:get_player_control()
@@ -81,16 +81,16 @@ nodecore.player_anim = nodecore.player_anim or function(player)
 	local mine = ctl.LMB or ctl.RMB
 
 	if not nodecore.player_swimming(player) then
-		if walk and mine then return anim_data.walk_mine end
-		if walk then return anim_data.walk end
-		if mine then return anim_data.mine end
-		return anim_data.stand
+		if walk and mine then return nodecore.player_anim_data.walk_mine end
+		if walk then return nodecore.player_anim_data.walk end
+		if mine then return nodecore.player_anim_data.mine end
+		return nodecore.player_anim_data.stand
 	end
 
-	if mine then return anim_data.swim_mine end
+	if mine then return nodecore.player_anim_data.swim_mine end
 	local v = player:get_player_velocity()
-	if v and v.y >= -0.5 then return anim_data.swim_up end
-	return anim_data.swim_down
+	if v and v.y >= -0.5 then return nodecore.player_anim_data.swim_up end
+	return nodecore.player_anim_data.swim_down
 end
 
 nodecore.player_visuals_base = nodecore.player_visuals_base or function(player)
