25f4a19761d9d2fd234c1edc82485a5486d9cf7b
Aaron Suen <warr1024@gmail.com>
2020-01-24 21:05:42 -0500

    Let's try another walking speed buff.
    Walking is a big part of gameplay, and builds tend to be
    big and clunky; this will shave of a bit of the boredom.

--- mods/nc_player_setup/setup.lua
+++ mods/nc_player_setup/setup.lua
@@ -14,7 +14,7 @@ minetest.register_on_joinplayer(function(player)
 		inv:set_size("craftpreview", 0)
 		inv:set_size("craftresult", 0)
 
-		player:set_physics_override({speed = 1.25})
+		player:set_physics_override({speed = 1.4})
 
 		player:set_properties({
 				pointable = false,
