5a5c546596807a329523d73e3f1c03bba085fa9b
Aaron Suen <warr1024@gmail.com>
2020-02-24 06:22:09 -0500

    Use actual mesh and skin for player hand.

--- mods/nc_player_hand/hand.lua
+++ mods/nc_player_hand/hand.lua
@@ -5,6 +5,8 @@ local math_pow
     = math.pow
 -- LUALOCALS > ---------------------------------------------------------
 
+local modname = minetest.get_current_modname()
+
 local toolcaps = nodecore.toolcaps({
 		uses = 0,
 		crumbly = 1,
@@ -21,7 +23,20 @@ end
 
 minetest.register_item(":", {
 		["type"] = "none",
-		wield_image = "nc_player_hand.png",
-		wield_scale = {x = 4, y = 8, z = 3},
 		tool_capabilities = toolcaps
 	})
+
+local scale = 2
+minetest.register_node(modname .. ":hand", {
+		drawtype = "mesh",
+		mesh = modname .. ".obj",
+		tiles = {"nc_player_model_base.png"},
+		wield_scale = {x = scale, y = scale, z = scale},
+		virtual_item = true
+	})
+
+minetest.register_on_joinplayer(function(player)
+		local inv = player:get_inventory()
+		inv:set_size("hand", 1)
+		inv:set_stack("hand", 1, modname .. ":hand")
+	end)
