ba95b0989ab576fcb369365dace8c447799c6f8f
Aaron Suen <warr1024@gmail.com>
2020-03-21 09:05:53 -0400

    Mapgen: lava and water springs.
    Rarity values may need to be adjusted.

--- mods/nc_sponge/gen.lua
+++ mods/nc_sponge/gen.lua
@@ -34,11 +34,14 @@ nodecore.register_mapgen_shared({
 		func = function(minp, maxp, area, data)
 			if minp.y > maxy or maxp.y < miny then return end
 
-			local qty = math_floor(math_random() * (maxp.x - minp.x)
-				* (maxp.z - minp.z) / (64 * 64))
+			local rawqty = math_random() * (maxp.x - minp.x + 1)
+			* (maxp.z - minp.z + 1) / (64 * 64)
+			local qty = math_floor(rawqty)
+			if math_random() < (rawqty - qty) then qty = qty + 1 end
+
 			for _ = 1, qty do
-				local x = math_floor(math_random() * (maxp.x - minp.x)) + minp.x
-				local z = math_floor(math_random() * (maxp.z - minp.z)) + minp.z
+				local x = math_floor(math_random() * (maxp.x - minp.x + 1)) + minp.x
+				local z = math_floor(math_random() * (maxp.z - minp.z + 1)) + minp.z
 				local starty = maxp.y
 				if starty > (maxy + 1) then starty = (maxy + 1) end
 				local endy = minp.y
--- mods/nc_terrain/ore.lua
+++ mods/nc_terrain/ore.lua
@@ -1,6 +1,8 @@
 -- LUALOCALS < ---------------------------------------------------------
-local minetest
-    = minetest
+local math, minetest, nodecore, pairs
+    = math, minetest, nodecore, pairs
+local math_floor, math_random
+    = math.floor, math.random
 -- LUALOCALS > ---------------------------------------------------------
 
 local modname = minetest.get_current_modname()
@@ -24,3 +26,38 @@ minetest.register_ore({
 		},
 		noise_threshold = 1.2
 	})
+
+local c_air = minetest.get_content_id("air")
+local c_stones = {}
+for _, n in pairs(minetest.registered_nodes[modname .. ":stone"].strata) do
+	c_stones[minetest.get_content_id(n)] = true
+end
+local function regspring(label, node, rarity)
+	local c_node = minetest.get_content_id(node)
+	nodecore.register_mapgen_shared({
+			label = label,
+			func = function(minp, maxp, area, data)
+				local rawqty = math_random() * (maxp.x - minp.x + 1)
+				* (maxp.z - minp.z + 1) * (maxp.y - minp.y + 1) / rarity
+				local qty = math_floor(rawqty)
+				if math_random() < (rawqty - qty) then qty = qty + 1 end
+
+				for _ = 1, qty do
+					local x = math_floor(math_random() * (maxp.x - minp.x + 1)) + minp.x
+					local y = math_floor(math_random() * (maxp.y - minp.y + 1)) + minp.y
+					local z = math_floor(math_random() * (maxp.z - minp.z + 1)) + minp.z
+					if c_stones[data[area:index(x, y, z)]]
+					and (x < maxp.x and data[area:index(x + 1, y, z)] == c_air
+						or x > minp.x and data[area:index(x - 1, y, z)] == c_air
+						or y < maxp.y and data[area:index(x, y + 1, z)] == c_air
+						or y > minp.y and data[area:index(x, y - 1, z)] == c_air
+						or z < maxp.z and data[area:index(x, y, z + 1)] == c_air
+						or z > minp.z and data[area:index(x, y, z - 1)] == c_air)
+					then data[area:index(x, y, z)] = c_node end
+				end
+			end
+		})
+end
+local baserarity = 32 * 32 * 32
+regspring("water spring", modname .. ":water_source", baserarity)
+regspring("lava spring", modname .. ":lava_source", baserarity * 8)
