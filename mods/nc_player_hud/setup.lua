e987a87bd8968071fca33c54659c344ffcaf4528
Aaron Suen <warr1024@gmail.com>
2020-03-22 08:14:51 -0400

    Simplify breath hud handling

--- mods/nc_player_hud/setup.lua
+++ mods/nc_player_hud/setup.lua
@@ -5,13 +5,6 @@ local math_floor
     = math.floor
 -- LUALOCALS > ---------------------------------------------------------
 
-local function breathimg(br)
-	local o = 255 * (1 - br / 11)
-	if o == 0 then return "" end
-	return "nc_player_hud_breath.png^[opacity:"
-	.. math_floor(o)
-end
-
 local function sethudflags(player, pname)
 	local interact = nodecore.interact(pname or player)
 	player:hud_set_flags({
@@ -34,33 +27,33 @@ end
 minetest.register_on_priv_grant(grantrevoke)
 minetest.register_on_priv_revoke(grantrevoke)
 
+local function breath_hud(player)
+	local br = player:get_breath()
+	local img = ""
+	local o = 255 * (1 - br / 11)
+	if o > 0 then img = "nc_player_hud_breath.png^[opacity:" .. math_floor(o) end
+	nodecore.hud_set(player, {
+			label = "breath",
+			hud_elem_type = "image",
+			position = {x = 0.5, y = 0.5},
+			text = img,
+			direction = 0,
+			scale = {x = -100, y = -100},
+			offset = {x = 0, y = 0},
+			quick = true
+		})
+end
+
 minetest.register_on_joinplayer(function(player)
 		sethudflags(player)
 		player:hud_set_hotbar_itemcount(8)
 		player:hud_set_hotbar_image("nc_player_hud_bar.png")
 		player:hud_set_hotbar_selected_image("nc_player_hud_sel.png")
-
-		if not minetest.settings:get_bool("enable_damage") then
-			player:set_breath(11)
-		end
-		local img = breathimg(player:get_breath())
-		nodecore.hud_set(player, {
-				label = "breath",
-				hud_elem_type = "image",
-				position = {x = 0.5, y = 0.5},
-				text = img,
-				direction = 0,
-				scale = {x = -100, y = -100},
-				offset = {x = 0, y = 0},
-				quick = true
-			})
+		breath_hud(player)
 	end)
 
 minetest.register_globalstep(function()
 		for _, player in pairs(minetest.get_connected_players()) do
-			nodecore.hud_set(player, {
-					label = "breath",
-					text = breathimg(player:get_breath())
-				})
+			breath_hud(player)
 		end
 	end)
