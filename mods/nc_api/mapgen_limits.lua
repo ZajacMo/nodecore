cfcbb4bdb00367a50806d3dd3f8d72ad4101ec52
Aaron Suen <warr1024@gmail.com>
2020-03-22 11:07:20 -0400

    Fix broken bounds calc.

--- mods/nc_api/mapgen_limits.lua
+++ mods/nc_api/mapgen_limits.lua
@@ -1,12 +1,18 @@
 -- LUALOCALS < ---------------------------------------------------------
-local minetest, nodecore, tonumber
-    = minetest, nodecore, tonumber
+local math, minetest, nodecore, string, tonumber
+    = math, minetest, nodecore, string, tonumber
+local math_floor, string_format
+    = math.floor, string.format
 -- LUALOCALS > ---------------------------------------------------------
 
 local limit = tonumber(minetest.get_mapgen_setting("mapgen_limit")) or 31000
 
 local chunksize = tonumber(minetest.get_mapgen_setting("chunksize")) or 5
 chunksize = chunksize * 16
+local limitchunks = math_floor(limit / chunksize)
 
-nodecore.map_limit_min = (-limit + 0.5) * chunksize + 7.5
-nodecore.map_limit_max = (limit - 0.5) * chunksize + 7.5
+nodecore.map_limit_min = (-limitchunks + 0.5) * chunksize + 7.5
+nodecore.map_limit_max = (limitchunks - 0.5) * chunksize + 7.5
+
+minetest.log(string_format("mapgen limit: %d, chunk: %d, bounds: %0.1f to %0.1f",
+		limit, chunksize, nodecore.map_limit_min, nodecore.map_limit_max))
