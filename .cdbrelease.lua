f4425c7e4ed83d9b0c48fdafd83046d1e12883b2
Aaron Suen <warr1024@gmail.com>
2020-03-27 23:46:40 -0400

    New cdbrelease metadata format.

--- .cdbrelease.lua
+++ .cdbrelease.lua
@@ -3,10 +3,28 @@ local dofile
     = dofile
 -- LUALOCALS > ---------------------------------------------------------
 
+-- luacheck: push
+-- luacheck: globals config slurp
+
+local alpha = config and config.branch == "dev"
+
 return {
 	user = "Warr1024",
 	pkg = "nodecore",
 	min = "5.0",
 	version = dofile("./mods/nc_api/version.lua"),
-	path = "."
+	path = ".",
+	type = "game",
+	title = "NodeCore",
+	short_desc = (alpha
+		and "Experimental early-access releases of NodeCore."
+		or "An original, immersive puzzle/adventure game with NO popup GUIs, minimal HUDs."),
+	tags = "building, crafting, environment, inventory, machines, puzzle",
+	license = "mit",
+	desc = alpha and slurp('cdb-alpha.md') or slurp('cdb-release.md'),
+	repo = "https://gitlab.com/sztest/nodecore",
+	website = "https://nodecore.mine.nu",
+	issueTracker = "https://discord.gg/NNYeF6f",
 }
+
+-- luacheck: pop
