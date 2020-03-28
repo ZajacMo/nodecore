248a4e4cf7ec8c96f69ee100ffad4310a9f6c88b
Aaron Suen <warr1024@gmail.com>
2020-03-28 06:46:31 -0400

    Metadata file lexical proximity

--- .cdbrelease.lua
+++ .cdbrelease.lua
@@ -21,7 +21,7 @@ return {
 		or "An original, immersive puzzle/adventure game with NO popup GUIs, minimal HUDs."),
 	tags = "building, crafting, environment, inventory, machines, puzzle",
 	license = "mit",
-	desc = alpha and slurp('cdb-alpha.md') or slurp('cdb-release.md'),
+	desc = alpha and slurp('.cdb-alpha.md') or slurp('.cdb-release.md'),
 	repo = "https://gitlab.com/sztest/nodecore",
 	website = "https://nodecore.mine.nu",
 	issueTracker = "https://discord.gg/NNYeF6f",
