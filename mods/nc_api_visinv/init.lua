2c85ee9a811a8bea0aa079f8d2c21f0edd6542a6
Aaron Suen <warr1024@gmail.com>
2020-05-26 19:07:28 -0400

    Unreorder a small calc

--- mods/nc_api_visinv/init.lua
+++ mods/nc_api_visinv/init.lua
@@ -23,11 +23,11 @@ minetest.register_entity(modname .. ":stackent", {
 			local obj = self.object
 			local pos = obj:get_pos()
 			if not pos then return end
-			local rp = vector.round(pos)
 
 			local stack = nodecore.stack_get(pos)
 			if not stack or stack:is_empty() then return obj:remove() end
 
+			local rp = vector.round(pos)
 			local def = minetest.registered_items[stack:get_name()] or {}
 			local src = def.light_source or 0
 			if src > 0 then
