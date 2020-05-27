8a04c5163b8625ae698e1f61f87e725282be5dc6
Aaron Suen <warr1024@gmail.com>
2020-05-26 20:52:54 -0400

    Make damage touch ignore non-pointables

--- mods/nc_api/item_touch_hurt.lua
+++ mods/nc_api/item_touch_hurt.lua
@@ -14,7 +14,8 @@ local function toolcandig(wield, node)
 end
 
 nodecore.register_on_register_item(function(_, def)
-		local dmg = def.groups and def.groups.damage_touch
+		local dmg = def.pointable ~= false and def.groups
+		and def.groups.damage_touch
 		if not (dmg and dmg > 0) then return end
 
 		def.on_punch = def.on_punch or function(pos, node, puncher, ...)
