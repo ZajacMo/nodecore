ZajacMo/nodecore

commit 16a7c9fe2689e73dd2930f629c38abfc9144ae3e

Completing a dig resets pummel
This should fix false pummel detection on long
hard-stone dig/drop-in-place sequences

mods/nc_api_craft/on_punchnode.lua
@@ -43,6 +43,11 @@ end
 
 local pummeling = {}
 
+minetest.register_on_dignode(function(_, _, digger)
+		if not digger:is_player() then return end
+		pummeling[digger:get_player_name()] = nil
+	end)
+
 minetest.register_on_punchnode(function(pos, node, puncher, pointed)
 		if not puncher:is_player() then return end
 		local pname = puncher:get_player_name()

commit 5f165b4da398ad86a234657a54581c9c7bbaffb7

Fix crash on completing stone brick chisel
Bug was introduced in 16a7c9fe, caused by
the fact that sometimes nodes are dug by nil,
i.e. the way the chisel is dug up automatically
in the chiseling process.

mods/nc_api_craft/on_punchnode.lua
@@ -44,7 +44,7 @@ end
 local pummeling = {}
 
 minetest.register_on_dignode(function(_, _, digger)
-		if not digger:is_player() then return end
+		if not (digger and digger:is_player()) then return end
 		pummeling[digger:get_player_name()] = nil
 	end)
 