7e065c813487c4102417e7938d9363d517186797
Aaron Suen <warr1024@gmail.com>
2020-06-07 22:26:28 -0400

    Nerf lux tool durability
    These things are way too durable as they are, and it's
    actually kind of game-breaking.  Players with lux
    tools are in an entirely different class from players
    without.
    
    Instead, players should be forced to invest more in
    lux reactors, or rely more on tempered lode as an
    "everyday" tool and use lux for special projects.
    
    Boosted lux is exempt from this nerf, and is now 4
    times as durable as ordinary lux.  It might not be a
    bad idea to allow them not to experience any wear
    at all when boosted...

--- mods/nc_lux/tools.lua
+++ mods/nc_lux/tools.lua
@@ -33,7 +33,7 @@ for _, shape in pairs({"mallet", "spade", "hatchet", "pick", "mattock"}) do
 		for k, v in pairs(orig.tool_capabilities.opts) do
 			tc[k] = v + 1
 		end
-		tc.uses = 0.5
+		tc.uses = 0.25
 		def.tool_capabilities = nodecore.toolcaps(tc)
 
 		for k, v in pairs(orig.tool_capabilities.opts) do
