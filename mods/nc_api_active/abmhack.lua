ebcc7921e9f21f882b58baa06ee5f48679329730
Aaron Suen <warr1024@gmail.com>
2020-06-21 14:46:30 -0400

    Smooth laundered ABMs across multiple ticks

--- mods/nc_api_active/abmhack.lua
+++ mods/nc_api_active/abmhack.lua
@@ -1,31 +1,43 @@
 -- LUALOCALS < ---------------------------------------------------------
-local minetest, nodecore, pairs, tonumber
-    = minetest, nodecore, pairs, tonumber
+local math, minetest, nodecore, pairs, tonumber
+    = math, minetest, nodecore, pairs, tonumber
+local math_floor
+    = math.floor
 -- LUALOCALS > ---------------------------------------------------------
 
-local defer = {}
+local ratio = math_floor(tonumber(minetest.settings:get(nodecore.product
+			.. "_abm_launder_ratio")) or 4)
+if ratio < 2 then return end
 
+local defer = {}
+for i = 1, (ratio - 1) do defer[i] = {} end
+
+local proc = 0
 minetest.register_globalstep(function()
-		local getnode = minetest.get_node
-		for _, v in pairs(defer) do
-			local node = getnode(v[1])
-			if node.name == v[2] then v[3](v[1], node) end
+		proc = (proc % (ratio - 1)) + 1
+		local batch = defer[proc]
+		print(proc)
+		if #batch > 0 then
+			local getnode = minetest.get_node
+			for _, v in pairs(batch) do
+				local node = getnode(v[1])
+				if node.name == v[2] then v[3](v[1], node) end
+			end
+			defer[proc] = {}
 		end
-		defer = {}
 	end)
 
-local ratio = tonumber(minetest.settings:get(nodecore.product .. "_abm_launder_ratio")) or 4
-
+local bucket = 0
 local oldreg = minetest.register_abm
 function minetest.register_abm(def, ...)
-	local count = 0
 	local oldact = def.action
 	def.action = function(pos, node)
-		count = count + 1
-		if count % ratio == 1 then
+		bucket = (bucket + 1) % ratio
+		if bucket == 0 then
 			return oldact(pos, node)
 		else
-			defer[#defer + 1] = {pos, node.name, oldact}
+			local batch = defer[bucket]
+			batch[#batch + 1] = {pos, node.name, oldact}
 		end
 	end
 	return oldreg(def, ...)
