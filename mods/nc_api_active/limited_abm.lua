a4bf23e421409aa16c2fabf99690be7a044c34ce
Aaron Suen <warr1024@gmail.com>
2020-06-21 00:48:30 -0400

    ABM Laundering: speed up to 4x
    Instead of using 20% of CPU time to run
    ABMs, defer 75% of them, so we are using
    20% time to run 20% ABMs plus defer
    a further 60%, giving us a total of roughly
    80% CPU time for ABMs (not factoring in
    overhead).
    
    This should let us work around the engine's
    hardcoded 20% ABM limit.

--- mods/nc_api_active/limited_abm.lua
+++ mods/nc_api_active/limited_abm.lua
@@ -1,6 +1,30 @@
 -- LUALOCALS < ---------------------------------------------------------
-local minetest, nodecore
-    = minetest, nodecore
+local minetest, nodecore, pairs, tonumber
+    = minetest, nodecore, pairs, tonumber
 -- LUALOCALS > ---------------------------------------------------------
 
-nodecore.register_limited_abm = minetest.register_abm
+local defer = {}
+
+minetest.register_globalstep(function()
+		for _, v in pairs(defer) do v() end
+		defer = {}
+	end)
+
+local ratio = tonumber(minetest.settings:get(nodecore.product .. "_abm_launder_ratio")) or 4
+function nodecore.register_limited_abm(def)
+	local count = 0
+	local oldact = def.action
+	def.action = function(pos, node)
+		count = count + 1
+		if count % ratio == 1 then
+			return oldact(pos, node)
+		else
+			local nn = node.name
+			defer[#defer + 1] = function()
+				node = minetest.get_node(pos)
+				if node.name == nn then return oldact(pos, node) end
+			end
+		end
+	end
+	return minetest.register_abm(def)
+end
