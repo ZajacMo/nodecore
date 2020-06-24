e06fff7620790bedda06c7a7f584026e28d3a208
Aaron Suen <warr1024@gmail.com>
2020-06-24 08:21:43 -0400

    Pumice quenching can now affect water too
    Face-touching water can be converted to pumice instead of the
    flowing lava (e.g. lava intrudes into that space and quenches
    instead) creating even more of a mess.

--- mods/nc_igneous/pumice.lua
+++ mods/nc_igneous/pumice.lua
@@ -1,6 +1,6 @@
 -- LUALOCALS < ---------------------------------------------------------
-local math, minetest, nodecore
-    = math, minetest, nodecore
+local math, minetest, nodecore, vector
+    = math, minetest, nodecore, vector
 local math_random
     = math.random
 -- LUALOCALS > ---------------------------------------------------------
@@ -41,16 +41,26 @@ pumdef = {
 }
 minetest.register_node(pumname, pumdef)
 
-nodecore.register_limited_abm({
-		label = "lava pumice",
-		interval = 1,
-		chance = 2,
-		nodenames = {"nc_terrain:lava_flowing"},
-		neighbors = {"group:coolant"},
-		action = function(pos)
-			return nodecore.set_loud(pos, {name = pumname})
-		end
-	})
+do
+	local dirs = nodecore.dirs()
+	nodecore.register_limited_abm({
+			label = "lava pumice",
+			interval = 1,
+			chance = 2,
+			nodenames = {"nc_terrain:lava_flowing"},
+			neighbors = {"group:coolant"},
+			action = function(pos)
+				if math_random() < 0.5 then
+					local p = vector.add(pos, dirs[math_random(1, #dirs)])
+					local n = minetest.get_node(p)
+					if minetest.get_item_group(n.name, "water") > 0 then
+						return nodecore.set_loud(p, {name = pumname})
+					end
+				end
+				return nodecore.set_loud(pos, {name = pumname})
+			end
+		})
+end
 
 nodecore.register_limited_abm({
 		label = "pumice melt",
