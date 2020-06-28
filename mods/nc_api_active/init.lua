0eee06c8ea06a434563c2f4fb28487efad63d899
Aaron Suen <warr1024@gmail.com>
2020-06-27 21:01:23 -0400

    Officially retire the ABM laundering hack
    Never saw any empirical evidence that this
    helped one bit.

--- mods/nc_api_active/init.lua
+++ mods/nc_api_active/init.lua
@@ -1,13 +1,14 @@
 -- LUALOCALS < ---------------------------------------------------------
-local include, nodecore
-    = include, nodecore
+local include, minetest, nodecore
+    = include, minetest, nodecore
 -- LUALOCALS > ---------------------------------------------------------
 
 nodecore.amcoremod()
 
+nodecore.register_limited_abm = function(...) return minetest.register_abm(...) end
+
 include("abmmux")
 include("stasis")
-include("abmhack")
 include("dnts")
 include("aism")
 include("soaking")
