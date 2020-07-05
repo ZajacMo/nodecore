e12047af6438b044ebd36195785a9edaf65589fe
Aaron Suen <warr1024@gmail.com>
2020-07-05 14:12:18 -0400

    Freeze all animations in screenshot world
    Now we should be able to compose an exactly
    matching screenshot each time.

---
+++ docs/screenshot-world/worldmods/noanim/init.lua
@@ -0,0 +1,23 @@
+-- LUALOCALS < ---------------------------------------------------------
+local minetest, nodecore, pairs, rawset, type
+    = minetest, nodecore, pairs, rawset, type
+-- LUALOCALS > ---------------------------------------------------------
+
+local function noanim(t)
+	if type(t) ~= "table" then return t end
+	if t.animation then
+		rawset(t.animation, "length", 1/0)
+	end
+	for _, v in pairs(t) do noanim(v) end
+	return t
+end
+
+for _, v in pairs(minetest.registered_nodes) do
+	noanim(v.tiles)
+	noanim(v.special_tiles)
+end
+
+nodecore.register_on_register_item(function(_, def)
+		noanim(def.tiles)
+		noanim(def.special_tiles)
+	end)
