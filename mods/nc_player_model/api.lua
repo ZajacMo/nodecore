8074e0a6399acd616da90132646b37f92f98829c
Aaron Suen <warr1024@gmail.com>
2020-07-05 17:52:14 -0400

    Enable backface culling for players

--- mods/nc_player_model/api.lua
+++ mods/nc_player_model/api.lua
@@ -101,6 +101,7 @@ nodecore.player_visuals_base = nodecore.player_visuals_base or function(player)
 	return {
 		visual = "mesh",
 		visual_size = {x = 0.9, y = 0.9, z = 0.9},
-		mesh = mesh and mesh ~= "" and mesh or modname .. ".b3d"
+		mesh = mesh and mesh ~= "" and mesh or modname .. ".b3d",
+		backface_culling = true
 	}
 end
