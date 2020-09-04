3b64a3ecc4eedbe19ffa692e8e3275a3a95e4c8d
Aaron Suen <warr1024@gmail.com>
2020-09-04 13:16:41 -0400

    Aux+sneak+drop = drop all matching

--- mods/nc_player_pickup/dropall.lua
+++ mods/nc_player_pickup/dropall.lua
@@ -1,10 +1,10 @@
 -- LUALOCALS < ---------------------------------------------------------
-local minetest
-    = minetest
+local minetest, nodecore
+    = minetest, nodecore
 -- LUALOCALS > ---------------------------------------------------------
 
 local droppingall = {}
-local function dropall(pname, pos)
+local function dropall(pname, pos, matching)
 	local player = minetest.get_player_by_name(pname)
 	if not player then return end
 	if droppingall[pname] then return end
@@ -12,7 +12,8 @@ local function dropall(pname, pos)
 	local inv = player:get_inventory()
 	for i = 1, inv:get_size("main") do
 		local stack = inv:get_stack("main", i)
-		if not stack:is_empty() then
+		if not (stack:is_empty() or nodecore.item_is_virtual(stack)
+			or matching and stack:get_name() ~= matching) then
 			stack = minetest.item_drop(stack, player, pos)
 			inv:set_stack("main", i, stack)
 		end
@@ -22,9 +23,11 @@ end
 
 local olddrop = minetest.item_drop
 function minetest.item_drop(item, player, ...)
-	if player:get_player_control().aux1 then
+	local pctl = player:get_player_control()
+	if pctl.aux1 then
 		local pname = player:get_player_name()
-		minetest.after(0, dropall, pname, player:get_pos())
+		minetest.after(0, dropall, pname, player:get_pos(),
+			pctl.sneak and player:get_wielded_item():get_name())
 	end
 	return olddrop(item, player, ...)
 end
