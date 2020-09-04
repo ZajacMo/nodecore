7eb65578702eb344baab7d14a6376785086c5aed
Aaron Suen <warr1024@gmail.com>
2020-09-03 21:24:59 -0400

    Genericized brickmaking API
    You can now chisel and bond bricks of other
    smooth stone types, i.e. the kind made in
    concrete processes.

--- mods/nc_stonework/bricks.lua
+++ mods/nc_stonework/bricks.lua
@@ -5,60 +5,69 @@ local minetest, nodecore
 
 local modname = minetest.get_current_modname()
 
-minetest.register_node(modname .. ":bricks", {
-		description = "Stone Bricks",
-		tiles = {"nc_terrain_stone.png^" .. modname .. "_bricks.png"},
-		groups = {
-			stone = 1,
-			rock = 1,
-			cracky = 2,
+function nodecore.register_stone_bricks(name, desc, tile, madefrom, groups, bonded)
+	groups = nodecore.underride(groups, {
 			falling_node = 1
-		},
-		crush_damage = 2,
-		sounds = nodecore.sounds("nc_terrain_stony")
-	})
+		})
+	minetest.register_node(":" .. modname .. ":bricks_" .. name, {
+			description = desc .. " Bricks",
+			tiles = {tile .. "^" .. modname .. "_bricks.png"},
+			groups = groups,
+			crush_damage = 2,
+			sounds = nodecore.sounds("nc_terrain_stony")
+		})
 
-nodecore.register_craft({
-		label = "chisel stone into bricks",
-		action = "pummel",
-		toolgroups = {thumpy = 3},
-		normal = {y = 1},
-		indexkeys = {"group:chisel"},
-		nodes = {
-			{
-				match = {
-					metal_temper_cool = true,
-					groups = {chisel = true}
+	nodecore.register_craft({
+			label = "chisel " .. name .. " bricks",
+			action = "pummel",
+			toolgroups = {thumpy = 3},
+			normal = {y = 1},
+			indexkeys = {"group:chisel"},
+			nodes = {
+				{
+					match = {
+						metal_temper_cool = true,
+						groups = {chisel = true}
+					},
+					dig = true
 				},
-				dig = true
-			},
-			{
-				y = -1,
-				match = {groups = {smoothstone = true}},
-				replace = modname .. ":bricks"
+				{
+					y = -1,
+					match = madefrom,
+					replace = modname .. ":bricks_" .. name
+				}
 			}
-		}
-	})
+		})
 
-minetest.register_node(modname .. ":bricks_bonded", {
-		description = "Bonded Stone Bricks",
-		tiles = {"nc_terrain_stone.png^(" .. modname .. "_bricks.png^[opacity:128)"},
-		groups = {
-			stone = 1,
-			rock = 1,
-			cracky = 3
-		},
-		crush_damage = 2,
-		sounds = nodecore.sounds("nc_terrain_stony")
-	})
+	bonded = nodecore.underride(bonded, groups)
+	bonded.falling_node = nil
+	minetest.register_node(":" .. modname .. ":bricks_" .. name .. "_bonded", {
+			description = "Bonded " .. desc .. " Bricks",
+			tiles = {tile .. "^(" .. modname .. "_bricks.png^[opacity:128)"},
+			groups = bonded,
+			crush_damage = 2,
+			sounds = nodecore.sounds("nc_terrain_stony")
+		})
 
-nodecore.register_limited_abm({
-		label = "bond stone bricks",
-		nodenames = {modname .. ":bricks"},
-		neighbors = {"group:concrete_wet"},
-		interval = 1,
-		chance = 2,
-		action = function(pos)
-			nodecore.set_loud(pos, {name = modname .. ":bricks_bonded"})
-		end
-	})
+	nodecore.register_limited_abm({
+			label = "bond " .. name .. " bricks",
+			nodenames = {modname .. ":bricks_" .. name},
+			neighbors = {"group:concrete_wet"},
+			interval = 1,
+			chance = 2,
+			action = function(pos)
+				nodecore.set_loud(pos, {name = modname .. ":bricks_"
+						.. name .. "_bonded"})
+			end
+		})
+end
+
+nodecore.register_stone_bricks("stone", "Stone",
+	"nc_terrain_stone.png",
+	{groups = {smoothstone = true}},
+	{stone = 1, rock = 1, cracky = 2},
+	{cracky = 3}
+)
+
+minetest.register_alias(modname .. ":bricks", modname .. ":bricks_stone")
+minetest.register_alias(modname .. ":bricks_bonded", modname .. ":bricks_stone_bonded")
