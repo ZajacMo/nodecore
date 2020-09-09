0a8583d4a5fc07a0c4a93195a43e5865d7bb7d82
Aaron Suen <warr1024@gmail.com>
2020-09-09 09:56:50 -0400

    Fix on_timer trap hook

--- mods/nc_api_active/dnts.lua
+++ mods/nc_api_active/dnts.lua
@@ -136,7 +136,7 @@ minetest.nodedef_default.on_timer = function(pos)
 	return save()
 end
 
-nodecore.register_on_register_item(function(def)
+nodecore.register_on_register_item(function(_, def)
 		if def.on_timer then
 			return error("on_timer hook is disallowed in "
 				.. nodecore.product .. "; use DNT instead")
