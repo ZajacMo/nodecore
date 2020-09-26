3b864e5330859050bf701217f74e536ac46367bd
Aaron Suen <warr1024@gmail.com>
2020-09-26 15:33:23 -0400

    Fix and cache hint key expansion

--- mods/nc_api_hints/state.lua
+++ mods/nc_api_hints/state.lua
@@ -1,9 +1,52 @@
 -- LUALOCALS < ---------------------------------------------------------
 local ipairs, minetest, nodecore, pairs, string
     = ipairs, minetest, nodecore, pairs, string
-local string_gsub, string_match
-    = string.gsub, string.match
+local string_gsub, string_match, string_sub
+    = string.gsub, string.match, string.sub
 -- LUALOCALS > ---------------------------------------------------------
+
+local expandcache = {}
+local function expandkey(k)
+	local keys = expandcache[k]
+	if keys then return keys end
+
+	keys = {}
+	keys[k] = true
+	for name, def in pairs(minetest.registered_items) do
+		if string_sub(k, -#name) == name then
+			local pref = string_sub(k, 1, #k - #name)
+			for g in pairs(def.groups or {}) do
+				keys[pref .. "group:" .. g] = true
+			end
+			if def.tool_capabilities and def.tool_capabilities.groupcaps then
+				for gn, gv in pairs(def.tool_capabilities.groupcaps) do
+					for gt, gtv in pairs(gv.times or {}) do
+						if gtv <= 4 then
+							keys[pref .. "toolcap:" .. gn] = true
+							keys[pref .. "toolcap:" .. gn
+							.. ":" .. gt] = true
+						end
+					end
+				end
+			end
+		end
+	end
+
+	local rawkeys = keys
+	keys = {}
+	for r in pairs(rawkeys) do
+		keys[r] = true
+		while string_match(r, ":") do
+			r = string_gsub(r, "^[^:]*:", "")
+			keys[r] = true
+		end
+	end
+
+	local t = {}
+	for x in pairs(keys) do t[#t + 1] = x end
+	expandcache[k] = t
+	return t
+end
 
 function nodecore.hint_state(pspec)
 	local rawdb, player, pname = nodecore.get_player_discovered(pspec)
@@ -11,25 +54,8 @@ function nodecore.hint_state(pspec)
 
 	local db = {}
 	for k in pairs(rawdb) do
-		db[k] = true
-		while string_match(k, ":") do
-			k = string_gsub(k, "^[^:]*:", "")
-			db[k] = true
-		end
-	end
-	for k, v in pairs(minetest.registered_items) do
-		if db[k] then
-			if v.tool_capabilities and v.tool_capabilities.groupcaps then
-				for gn, gv in pairs(v.tool_capabilities.groupcaps) do
-					for gt in pairs(gv.times or {}) do
-						db["toolcap:" .. gn .. ":" .. gt] = true
-					end
-				end
-			end
-			for gn, gv in pairs(v.groups or {}) do
-				db["group:" .. gn] = gv
-			end
-		end
+		local exp = expandkey(k)
+		for i = 1, #exp do db[exp[i]] = true end
 	end
 
 	local done = {}
