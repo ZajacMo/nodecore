caf1de7c563f5f8f2bb631f61b255d1380880fea
Aaron Suen <warr1024@gmail.com>
2020-09-27 09:28:49 -0400

    Expand on_register_item API, add retroactivity
    - Pass a def table instead of a function
    - Retroactive defs will also scan over all existing
      definitions and have an opportunity to modify them
      at time of registration.

--- mods/nc_api/item_on_register.lua
+++ mods/nc_api/item_on_register.lua
@@ -1,16 +1,32 @@
 -- LUALOCALS < ---------------------------------------------------------
-local ipairs, minetest, nodecore
-    = ipairs, minetest, nodecore
+local ipairs, minetest, nodecore, pairs, rawset, setmetatable, type
+    = ipairs, minetest, nodecore, pairs, rawset, setmetatable, type
 -- LUALOCALS > ---------------------------------------------------------
 
-nodecore.register_on_register_item,
-nodecore.registered_on_register_item
-= nodecore.mkreg()
+local regs = {}
+nodecore.registered_on_register_item = regs
+
+function nodecore.register_on_register_item(def)
+	if type(def) == "function" then def = {func = def} end
+	regs[#regs + 1] = def
+	if def.retroactive then
+		for name, itemdef in pairs(minetest.registered_nodes) do
+			local t = {}
+			setmetatable(t, {
+					__index = itemdef,
+					__newindex = function(_, k, v)
+						return rawset(itemdef, k, v)
+					end
+				})
+			def.func(name, itemdef)
+		end
+	end
+end
 
 local oldreg = minetest.register_item
 function minetest.register_item(name, def, ...)
 	for _, v in ipairs(nodecore.registered_on_register_item) do
-		local x = v(name, def, ...)
+		local x = v.func(name, def, ...)
 		if x then return x end
 	end
 	return oldreg(name, def, ...)
