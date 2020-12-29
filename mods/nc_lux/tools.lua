e1dc6327792ad4cd38ee7484b4f3d814b35a6f23
Aaron Suen <warr1024@gmail.com>
2020-12-28 20:54:57 -0500

    New lux tool infusion API
    - Split infusion logic from tool registration
      so tools aren't depending on sharing local
      tables with infusion logic.
    - Use alternative_lux_* fields on tool item
      registrations to control how lux infusion and
      boost/unboost applies to tools.
    - It should now be possible to register tools
      independently that can tap into lux infusion
      and boost features.
    - It should now be possible to make tools
      boostable, unboostable, and infusable all
      independently, so they don't need to support
      all those actions, and they don't need to
      round-trip from e.g. a boost/unboost operation.
    - We no longer depend on a naming convention
      for boost/unboost pairs (though in practice we
      follow it in the core game, but mods are not
      required to).

--- mods/nc_lux/tools.lua
+++ mods/nc_lux/tools.lua
@@ -1,16 +1,9 @@
 -- LUALOCALS < ---------------------------------------------------------
-local ItemStack, math, minetest, nodecore, pairs, vector
-    = ItemStack, math, minetest, nodecore, pairs, vector
-local math_ceil, math_exp, math_log
-    = math.ceil, math.exp, math.log
+local minetest, nodecore, pairs
+    = minetest, nodecore, pairs
 -- LUALOCALS > ---------------------------------------------------------
 
 local modname = minetest.get_current_modname()
-
-local convert = {}
-local charge = {}
-
-local boost_suff = "_boost"
 
 local function mktool(tshape, buffs)
 	buffs = buffs or {}
@@ -57,14 +50,14 @@ local function mktool(tshape, buffs)
 		boost.groups = nodecore.underride({lux_tool = 1, lux_emit = 2}, def.groups)
 
 		def.name = modname .. ":" .. tshape .. "_" .. temper
+		boost.name = modname .. ":" .. tshape .. "_" .. temper .. "_boost"
+
+		def.alternative_lux_boosted = boost.name
+		boost.alternative_lux_unboosted = def.name
+
+		minetest.override_item(orig.name, {alternative_lux_infused = def.name})
 		minetest.register_tool(def.name, def)
-
-		boost.name = modname .. ":" .. tshape .. "_" .. temper .. boost_suff
 		minetest.register_tool(boost.name, boost)
-
-		convert[orig.name] = def.name
-		charge[def.name] = true
-		charge[boost.name] = true
 	end
 end
 for _, shape in pairs({"mallet", "spade", "hatchet", "pick", "mattock"}) do
@@ -72,85 +65,3 @@ for _, shape in pairs({"mallet", "spade", "hatchet", "pick", "mattock"}) do
 end
 mktool("adze")
 mktool("rake", {snappy = 3, crumbly = 3, uses = 5})
-
-local alltools = {}
-for k in pairs(convert) do alltools[#alltools + 1] = k end
-for k in pairs(charge) do
-	if not convert[k] then
-		alltools[#alltools + 1] = k
-	end
-end
-
-local ratefactor = 20000
-nodecore.register_soaking_aism({
-		label = "lux infuse",
-		fieldname = "infuse",
-		interval = 2,
-		chance = 1,
-		itemnames = alltools,
-		soakrate = function(stack, aismdata)
-			local name = stack:get_name()
-			if (not charge[name]) and (not convert[name]) then return false end
-
-			local pos = aismdata.pos or aismdata.player and aismdata.player:get_pos()
-			return nodecore.lux_soak_rate(pos)
-		end,
-		soakcheck = function(data, stack)
-			local name = stack:get_name()
-			if convert[name] and stack:get_wear() < 3277 then
-				stack = ItemStack(convert[name])
-				stack:set_wear(65535)
-				return data.total, stack
-			end
-			if not charge[name] then return data.total, stack end
-			local wear = stack:get_wear()
-			local newear = math_ceil(wear * math_exp(-data.total / ratefactor))
-			if newear == wear then return data.total, stack end
-			if newear < 1 then return 1 end
-			local used = math_log(wear / newear) * ratefactor
-			stack:set_wear(newear)
-			return data.total - used, stack
-		end
-	})
-
-nodecore.register_aism({
-		label = "lux boost",
-		interval = 2,
-		chance = 1,
-		itemnames = {"group:lux_tool"},
-		action = function(stack, data)
-			local name = stack:get_name()
-			local boosted = name:sub(-#boost_suff) == boost_suff
-			local boost = #nodecore.find_nodes_around(data.pos, "group:lux_fluid", 2) > 0
-			if boost == boosted then return end
-
-			if boost and not boosted then
-				name = name .. boost_suff
-			else
-				name = name:sub(1, -1 - #boost_suff)
-			end
-			if not minetest.registered_items[name] then return end
-			stack:set_name(name)
-			return stack
-		end
-	})
-
-nodecore.register_aism({
-		label = "lux diffuse in water",
-		interval = 2,
-		chance = 1,
-		itemnames = {"group:lux_tool"},
-		action = function(stack, data)
-			if not data.pos then return end
-			local qty = #nodecore.find_nodes_around(data.pos, "group:water")
-			if qty < 1 then return end
-			if data.player then
-				qty = qty * (1 + vector.length(
-						data.player:get_player_velocity()) / 5)
-			end
-			local dur = 65535 - stack:get_wear()
-			dur = dur * 0.9998 ^ qty
-			stack:set_wear(65535 - dur)
-			return stack
-		end
-	})
