05f43d6dd65a7d1f1c7836b3f33e4a836e5e680c
Aaron Suen <warr1024@gmail.com>
2021-02-26 09:42:33 -0500

    Officially drop 5.2 support
    5.4 has been released, so we now support
    only 5.3 and 5.4 (and hypothetical future
    versions).

--- .cdbrelease.lua
+++ .cdbrelease.lua
@@ -13,7 +13,7 @@ readbinary = readbinary or function() end
 return {
 	user = "Warr1024",
 	pkg = alpha and "nodecore_alpha" or "nodecore",
-	min = "5.2",
+	min = "5.3",
 	version = dofile("./mods/nc_api/version.lua"),
 	path = ".",
 	type = "game",
