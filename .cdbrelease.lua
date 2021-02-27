efdf54d3011e8f1e1fb6bae915d30f791728f12f
Aaron Suen <warr1024@gmail.com>
2021-02-27 10:01:03 -0500

    Migrate CDB metadata to new cdbrelease format
    - Move static stuff into .cdb.json
    - Use new official API key names

--- .cdbrelease.lua
+++ .cdbrelease.lua
@@ -11,25 +11,17 @@ readtext = readtext or function() end
 readbinary = readbinary or function() end
 
 return {
-	user = "Warr1024",
 	pkg = alpha and "nodecore_alpha" or "nodecore",
 	version = dofile("./mods/nc_api/version.lua"),
 	path = ".",
-	type = "game",
 	title = "NodeCore" .. (alpha and " ALPHA" or ""),
-	short_desc = (alpha
+	short_description = (alpha
 		and "Experimental early-access release of NodeCore."
 		or "Original, immersive puzzle/adventure game with NO popup GUIs, minimal HUDs."),
-	tags = "building, crafting, environment, inventory, machines, player vs env, puzzle",
-	license = "mit",
-	desc = alpha and readtext('.cdb-alpha.md') or readtext('.cdb-release.md'),
-	repo = "https://gitlab.com/sztest/nodecore",
-	website = "https://nodecore.mine.nu",
-	issueTracker = "https://discord.gg/NNYeF6f",
-	forums = "https://forum.minetest.net/viewtopic.php?f=15&t=24857",
-	screenshots = alpha
-	and {readbinary('.cdb-alpha.jpg'), readbinary('.cdb-release.jpg')}
-	or {readbinary('.cdb-release.jpg')}
+	long_description = alpha and readtext('.cdb-alpha.md') or readtext('.cdb-release.md'),
+	screenshots = (alpha
+		and {readbinary('.cdb-alpha.jpg'), readbinary('.cdb-release.jpg')}
+		or {readbinary('.cdb-release.jpg')})
 }
 
 -- luacheck: pop
