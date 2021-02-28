f91178f96b3f5772901ef484183555488429cf8e
Aaron Suen <warr1024@gmail.com>
2021-02-28 08:15:15 -0500

    Rebuild package metadata from template
    Consolidate back into .cdbrelease.lua, and fix any
    typos or field name issues for new stricter
    cdbrelease option parsing.

--- .cdbrelease.lua
+++ .cdbrelease.lua
@@ -6,19 +6,39 @@ local dofile
 -- luacheck: push
 -- luacheck: globals config readtext readbinary
 
-local alpha = config and config.branch == "dev"
 readtext = readtext or function() end
 readbinary = readbinary or function() end
+
+local alpha = config and config.branch == "dev"
 
 return {
 	pkg = alpha and "nodecore_alpha" or "nodecore",
 	version = dofile("./mods/nc_api/version.lua"),
-	path = ".",
+	type = "mod",
 	title = "NodeCore" .. (alpha and " ALPHA" or ""),
 	short_description = (alpha
 		and "Experimental early-access release of NodeCore."
 		or "Original, immersive puzzle/adventure game with NO popup GUIs, minimal HUDs."),
+	tags = {
+		"building",
+		"crafting",
+		"environment",
+		"inventory",
+		"oneofakind__original",
+		"player_effects",
+		"puzzle",
+		"pve",
+		"technology"
+	},
+	content_warnings = {},
+	license = "MIT",
+	media_license = "MIT",
 	long_description = alpha and readtext('.cdb-alpha.md') or readtext('.cdb-release.md'),
+	repo = "https://gitlab.com/sztest/nodecore",
+	website = "https://nodecore.mine.nu",
+	issue_tracker = "https://discord.gg/NNYeF6f",
+	forums = 24857,
+	maintainers = {"Warr1024"},
 	screenshots = (alpha
 		and {readbinary('.cdb-alpha.jpg'), readbinary('.cdb-release.jpg')}
 		or {readbinary('.cdb-release.jpg')})
