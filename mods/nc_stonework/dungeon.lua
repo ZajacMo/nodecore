b67a9849676d46e265657fdb14b8dd2a860fa47d
Aaron Suen <warr1024@gmail.com>
2021-03-05 21:40:48 -0500

    Reduce dungeon brickiness
    Bricks are a valuable resource, so we don't
    want to give too many to the player just
    for finding a dungeon.

--- mods/nc_stonework/dungeon.lua
+++ mods/nc_stonework/dungeon.lua
@@ -12,12 +12,12 @@ nodecore.register_dungeongen({
 		label = "dungeon concrete/bricks",
 		func = function(pos)
 			local rng = nodecore.seeded_rng(mapperlin:get_3d(pos))
-			if rng(1, 2) == 1 then return end
+			if rng(1, 4) ~= 1 then return end
 			local below = {x = pos.x, y = pos.y - 1, z = pos.z}
 			local bnode = minetest.get_node(below)
 			local bdef = minetest.registered_nodes[bnode.name] or {}
 			return minetest.set_node(pos, {name = modname
-					.. (rng(1, 10) <= (bdef.walkable and 9 or 1)
+					.. (rng(1, 10) <= (bdef.walkable and 9 or 2)
 						and ":bricks_stone"
 						or ":bricks_stone_bonded")})
 		end
