d949430a6579c213cc39086605164ac50dce613a
Aaron Suen <warr1024@gmail.com>
2021-03-20 08:08:05 -0400

    Make protection exemption exemptable
    So nc_exmachina and other downstream
    mods can override the protection override
    for item stacks too.

--- mods/nc_items/protect.lua
+++ mods/nc_items/protect.lua
@@ -1,15 +1,19 @@
 -- LUALOCALS < ---------------------------------------------------------
-local minetest
-    = minetest
+local minetest, nodecore
+    = minetest, nodecore
 -- LUALOCALS > ---------------------------------------------------------
 
 local modname = minetest.get_current_modname()
 
+function nodecore.protection_exempt(pos)
+	local node = minetest.get_node(pos)
+	return node.name == modname .. ":stack"
+end
+
 minetest.after(0, function()
 		local prot = minetest.is_protected
 		function minetest.is_protected(pos, name, ...)
-			local node = minetest.get_node(pos)
-			if node.name == modname .. ":stack" then return false end
+			if nodecore.protection_exempt(pos, name) then return false end
 			return prot(pos, name, ...)
 		end
 	end)
