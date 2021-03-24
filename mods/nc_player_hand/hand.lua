405833bf32dd0e501c2f79b4728ebe2478569371
Aaron Suen <warr1024@gmail.com>
2021-03-24 09:36:04 -0400

    Remove emergency digging
    This was added to allow a player to get out of a
    rare softlock (trapped inside a cobble room without
    any tools).  Since then we have not seen much use
    of it for this intended purpose.
    
    However, it has caused MUCH confusion among new
    players, wondering why it takes so long to mine
    dirt or tree trunks, instead of trying to figure out how
    to make the proper tool.
    
    Emergency digging is causing more problems than
    it's solving and thus it makes more sense to remove
    it than to keep it.  If we need some mechanic to fill
    its niche (i.e. a way out of softlock) then we will need
    to consider something that's harder to discover
    accidentally to avoid confusion.

--- mods/nc_player_hand/hand.lua
+++ mods/nc_player_hand/hand.lua
@@ -1,8 +1,6 @@
 -- LUALOCALS < ---------------------------------------------------------
-local math, minetest, nodecore, pairs
-    = math, minetest, nodecore, pairs
-local math_pow
-    = math.pow
+local minetest, nodecore
+    = minetest, nodecore
 -- LUALOCALS > ---------------------------------------------------------
 
 local modname = minetest.get_current_modname()
@@ -13,13 +11,6 @@ local toolcaps = nodecore.toolcaps({
 		snappy = 1,
 		thumpy = 1
 	})
-local gcaps = toolcaps.groupcaps
-for k, v in pairs(nodecore.tool_basetimes) do
-	gcaps[k] = gcaps[k] or {uses = 0, times = {}}
-	for n = 1, 100 do
-		gcaps[k].times[n] = gcaps[k].times[n] or (10 * v * math_pow(2, n))
-	end
-end
 
 minetest.register_item(":", {
 		["type"] = "none",
