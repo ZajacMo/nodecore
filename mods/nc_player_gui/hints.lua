c88f439b873b57339bcfd054bdab80a69f52c860
Aaron Suen <warr1024@gmail.com>
2021-04-02 22:35:45 -0400

    Full scrolling hint list
    Since we switched to a textarea and have proper
    word wrap and scrollbars, we might as well use the
    space and let players see ALL their hints.

--- mods/nc_player_gui/hints.lua
+++ mods/nc_player_gui/hints.lua
@@ -1,15 +1,13 @@
 -- LUALOCALS < ---------------------------------------------------------
 local math, minetest, nodecore, pairs, table
     = math, minetest, nodecore, pairs, table
-local math_floor, math_random, table_remove, table_sort
-    = math.floor, math.random, table.remove, table.sort
+local math_floor, table_insert, table_sort
+    = math.floor, table.insert, table.sort
 -- LUALOCALS > ---------------------------------------------------------
 
 local pcache = {}
 
 local strings = {
-	onemore = "(and 1 more hint)",
-	fewmore = "(and @1 more hints)",
 	progress = "Progress: @1 complete, @2 current, @3 future",
 	explore = "Not all game content is covered by hints. Explore!",
 	hint = "- @1",
@@ -30,29 +28,19 @@ local function gethint(player)
 
 	local found, done = nodecore.hint_state(pname)
 	for k, v in pairs(found) do found[k] = strings.hint(v.text) end
+	table_sort(found)
 	for k, v in pairs(done) do done[k] = strings.done(v.text) end
+	table_sort(done)
 
 	local prog = #found
 	local left = #(nodecore.hints) - prog - #done
 
-	while #found > 5 do
-		table_remove(found, math_random(1, #found))
-	end
-	while #found < 5 and #done > 0 do
-		local j = math_random(1, #done)
-		found[#found + 1] = done[j]
-		table_remove(done, j)
-	end
-	table_sort(found)
-	if #found == (prog - 1) then
-		found[#found + 1] = strings.onemore()
-	elseif #found < prog then
-		found[#found + 1] = strings.fewmore(prog - #found)
-	end
-
+	table_insert(found, 1, "")
+	table_insert(found, 1, strings.progress(#done, prog, left))
 	found[#found + 1] = ""
-	found[#found + 1] = strings.progress(#done, prog, left)
 	found[#found + 1] = strings.explore()
+	found[#found + 1] = ""
+	for i = 1, #done do found[#found + 1] = done[i] end
 
 	pcache[pname] = {time = now, found = found}
 	return found
