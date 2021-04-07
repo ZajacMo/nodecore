1e64c191a0917ee6ddb2f43d27a675e4b9aa9168
Aaron Suen <warr1024@gmail.com>
2021-04-06 22:24:05 -0400

    drop_in_place rework
    - Apply logic as first-class support so we can still
      have custom after_dig_node hooks.
    - Shared flag nodecore.silktouch_digging that can
      be used in after_dig_node callbacks to determine
      if we're in the middle of a silk-touch dig.
    - Add coal particles to tarstone digging if the coal
      is destroyed/dispersed during the dig operation.

--- mods/nc_api/item_drop_in_place.lua
+++ mods/nc_api/item_drop_in_place.lua
@@ -13,45 +13,72 @@ local function yielditem(pos, digger, stack)
 		stack = digger:get_inventory():add_item("main", stack)
 	end
 	if stack:is_empty() then return end
+	minetest.chat_send_all(stack:to_string())
 	return nodecore.item_eject(pos, stack)
 end
 
-nodecore.register_on_register_item(function(_, def)
-		if def.type ~= "node" then return end
-		local dip = def.drop_in_place
-		if dip then
-			if type(dip) ~= "table" then dip = {name = dip} end
-			def.drop_non_silktouch = def.drop_non_silktouch
-			or def.drop ~= "" and def.drop
-			def.drop = ""
-			def.node_dig_prediction = def.node_dig_prediction or dip.name
-			local st = def.silktouch
-			if st == nil then
-				st = {}
-				for k, v in pairs(def.groups or {}) do
-					st[k] = v + 5
-				end
+local olddig = minetest.node_dig
+function minetest.node_dig(pos, node, digger, ...)
+	local def = node and node.name and minetest.registered_nodes[node.name]
+	if def and def.drop_in_place then
+		local oldrm = minetest.remove_node
+		local function helper(...)
+			minetest.remove_node = oldrm
+			nodecore.silktouch_digging = nil
+			return ...
+		end
+		function minetest.remove_node(p2, ...)
+			if not vector.equals(pos, p2) then return oldrm(p2, ...) end
+			minetest.remove_node = oldrm
+
+			local tool = digger and digger:is_player()
+			and digger:get_wielded_item()
+			or nodecore.machine_digging
+			and vector.equals(nodecore.machine_digging.pos, pos)
+			and nodecore.machine_digging.tool
+
+			nodecore.machine_digging = nil
+
+			if def.silktouch and digger and nodecore.tool_digs(tool,
+				def.silktouch) then
+				nodecore.silktouch_digging = true
+				yielditem(pos, digger, ItemStack(node.name))
+				return oldrm(p2, ...)
 			end
-			def.after_dig_node = def.after_dig_node or function(pos, node, _, digger)
-				local tool = digger and digger:is_player()
-				and digger:get_wielded_item()
-				or nodecore.machine_digging
-				and vector.equals(nodecore.machine_digging.pos, pos)
-				and nodecore.machine_digging.tool
 
-				nodecore.machine_digging = nil
+			if def.drop_non_silktouch then
+				return yielditem(pos, digger,
+					ItemStack(def.drop_non_silktouch))
+			end
 
-				if st and digger and nodecore.tool_digs(tool, st) then
-					return yielditem(pos, digger, ItemStack(node.name))
-				end
+			return minetest.set_node(pos, {
+					name = def.drop_in_place.name,
+					param = def.drop_in_place.param or node.param,
+					param2 = def.drop_in_place.param2 or node.param2
+				})
+		end
+		return helper(olddig(pos, node, digger, ...))
+	end
+	return olddig(pos, node, digger, ...)
+end
 
-				dip.param2 = node.param2
-				minetest.set_node(pos, dip)
+nodecore.register_on_register_item(function(_, def)
+		if def.type ~= "node" or not def.drop_in_place then return end
 
-				if def.drop_non_silktouch then
-					return yielditem(pos, digger,
-						ItemStack(def.drop_non_silktouch))
-				end
+		if type(def.drop_in_place) ~= "table" then
+			def.drop_in_place = {name = def.drop_in_place}
+		end
+
+		def.drop_non_silktouch = def.drop_non_silktouch
+		or def.drop ~= "" and def.drop
+		def.drop = ""
+
+		def.node_dig_prediction = def.node_dig_prediction or def.drop_in_place.name
+
+		if def.silktouch == nil then
+			def.silktouch = {}
+			for k, v in pairs(def.groups or {}) do
+				def.silktouch[k] = v + 5
 			end
 		end
 	end)
