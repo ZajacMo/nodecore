f76fd96d6fd23b8bdef5fb1e38d8cd04f2fb3ca9
Aaron Suen <warr1024@gmail.com>
2021-06-17 16:47:52 -0400

    Russian translation re-completed by Arekusei
    Also:
    - Catch errors in translation update scripts
    - Accept a handful other anonymous suggestions

--- mods/nc_api/locale/src/update.pl
+++ mods/nc_api/locale/src/update.pl
@@ -6,7 +6,7 @@ use JSON qw(from_json);
 sub curl {
 	my @cmd = ("curl");
 	$ENV{NC_WEBLATE_TOKEN} and
-	  push @cmd, "-H", "Authorization: Token $ENV{NC_WEBLATE_TOKEN}";
+	  push @cmd, "-f", "-H", "Authorization: Token $ENV{NC_WEBLATE_TOKEN}";
 	push @cmd, @_;
 	open(my $fh, "-|", @cmd) or die($!);
 	return $fh;
