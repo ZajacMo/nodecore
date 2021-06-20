5344012ce9196620a40904d3964d2aaf30d826d0
Aaron Suen <warr1024@gmail.com>
2021-06-20 10:13:10 -0400

    Expand player guide
    - Break into more sections
    - Add more content
    - Enlarge layout

--- mods/nc_player_gui/guide.lua
+++ mods/nc_player_gui/guide.lua
@@ -11,11 +11,23 @@ nodecore.register_inventory_tab({
 			"- There is NO inventory screen.",
 			"- Drop items onto ground to create stack nodes. They do not decay.",
 			"- Sneak+drop to count out single items from stack.",
+			"- Aux+drop any item to drop everything.",
+			"- Sneak+aux+drop an item to drop all matching items.",
 			"- Items picked up try to fit into the current selected slot first.",
+			"- Drop and pick up items to rearrange your inventory."
+		}
+	})
+
+nodecore.register_inventory_tab({
+		title = "Crafting",
+		content = {
+			"Player's Guide: Crafting",
+			"",
 			"- Crafting is done by building recipes in-world.",
 			"- Order and specific face of placement may matter for crafting.",
 			"- Some recipes use a 3x3 \"grid\", laid out flat on the ground.",
-			"- Larger recipes are usually more symmetrical."
+			"- Larger recipes are usually more symmetrical.",
+			"- For larger recipes, the center item is usually placed last."
 		}
 	})
 
@@ -35,12 +47,24 @@ nodecore.register_inventory_tab({
 	})
 
 nodecore.register_inventory_tab({
+		title = "Movement",
+		content = {
+			"Player's Guide: Movement and Navigation",
+			"",
+			"- To run faster, walk/swim forward or climb/swim upward continuously.",
+			"- Hold/repeat right-click on walls/ceilings barehanded to climb.",
+			"- Climbing spots also produce very faint light; raise display gamma to see.",
+			"- Climbing spots may be climbed once black particles appear.",
+			"- Be wary of dark caves/chasms; you are responsible for getting yourself out."
+		}
+	})
+
+nodecore.register_inventory_tab({
 		title = "Tips",
 		content = {
 			"Player's Guide: Tips and Guidance",
 			"",
 			"- Do not use F5 debug info; it will mislead you!",
-			"- Hold/repeat right-click on walls/ceilings barehanded to climb.",
 			"- Can't dig trees or grass? Search for sticks in the canopy.",
 			"- Ores may be hidden, but revealed by subtle clues in terrain.",
 			"- \"Furnaces\" are not a thing; discover smelting with open flames.",
