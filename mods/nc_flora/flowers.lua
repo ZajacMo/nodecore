f94c14f3cc57c8348d241dad3031f4a3006b25fd
Aaron Suen <warr1024@gmail.com>
2021-06-30 23:21:37 -0400

    Baseline flower implementation
    - 9 colors * 5 shapes = 45 varieties
    - Spawn a handful of baseline species into the world
    
    The plan is to use a breeding system where flowers spread
    onto nearby fertile ground and accept mutations from
    parents, plus random mutations to nearby strains, each
    time they're bred, so players can farm flowers to unlocks
    all possible varieties.

---
+++ mods/nc_flora/flowers.lua
@@ -0,0 +1,84 @@
+-- LUALOCALS < ---------------------------------------------------------
+local minetest, nodecore, string
+    = minetest, nodecore, string
+local string_format
+    = string.format
+-- LUALOCALS > ---------------------------------------------------------
+
+local modname = minetest.get_current_modname()
+
+local shapeparams = {0, 0, 0, 2, 4}
+
+local colors = {
+	{name = "Pink", color = "e040c0"},
+	{name = "Red", color = "ff0000"},
+	{name = "Orange", color = "ff8000"},
+	{name = "Yellow", color = "ffff00"},
+	{name = "White", color = "e0e0e0"},
+	{name = "Cyan", color = "00c0c0"},
+	{name = "Blue", color = "0000ff"},
+	{name = "Violet", color = "8000ff"},
+	{name = "Black", color = "202020"},
+}
+local function flowername(shapeid, colorid)
+	return string_format("%s:flower%d%d", modname, shapeid, colorid)
+end
+
+for shapeid = 1, 5 do
+	for colorid = 1, #colors do
+		local txr = modname .. "_flower_" .. shapeid .. "_top.png^[multiply:#"
+		.. colors[colorid].color .. "^" .. modname .. "_flower_" .. shapeid
+		.. "_base.png"
+		minetest.register_node(flowername(shapeid, colorid),
+			{
+				description = colors[colorid].name .. " Flower",
+				drawtype = 'plantlike',
+				waving = 1,
+				tiles = {txr},
+				wield_image = txr,
+				inventory_image = txr,
+				sunlight_propagates = true,
+				paramtype = 'light',
+				walkable = false,
+				paramtype2 = "meshoptions",
+				place_param2 = shapeparams[shapeid],
+				groups = {
+					snappy = 1,
+					flower = 1,
+					flammable = 1,
+					attached_node = 1
+				},
+				nc_flower_shape = shapeid,
+				nc_flower_color = colorid,
+				sounds = nodecore.sounds("nc_terrain_swishy"),
+				selection_box = {
+					type = "fixed",
+					fixed = {-6/16, -0.5, -6/16, 6/16, 4/16, 6/16},
+				},
+			})
+	end
+end
+
+local function reggen(shapeid, colorid, rare)
+	return minetest.register_decoration({
+			name = flowername(shapeid, colorid),
+			deco_type = "simple",
+			place_on = {"nc_terrain:dirt_with_grass"},
+			sidelen = 1,
+			noise_params = {
+				offset = -0.001 + 0.001 * rare,
+				scale = 0.001,
+				spread = {x = 200, y = 200, z = 200},
+				seed = 1572,
+				octaves = 3,
+				persist = 0.7
+			},
+			decoration = flowername(shapeid, colorid),
+			param2 = shapeparams[shapeid],
+		})
+end
+reggen(1, 2, 0.002)
+reggen(2, 3, 0.02)
+reggen(3, 4, 0.2)
+reggen(4, 5, 0.02)
+reggen(5, 6, 0.002)
--- mods/nc_flora/init.lua
+++ mods/nc_flora/init.lua
@@ -7,3 +7,4 @@ nodecore.amcoremod()
 
 include("rushes")
 include("sedges")
+include("flowers")
