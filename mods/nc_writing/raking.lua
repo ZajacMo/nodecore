04bb7d083e25abee1e964535ec504e5415b0844c
Aaron Suen <warr1024@gmail.com>
2021-07-02 23:08:04 -0400

    New raked sand prototype
    New pattern textures by GreenXenith
    - Linear and Nexus shapes
    - Recipe determined by player looking direction
    - Rake same again to clear
    - Handle pattern loss on digging, silktouch

--- mods/nc_writing/raking.lua
+++ mods/nc_writing/raking.lua
@@ -1,6 +1,8 @@
 -- LUALOCALS < ---------------------------------------------------------
-local minetest, nodecore
-    = minetest, nodecore
+local math, minetest, nodecore, vector
+    = math, minetest, nodecore, vector
+local math_pi
+    = math.pi
 -- LUALOCALS > ---------------------------------------------------------
 
 local modname = minetest.get_current_modname()
@@ -12,7 +14,10 @@ minetest.register_node(modname .. ":sand_raked",
 			description = "Raked Sand",
 			tiles = {
 				sanddef.tiles[1] .. "^" .. modname
-				.. "_raking.png", sanddef.tiles[1]
+				.. "_raking_linear.png",
+				sanddef.tiles[1],
+				sanddef.tiles[1] .. "^" .. modname
+				.. "_raking_side.png"
 			},
 			paramtype2 = "facedir",
 			on_place = function(itemstack, placer, pointed_thing)
@@ -21,23 +26,63 @@ minetest.register_node(modname .. ":sand_raked",
 					false, {force_floor = true})
 			end,
 			falling_replacement = sandname,
+			silktouch_as = sandname
+		}, sanddef))
+minetest.register_node(modname .. ":sand_raked_nexus",
+	nodecore.underride({
+			description = "Raked Sand",
+			tiles = {
+				sanddef.tiles[1] .. "^" .. modname
+				.. "_raking_nexus.png",
+				sanddef.tiles[1],
+				sanddef.tiles[1] .. "^" .. modname
+				.. "_raking_side.png"
+			},
+			paramtype2 = "facedir",
+			on_place = function(itemstack, placer, pointed_thing)
+				return minetest.rotate_and_place(
+					itemstack, placer, pointed_thing,
+					false, {force_floor = true})
+			end,
+			falling_replacement = sandname,
+			silktouch_as = sandname
 		}, sanddef))
 
 nodecore.register_craft({
 		label = "rake sand",
 		action = "pummel",
 		wield = {groups = {rakey = true}},
-		normal = {y = 1},
-		indexkeys = {sandname},
-		nodes = {{match = sandname}},
 		duration = 0.5,
+		normal = {y = 1},
+		indexkeys = {"group:sand"},
+		nodes = {{match = {groups = {sand = true, falling_repose = false}}}},
 		after = function(pos, data)
-			if not (data.crafter and data.crafter.get_look_dir) then return end
-			local dir = data.crafter:get_look_dir()
-			dir.y = 0
-			nodecore.set_loud(pos, {
+			if not (data.crafter and data.crafter.getpos
+				and data.crafter.get_look_horizontal) then return end
+
+			local newnode
+			local ppos = data.crafter:get_pos()
+			ppos.y = pos.y
+			if vector.distance(pos, ppos) < 0.4 then
+				newnode = {
+					name = modname .. ":sand_raked_nexus",
+					param2 = 0
+				}
+			else
+				local dir = data.crafter:get_look_horizontal()
+				while dir >= math_pi * 3/4 do dir = dir - math_pi end
+				dir = minetest.yaw_to_dir(dir + math_pi / 4)
+				newnode = {
 					name = modname .. ":sand_raked",
 					param2 = minetest.dir_to_facedir(dir)
-				})
+				}
+			end
+
+			local node = data.node or minetest.get_node(pos)
+			if node.name == newnode.name and node.param2 == newnode.param2 then
+				newnode = {name = sandname}
+			end
+			nodecore.wear_wield(data.crafter, {snappy = 1}, 1)
+			return nodecore.set_loud(pos, newnode)
 		end
 	})
