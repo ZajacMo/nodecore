11a3790bc692f2e5b4c913b6faf2bed1de73de41
Aaron Suen <warr1024@gmail.com>
2021-07-05 12:10:47 -0400

    Fix door ablation spurious refiring

--- mods/nc_doors/ablation.lua
+++ mods/nc_doors/ablation.lua
@@ -9,6 +9,11 @@ local dntname = modname .. ":ablation"
 local lenson = "nc_optics:lens_on"
 
 local function ablation(pos, node)
+	local meta = minetest.get_meta(pos)
+	local cooldown = meta:get_float("ablation") or 0
+	if cooldown > nodecore.gametime then return end
+	meta:set_float("ablation", nodecore.gametime + 2)
+
 	local face = nodecore.facedirs[node.param2]
 	local out = vector.add(face.k, pos)
 	local tn = minetest.get_node(out)
