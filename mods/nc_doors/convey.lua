2e27f9963f08c2cc66a023881b639be8a201956f
Aaron Suen <warr1024@gmail.com>
2021-07-05 10:44:31 -0400

    Iterative new approach to compact conveyors
    If something is blocked, store it in a separate retry
    queue.  If the thing that blocked it moved out of
    the way, re-queue the things waiting on it for retry.
    This is probably theoretically less efficient than the
    old approach since it relies on finding opportunities
    to move items on the fly and probably queues a
    number of retries that will ultimately fail, but it's
    simpler and easier to maintain than the old way.

--- mods/nc_doors/convey.lua
+++ mods/nc_doors/convey.lua
@@ -3,40 +3,70 @@ local ipairs, minetest, nodecore, vector
     = ipairs, minetest, nodecore, vector
 -- LUALOCALS > ---------------------------------------------------------
 
-local queue = {}
-
+local hashpos = minetest.pos_to_string
 local is_falling = {groups = {falling_node = true}}
 
-local function pushcore(pos, node, dir, ...)
-	if not dir then return end
-	local to = vector.add(pos, dir)
-	if nodecore.buildable_to(to) then
-		node.param = nil
-		queue[#queue + 1] = {
-			from = pos,
-			to = to,
-			node = node
-		}
-		return true
-	end
-	return pushcore(pos, node, ...)
-end
+local queue = {}
+local queued = {}
+
 function nodecore.door_push(pos, ...)
+	local key = hashpos(pos)
+	if queued[key] then return end
 	local node = minetest.get_node(pos)
 	if not nodecore.match(node, is_falling) then return end
-	return pushcore(pos, node, ...)
+	node.param = nil
+	local try = {}
+	for i, dir in ipairs({...}) do
+		try[i] = vector.add(pos, dir)
+	end
+	queue[#queue + 1] = {
+		key = key,
+		from = pos,
+		try = try,
+	}
+	queued[key] = true
+end
+
+local function tryprocess(item, retry)
+	local node = minetest.get_node(item.from)
+	if not nodecore.match(node, is_falling) then return end
+	for _, t in ipairs(item.try) do
+		if nodecore.buildable_to(t) then
+			local meta = minetest.get_meta(item.from):to_table()
+			minetest.remove_node(item.from)
+			nodecore.set_loud(t, node)
+			minetest.get_meta(t):from_table(meta)
+			nodecore.fallcheck(t)
+			local re = retry[hashpos(item.from)]
+			if not re then return end
+			for _, r in ipairs(re) do
+				if not queued[r.key] then
+					queue[#queue + 1] = r
+					queued[r.key] = true
+				end
+			end
+		end
+	end
+	for _, t in ipairs(item.try) do
+		local key = hashpos(t)
+		local r = retry[key]
+		if not r then
+			r = {}
+			retry[key] = r
+		end
+		r[#r + 1] = item
+	end
 end
 
 minetest.register_globalstep(function()
-		for _, v in ipairs(queue) do
-			local node = minetest.get_node(v.from)
-			if node.name == v.node.name and node.param2 == v.node.param2
-			and nodecore.buildable_to(v.to) then
-				local meta = minetest.get_meta(v.from):to_table()
-				minetest.remove_node(v.from)
-				nodecore.set_loud(v.to, v.node)
-				minetest.get_meta(v.to):from_table(meta)
-			end
+		local retry = {}
+		local i = 1
+		while i <= #queue do
+			local item = queue[i]
+			queued[hashpos(item.from)] = nil
+			tryprocess(item, retry)
+			i = i + 1
 		end
 		queue = {}
+		queued = {}
 	end)
