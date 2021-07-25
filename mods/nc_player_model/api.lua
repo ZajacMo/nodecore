c326d24d1b438e4de93c6fa8e2b565ace7cae3ea
Aaron Suen <warr1024@gmail.com>
2021-07-25 09:58:01 -0400

    Fix singleplayer skin
    Handle single player as a special case and use the
    default, un-colorized skin.  This should also make
    it easier for people to change the SP skin without
    having to interact with the colorization system.

--- mods/nc_player_model/api.lua
+++ mods/nc_player_model/api.lua
@@ -38,10 +38,14 @@ local function getcolors(name, layers)
 	layers[#layers + 1] = found
 end
 
+local spname = "singleplayer"
 nodecore.player_skin = nodecore.player_skin or function(player, options)
 	if type(options) ~= "table" then options = {} end
 
-	local name = options.playername or player:get_player_name()
+	local name = options.playername or player
+	and player:get_player_name() or spname
+
+	if name == spname then return modname .. "_singleplayer.png" end
 
 	local layers = {}
 	getcolors(name, layers)
