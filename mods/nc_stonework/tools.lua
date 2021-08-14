8237f8c400dea5d8fcce5f388a1b7931bc407fb6
Aaron Suen <warr1024@gmail.com>
2021-08-14 08:06:27 -0400

    Convert tool stone-tipping to stackapply recipe

--- mods/nc_stonework/tools.lua
+++ mods/nc_stonework/tools.lua
@@ -1,43 +1,9 @@
 -- LUALOCALS < ---------------------------------------------------------
-local ipairs, minetest, nodecore
-    = ipairs, minetest, nodecore
+local minetest, nodecore
+    = minetest, nodecore
 -- LUALOCALS > ---------------------------------------------------------
 
 local modname = minetest.get_current_modname()
-
-nodecore.register_stone_tip_tool,
-nodecore.registered_stone_tip_tools
-= nodecore.mkreg()
-
-local stoned = nodecore.sounds("nc_terrain_stony").place
-
-local chip = modname .. ":chip"
-nodecore.extend_item(chip, function(copy, orig)
-		copy.on_place = function(itemstack, placer, pointed_thing, ...)
-			if not nodecore.interact(placer) then return end
-			if itemstack:get_name() == chip and pointed_thing.type == "node" then
-				local pos = pointed_thing.under
-				for _, v in ipairs(nodecore.registered_stone_tip_tools) do
-					if nodecore.match(pos, {
-							name = v.from,
-							wear = 0.05
-						}) then
-						minetest.remove_node(pos)
-						nodecore.item_eject(pos, v.to)
-						stoned.pos = pos
-						nodecore.sound_play(stoned.name, stoned)
-						itemstack:set_count(itemstack:get_count() - 1)
-						if placer then
-							nodecore.player_discover(placer,
-								"craft:assemble " .. v.to)
-						end
-						return itemstack
-					end
-				end
-			end
-			return orig.on_place(itemstack, placer, pointed_thing, ...)
-		end
-	end)
 
 local function tooltip(name, group)
 	local tool = modname .. ":tool_" .. name:lower()
@@ -57,7 +23,15 @@ local function tooltip(name, group)
 			on_ignite = modname .. ":chip",
 			sounds = nodecore.sounds("nc_terrain_stony")
 		})
-	nodecore.register_stone_tip_tool({from = wood, to = tool})
+	nodecore.register_craft({
+			label = "assemble " .. tool,
+			action = "stackapply",
+			wield = {name = modname .. ":chip"},
+			consumewield = 1,
+			indexkeys = {wood},
+			nodes = {{match = wood, replace = "air"}},
+			items = {tool}
+		})
 end
 
 tooltip("Mallet", "thumpy")
