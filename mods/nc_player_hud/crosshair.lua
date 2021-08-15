16959bf2eec78d0f7f3f31798d3475ff11fdb57a
Aaron Suen <warr1024@gmail.com>
2021-08-14 23:21:52 -0400

    Resimplify crosshair
    The "player character" only assists with "depth
    perception" and determining if pointing at
    something within reach; diggability, protection,
    and other consequences of touching the thing
    should really be the actual player's responsibility.
    We don't want things to be too "automatic" for
    the player; they are supposed to learn about the
    world, not about the HUD instrumentation.

--- mods/nc_player_hud/crosshair.lua
+++ mods/nc_player_hud/crosshair.lua
@@ -1,15 +1,15 @@
 -- LUALOCALS < ---------------------------------------------------------
-local minetest, nodecore
-    = minetest, nodecore
+local nodecore
+    = nodecore
 -- LUALOCALS > ---------------------------------------------------------
 
-local function crosshair(player, locked, fade)
+local function crosshair(player, fade)
 	nodecore.hud_set(player, {
 			label = "crosshair",
 			hud_elem_type = "image",
 			position = {x = 0.5, y = 0.5},
-			text = "nc_player_hud_crosshair" .. (locked and "_locked" or "")
-			.. ".png^[opacity:" .. (fade and 32 or 192),
+			text = "nc_player_hud_crosshair.png^[opacity:"
+			.. (fade and 32 or 192),
 			direction = 0,
 			alignment = {x = 0, y = 0},
 			scale = {x = 1, y = 1},
@@ -26,15 +26,11 @@ nodecore.register_playerstep({
 			local pt = data.raycast()
 			if pt then
 				if pt.type == "node" then
-					if minetest.is_protected(pt.under, data.pname) then
-						return crosshair(player, true)
-					else
-						return crosshair(player)
-					end
+					return crosshair(player)
 				elseif pt.type == "object" then
 					return crosshair(player)
 				end
 			end
-			return crosshair(player, nil, true)
+			return crosshair(player, true)
 		end
 	})
