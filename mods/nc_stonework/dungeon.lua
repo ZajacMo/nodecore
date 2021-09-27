b5eb58d2bbc6046621ac335bf5d4d1954caa4669
Aaron Suen <warr1024@gmail.com>
2021-09-27 06:59:14 -0400

    Dungeon stonework API, restrict a few more loot items
    Add a generic method to get stonework materials for dungeon
    brick generation so we can extend dungeon bricking while keeping
    the same pattern of mixed cobble/bricks/bonded.

--- mods/nc_stonework/dungeon.lua
+++ mods/nc_stonework/dungeon.lua
@@ -8,18 +8,29 @@ local modname = minetest.get_current_modname()
 local mapperlin
 minetest.after(0, function() mapperlin = minetest.get_perlin(45821, 1, 0, 1) end)
 
+function nodecore.dungeon_bricks()
+	return {
+		brick = modname .. ":bricks_stone",
+		bonded = modname .. ":bricks_stone_bonded"
+	}
+end
+
 nodecore.register_dungeongen({
 		label = "dungeon concrete/bricks",
 		priority = -100,
 		func = function(pos)
 			local rng = nodecore.seeded_rng(mapperlin:get_3d(pos))
-			if rng(1, 4) ~= 1 then return end
+			local profile = nodecore.dungeon_bricks(pos, rng)
+			if rng(1, 4) ~= 1 then
+				return profile.fill and
+				minetest.set_node(pos, {name = profile.fill})
+			end
 			local below = {x = pos.x, y = pos.y - 1, z = pos.z}
 			local bnode = minetest.get_node(below)
 			local bdef = minetest.registered_nodes[bnode.name] or {}
-			return minetest.set_node(pos, {name = modname
-					.. (rng(1, 10) <= (bdef.walkable and 9 or 2)
-						and ":bricks_stone"
-						or ":bricks_stone_bonded")})
+			return minetest.set_node(pos, {name = (rng(1, 10) <=
+						(bdef.walkable and 9 or 2)
+						and profile.brick
+						or profile.bonded)})
 		end
 	})
