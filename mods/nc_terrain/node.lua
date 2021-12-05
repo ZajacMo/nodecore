8edd665b68b43ffac3d92ddc459f95f17fadfb40
Aaron Suen <warr1024@gmail.com>
2021-12-04 21:27:49 -0500

    Revert "Make grass node undersides subtly distinguishable from dirt"
    This reverts commit b1661c976e3a6420c3917a5322ee72e0dd9ddb6d.
    
    This was done inconsistently and the problem extends to a number
    of nodes, which should probably all be fixed at the same time.

--- mods/nc_terrain/node.lua
+++ mods/nc_terrain/node.lua
@@ -163,14 +163,13 @@ regterrain({
 		crush_damage = 1,
 		sounds = nodecore.sounds("nc_terrain_crunchy")
 	})
-local grassroots = modname .. "_dirt.png^(" .. modname .. "_grass_roots.png^[opacity:48)"
 regterrain({
 		name = "dirt_with_grass",
 		description = "Grass",
 		tiles = {
 			modname .. "_grass_top.png",
-			grassroots,
-			grassroots .. "^(" .. modname .. "_grass_top.png^[mask:"
+			modname .. "_dirt.png",
+			modname .. "_dirt.png^(" .. modname .. "_grass_top.png^[mask:"
 			.. modname .. "_grass_sidemask.png)"
 		},
 		mapgen = {
