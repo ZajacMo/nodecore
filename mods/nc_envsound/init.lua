3ea2ce8827ccd728dea725e9721cb9b9f5542d82
Aaron Suen <warr1024@gmail.com>
2021-12-07 00:11:15 -0500

    Much more palpable wind sounds
    Instead of statically-positioned wind sounds, many
    of which are often outside of hearing range,
    attach sounds to moving entities which often tend
    to move across the player's position.

--- mods/nc_envsound/init.lua
+++ mods/nc_envsound/init.lua
@@ -7,6 +7,24 @@ local math_exp, math_random
 
 nodecore.amcoremod()
 
+local modname = minetest.get_current_modname()
+
+minetest.register_entity(modname .. ":fx", {
+		initial_properties = {
+			visual_size = {x = 0, y = 0},
+			pointable = false,
+			physical = false,
+			is_visible = false,
+			collide_with_objects = false,
+			static_save = false
+		},
+
+		on_step = function(self, dtime)
+			self.age = (self.age or 0) + dtime
+			if self.age >= 5.5 then return self.object:remove() end
+		end
+	})
+
 local function dsqr(a, b)
 	local v = vector.subtract(a, b)
 	return vector.dot(v, v)
@@ -30,15 +48,27 @@ local function check(pos, done, srcs)
 
 	if nodecore.is_full_sun(sp) then
 		if sp.y <= 0 then return end
+		if math_random(1, 2) ~= 1 then return end
+		local np = {
+			x = pos.x + math_random() * 64 - 32,
+			y = pos.y + math_random() * 64 - 32,
+			z = pos.z + math_random() * 64 - 32,
+		}
+		local ent = minetest.add_entity(sp, modname .. ":fx")
+		ent:set_velocity(vector.multiply(vector.normalize(
+					vector.subtract(np, sp)), 4 * math_random()))
 		nodecore.sound_play("nc_envsound_air", {
 				pos = sp,
-				gain = nodecore.windiness(sp.y) / 100
+				object = ent,
+				gain = nodecore.windiness(sp.y) / 100,
+				max_hear_distance = 64
 			})
 	elseif nodecore.get_node_light(sp) < 4 then
 		nodecore.sound_play("nc_envsound_drip", {
 				pos = sp,
 				pitchvary = 0.4,
-				gain = math_exp(math_random()) / 5
+				gain = math_exp(math_random()) / 5,
+				max_hear_distance = 64
 			})
 	end
 
