80b716036676381d2b8659284e9e093780bb2d60
Aaron Suen <warr1024@gmail.com>
2021-12-08 23:12:22 -0500

    Earth raking hints

--- mods/nc_writing/hints.lua
+++ mods/nc_writing/hints.lua
@@ -1,6 +1,6 @@
 -- LUALOCALS < ---------------------------------------------------------
-local minetest, nodecore
-    = minetest, nodecore
+local minetest, nodecore, pairs
+    = minetest, nodecore, pairs
 -- LUALOCALS > ---------------------------------------------------------
 
 local modname = minetest.get_current_modname()
@@ -23,4 +23,15 @@ end
 nodecore.register_hint("cycle through all charcoal glyphs",
 	placedall,
 	"charcoal writing"
+)
+
+for _, v in pairs({"sand", "gravel", "dirt"}) do
+	nodecore.register_hint("rake " .. v,
+		"rake " .. v,
+		{"group:rakey", "nc_terrain:" .. v}
+	)
+end
+nodecore.register_hint("rake humus",
+	"rake humus",
+	{"group:rakey", "nc_tree:humus"}
 )
