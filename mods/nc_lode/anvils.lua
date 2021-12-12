df0a2a177f5c96c3e2b5b3a92cb381e6dc65f330
Aaron Suen <warr1024@gmail.com>
2021-12-11 22:56:51 -0500

    Fix anvil performance issues
    Instead of registering 12 separate recipes for
    each anvil recipe, just register 2: one for each
    temper, and do a second lookup for anvil types
    in pre/post check.  This should avoid having
    to re-evaluate all the rest of the stuff for each
    anvil/temper combination type.

--- mods/nc_lode/anvils.lua
+++ mods/nc_lode/anvils.lua
@@ -1,6 +1,8 @@
 -- LUALOCALS < ---------------------------------------------------------
-local minetest, nodecore, pairs, type
-    = minetest, nodecore, pairs, type
+local ipairs, math, minetest, nodecore, type
+    = ipairs, math, minetest, nodecore, type
+local math_random
+    = math.random
 -- LUALOCALS > ---------------------------------------------------------
 
 local modname = minetest.get_current_modname()
@@ -35,43 +37,77 @@ minetest.register_abm({
 		end
 	})
 
+local anvillogic = {
+	-- Tempered anvils can work hot or cold
+	["annealed/" .. modname .. ":block_tempered"] = true,
+	["hot/" .. modname .. ":block_tempered"] = true,
+
+	-- Annealed anvils only work hot
+	["hot/" .. modname .. ":block_annealed"] = true,
+
+	-- Smooth stone turns into cracked stone and only works
+	-- as long as it remains cracked stone.
+	["hot/nc_terrain:stone"] = function(pos)
+		return function()
+			nodecore.set_loud(pos, {name = cracked})
+		end
+	end,
+	["hot/" .. cracked] = true,
+}
+-- Hard stone is weakened probabilistically, but at half
+-- the rate for each layer of hardness.
+for i = 1, nodecore.hard_stone_strata do
+	anvillogic["hot/nc_terrain:hard_stone_" .. i] = function(pos)
+		return function()
+			if math_random(1, 2 ^ i) ~= 1 then return end
+			nodecore.set_loud(pos, {name = (i == 1)
+					and "nc_terrain:stone"
+					or ("nc_terrain:hard_stone_" .. (i - 1))
+				})
+		end
+	end
+end
+
+local function chain(func, oldfunc)
+	if not oldfunc then return func end
+	return function(...)
+		local r = func(...)
+		if not r then return r end
+		return oldfunc(...)
+	end
+end
+
 -- nodecore.register_lode_anvil_recipe(anvilpos, function(temper) end)
 -- - anvilpos can be a table or a number (bare relative y value)
 -- - temper is a string to be inserted into recipe item/node names
 -- the anvil node is added to the recipe automatically at anvilpos
 function nodecore.register_lode_anvil_recipe(anvilpos, func)
-	if type(anvilpos) == "number" then anvilpos = {y = anvilpos} end
-
-	local function register(anvil, temper, label)
-		for k, v in pairs(anvilpos) do anvil[k] = v end
-		local recipe = func(temper)
-		recipe.discover = {recipe.label, "anvil:" .. label}
-		recipe.label = recipe.label .. " (" .. label .. ")"
-		recipe.nodes = recipe.nodes or {}
-		recipe.nodes[#recipe.nodes + 1] = anvil
-		return nodecore.register_craft(recipe)
+	local anvilx = 0
+	local anvily
+	local anvilz = 0
+	if type(anvilpos) == "number" then
+		anvily = anvilpos
+	else
+		anvilx = anvilpos.x or 0
+		anvily = anvilpos.y or 0
+		anvilz = anvilpos.z or 0
 	end
-
-	-- Tempered anvils can work hot or cold
-	register({match = modname .. ":block_tempered"}, "annealed", "cold/tempered")
-	register({match = modname .. ":block_tempered"}, "hot", "hot/tempered")
-
-	-- Annealed anvils only work hot
-	register({match = modname .. ":block_annealed"}, "hot", "hot/annealed")
-
-	-- Smooth stone turns into cracked stone and only works
-	-- as long as it remains cracked stone.
-	register({match = "nc_terrain:stone", replace = cracked}, "hot", "hot/stone")
-	register({match = cracked}, "hot", "hot/stone")
-
-	-- Hard stone is weakened probabilistically, but at half
-	-- the rate for each layer of hardness.
-	for i = 1, nodecore.hard_stone_strata do
-		register({
-				match = "nc_terrain:hard_stone_" .. i,
-				replace = (i == 1) and "nc_terrain:stone"
-				or ("nc_terrain:hard_stone_" .. (i - 1)),
-				chance = 2 ^ i
-			}, "hot", "hot/stone")
+	for _, temper in ipairs({"hot", "annealed"}) do
+		local recipe = func(temper)
+		recipe.check = chain(function(_, data)
+				local pos = data.rel(anvilx, anvily, anvilz)
+				local node = minetest.get_node(pos)
+				local logic = anvillogic[temper .. "/" .. node.name]
+				if not logic then return end
+				if logic == true then return true end
+				data.anvilcommit = logic(pos)
+				return true
+			end,
+			recipe.check)
+		recipe.after = chain(function(_, data)
+				if data.anvilcommit then data.anvilcommit() end
+			end,
+			recipe.after)
+		nodecore.register_craft(recipe)
 	end
 end
