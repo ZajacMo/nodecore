28e2fd559006a9b29b546ad303954fc75d762f5b
Aaron Suen <warr1024@gmail.com>
2022-01-18 18:51:10 -0500

    Experimental API for adjusting in-game time
    Everything in NodeCore should already be using
    nodecore.gametime instead of minetest.get_gametime().
    This API allows an offset to be adjusted between these,
    allowing for skipping forward in time.
    
    The idea is to offer an optional "realtime clock" feature via
    a mod or config option to allow mobile SP players to advance
    time forward for idle mechanics when the game is not
    loaded and running, via the realtime clock, to make idle
    mechanics more accessible to players on limited battery.

--- mods/nc_api/util_gametime.lua
+++ mods/nc_api/util_gametime.lua
@@ -1,13 +1,48 @@
 -- LUALOCALS < ---------------------------------------------------------
-local math, minetest, nodecore
-    = math, minetest, nodecore
+local error, math, minetest, nodecore, pairs, type
+    = error, math, minetest, nodecore, pairs, type
 local math_abs
     = math.abs
 -- LUALOCALS > ---------------------------------------------------------
 
+local modstore = minetest.get_mod_storage()
+local metakey = "gametime_adjust"
+local raw = modstore:get_string(metakey)
+local adjusts = raw and raw ~= "" and minetest.deserialize(raw, true) or {}
+
+local dirty
+
+-- name: key to identify adjustment
+-- value: value to add to time
+-- reset: if true, reset adjustment to this value offset, otherwise add
+function nodecore.gametime_adjust(name, value, reset)
+	local old = adjusts[name] or 0
+	if not value then return old end
+
+	if type(value) ~= "number" then
+		error("invalid gametime_adjust " .. type(value))
+	end
+
+	if not reset then value = value + old end
+	if value == old then return end
+	if value < old then
+		error("time cannot be adjusted backwards")
+	end
+
+	if nodecore.gametime then
+		nodecore.gametime = nodecore.gametime + value
+	end
+
+	adjusts[name] = value
+	dirty = true
+
+	return value
+end
+
 nodecore.register_globalstep("gametime", function(dtime)
 		local mtt = minetest.get_gametime()
 		local nct = nodecore.gametime
+		for _, v in pairs(adjusts) do mtt = mtt + v end
 		if not nct then
 			nodecore.log("info", "nodecore.gametime: init to " .. mtt)
 			nct = mtt
@@ -19,4 +54,8 @@ nodecore.register_globalstep("gametime", function(dtime)
 			nct = mtt
 		end
 		nodecore.gametime = nct
+		if dirty then
+			modstore:set_string(metakey, minetest.serialize(adjusts))
+			dirty = nil
+		end
 	end)
