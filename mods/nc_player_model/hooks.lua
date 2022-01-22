9464f8d534e25f749cdd526c9fbd83f1e8cb25d5
Aaron Suen <warr1024@gmail.com>
2022-01-22 09:29:09 -0500

    Enable frame blending and head pitch
    - Pitch player head up/down based on look dir
    - Set frame_blend to 0.15, causing 150ms of tweening
      from one animation to another, so player bone
      movement is smooth.
    - Enabling frame_blend breaks some parts of the
      anims, but oddly, enabling head pitch control actually
      fixes these...
    - Head pitch does not follow lookdir 100%, there is
      some implied use of vertical eye movement balanced
      in, so the player does not look like they're breaking
      their neck at extreme angles.
    - Support anims specifying a head pitch offset, e.g.
      for the swimming anims which require the head to
      pitch backward to face forward.

--- mods/nc_player_model/hooks.lua
+++ mods/nc_player_model/hooks.lua
@@ -1,7 +1,15 @@
 -- LUALOCALS < ---------------------------------------------------------
-local minetest, nodecore, pairs
-    = minetest, nodecore, pairs
+local math, minetest, nodecore, pairs
+    = math, minetest, nodecore, pairs
+local math_abs, math_deg
+    = math.abs, math.deg
 -- LUALOCALS > ---------------------------------------------------------
+
+local frame_blend = 0.15
+
+local pitch_mult = 2/3
+local pitch_limit = 60
+local pitch_precision = 1
 
 nodecore.register_playerstep({
 		label = "player model visuals",
@@ -30,6 +38,19 @@ nodecore.register_playerstep({
 			if anim.name then
 				nodecore.player_discover(player, "anim_" .. anim.name)
 			end
-			data.animation = {{x = anim.x, y = anim.y}, anim.speed}
+			data.animation = {{x = anim.x, y = anim.y}, anim.speed, frame_blend}
+
+			local pitch = -math_deg(player:get_look_vertical()) * pitch_mult
+			if anim and anim.headpitch then pitch = pitch + anim.headpitch end
+			if pitch < -pitch_limit then pitch = -pitch_limit end
+			if pitch > pitch_limit then pitch = pitch_limit end
+			if not (data.headpitch and math_abs(data.headpitch - pitch)
+				< pitch_precision) then
+				data.headpitch = pitch
+				player:set_bone_position("Head",
+					{x = 0, y = 0, z = 0},
+					{x = pitch, y = 0, z = 0}
+				)
+			end
 		end
 	})
