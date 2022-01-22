46241b06d4006b4cbcacd30a3ca788896f8ecda3
Aaron Suen <warr1024@gmail.com>
2022-01-22 14:52:11 -0500

    Invincible players also clear rad HUD

--- mods/nc_lux/radiation.lua
+++ mods/nc_lux/radiation.lua
@@ -151,7 +151,8 @@ local logbase = math_log(base)
 nodecore.register_playerstep({
 		label = "lux rad scan",
 		action = function(player, data, dtime)
-			if not nodecore.player_visible(player) then
+			if not (nodecore.player_visible(player)
+				and nodecore.player_can_take_damage(player)) then
 				return nodecore.hud_set(player, {label = "radiation", ttl = 0})
 			end
 
@@ -173,16 +174,14 @@ nodecore.register_playerstep({
 				data.unradtime = data.unradtime - use
 			end
 
-			if nodecore.player_can_take_damage(player) then
-				data.radtime = (data.radtime or 0) + dtime
-				if data.radtime > 1 then data.radtime = 1 end
-				while data.radtime > 1/16 do
-					data.radtime = data.radtime - 1/16
-					local inrate = (nodescan(player) + itemscan(player)) / 256
-					rate = (rate or 0) * 0.99 + inrate * 0.01
-					if inrate > 0 and math_random() < inrate then
-						rad = 1 - (1 - rad) * 31/32
-					end
+			data.radtime = (data.radtime or 0) + dtime
+			if data.radtime > 1 then data.radtime = 1 end
+			while data.radtime > 1/16 do
+				data.radtime = data.radtime - 1/16
+				local inrate = (nodescan(player) + itemscan(player)) / 256
+				rate = (rate or 0) * 0.99 + inrate * 0.01
+				if inrate > 0 and math_random() < inrate then
+					rad = 1 - (1 - rad) * 31/32
 				end
 			end
 			setrate(rate)
