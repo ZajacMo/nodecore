9555a1ecbaa4f3e911a158fc92e705b1800a05bb
Aaron Suen <warr1024@gmail.com>
2022-01-23 17:28:48 -0500

    Optimize ABM delay
    Only register a single job with after() per tick.
    Store actual node processing to do in a much
    simpler linear array.

--- mods/nc_api_active/abmdelay.lua
+++ mods/nc_api_active/abmdelay.lua
@@ -3,19 +3,30 @@ local minetest
     = minetest
 -- LUALOCALS > ---------------------------------------------------------
 
+local pending
+
 local oldreg = minetest.register_abm
 function minetest.register_abm(def, ...)
 	if not def.action_delay then return oldreg(def, ...) end
 
 	local oldact = def.action
-	function def.action(pos, node, ...)
-		minetest.after(0, function(...)
-				local nn = minetest.get_node(pos)
-				if nn.name == node.name then
-					return oldact(pos, node, ...)
-				end
-				end, ...)
+	function def.action(pos, node)
+		if not pending then
+			pending = {}
+			minetest.after(0, function()
+					for i = 1, #pending do
+						(pending[i])()
+					end
+					pending = nil
+				end)
 		end
-
-		return oldreg(def, ...)
+		pending[#pending + 1] = function()
+			local nn = minetest.get_node(pos)
+			if nn.name == node.name then
+				return oldact(pos, node)
+			end
+		end
 	end
+
+	return oldreg(def, ...)
+end
