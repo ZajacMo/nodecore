6401faa87b3a5a6fd6c88c2a6ce67faf3a8d6acb
Aaron Suen <warr1024@gmail.com>
2022-04-22 07:29:54 -0400

    Translation support for package metadata

--- .cdbrelease.lua
+++ .cdbrelease.lua
@@ -31,14 +31,15 @@ for i = 1, 5 do
 	screenshots[#screenshots + 1] = readbinary('.cdb-screen' .. i .. '.jpg')
 end
 
+local version = dofile("./mods/nc_api/version.lua")
+local pkgmeta = dofile("./mods/nc_api/pkgmeta.lua")
+
 return {
 	pkg = alpha and "nodecore_alpha" or "nodecore",
-	version = dofile("./mods/nc_api/version.lua"),
+	version = version,
 	type = "game",
-	title = "NodeCore" .. (alpha and " ALPHA" or ""),
-	short_description = (alpha
-		and "Early-access edition of NodeCore with latest features (and maybe bugs)"
-		or "Minetest's top original voxel game about emergent mechanics and exploration"),
+	title = "NodeCore" .. pkgmeta.title(alpha),
+	short_description = pkgmeta.desc(alpha),
 	dev_state = alpha and "BETA" or "ACTIVELY_DEVELOPED",
 	tags = tags,
 	content_warnings = {},
