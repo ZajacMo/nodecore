0c7e0e819b2c69a7ee7ba0d1e5dd86fc2012676e
Aaron Suen <warr1024@gmail.com>
2022-06-26 08:13:56 -0400

    Guard against nil player
    https://gitlab.com/sztest/nodecore/-/issues/14
    Unable to reproduce the crash, but the fix
    seems obvious given the symptom.

--- mods/nc_player_pickup/dropall.lua
+++ mods/nc_player_pickup/dropall.lua
@@ -23,6 +23,7 @@ end
 
 local olddrop = minetest.item_drop
 function minetest.item_drop(item, player, ...)
+	if not player then return olddrop(item, player, ...) end
 	nodecore.player_discover(player, "item_drop")
 	local pctl = player:get_player_control()
 	if pctl.aux1 then
