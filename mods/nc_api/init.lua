553893b60a486a18169ab76f571a12aa00ad01df
Aaron Suen <warr1024@gmail.com>
2022-10-19 19:22:55 -0400

    Detect accidental global sound playing
    Every sound in the game (and all known mods for
    it) should play either at a specific location, to a
    specific player (e.g. chat beeps or notifications),
    or bound to a specific physical object.  Detect any
    that might be played globally by accident, e.g.
    like a bug that existed in NC ZipRunes for a while,
    to aid in debugging.

--- mods/nc_api/init.lua
+++ mods/nc_api/init.lua
@@ -91,6 +91,7 @@ include("compat_issue10127")
 include("compat_legacyent")
 include("compat_nodealpha")
 include("compat_authcache")
+include("compat_soundpos")
 
 include("util_settings")
 include("util_privs")
