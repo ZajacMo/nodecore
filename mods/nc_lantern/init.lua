87975129d1025e9dd750dc652fcb18b45c033530
Aaron Suen <warr1024@gmail.com>
2022-10-18 22:58:37 -0400

    Craft and recycle recipes

--- mods/nc_lantern/init.lua
+++ mods/nc_lantern/init.lua
@@ -29,14 +29,48 @@ local function reg(level)
 			backface_culling = true,
 			use_texture_alpha = "clip",
 			groups = {
-				[modname] = level,
+				[modname] = level + 1,
 				snappy = 1,
 				lux_emit = level,
 			},
 			stack_max = 1,
 			light_source = level * 2,
-			sounds = nodecore.sounds("nc_lode_annealed")
+			sounds = nodecore.sounds("nc_lode_annealed"),
+			preserve_metadata = function(_, _, oldmeta, drops)
+				drops[1]:get_meta():from_table({fields = oldmeta})
+			end,
+			after_place_node = function(pos, _, itemstack)
+				local meta = minetest.get_meta(pos)
+				meta:from_table(itemstack:get_meta():to_table())
+			end,
 		})
 end
 
 for i = 0, 7 do reg(i) end
+
+nodecore.register_craft({
+		label = "assemble lantern",
+		normal = {x = 1},
+		indexkeys = {"nc_optics:glass_opaque"},
+		nodes = {
+			{match = "nc_optics:glass_opaque", replace = "air"},
+			{x = -1, match = "nc_tote:handle", replace = modname .. ":lamp0"},
+		}
+	})
+
+nodecore.register_craft({
+		label = "break apart lantern",
+		action = "pummel",
+		toolgroups = {choppy = 5},
+		indexkeys = {"group:" .. modname},
+		nodes = {
+			{
+				match = {groups = {[modname] = true}},
+				replace = "air"
+			}
+		},
+		items = {
+			{name = "nc_lode:bar_annealed 2", count = 4, scatter = 5},
+			{name = "nc_optics:glass_crude", scatter = 5}
+		}
+	})
