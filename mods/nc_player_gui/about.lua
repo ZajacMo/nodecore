650b44f7c0b07c7018264e972d1e5462863385e4
Aaron Suen <warr1024@gmail.com>
2022-10-20 18:35:59 -0400

    Add LiberaPay link

--- mods/nc_player_gui/about.lua
+++ mods/nc_player_gui/about.lua
@@ -21,7 +21,9 @@ local about = {
 	"",
 	"Discord: https://discord.gg/NNYeF6f",
 	"Matrix: #nodecore:matrix.org",
-	"IRC: #nodecore @@ irc.libera.chat"
+	"IRC: #nodecore @@ irc.libera.chat",
+	"",
+	"Donate: https://liberapay.com/NodeCore",
 }
 
 local modfmt = "Additional Mods Loaded: @1"
