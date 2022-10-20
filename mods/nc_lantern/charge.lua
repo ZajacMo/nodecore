ad5f830fe0550f89ac99cee9dc7f9a1985cbe4a6
Aaron Suen <warr1024@gmail.com>
2022-10-19 21:38:12 -0400

    Simplified/corrected charge/discharge logic
    - Fix failure to detect immersion in water
    - Reduce re-wield animations
    
    Compute current charge based on past rate and
    time, like a soaking AISM, but do NOT update the
    totals in metadata unless the rate changed or
    we crossed a charge level threshold.  This means
    that we should only see the re-wield anim when
    the charge level changes, the item is brought
    in or out of water, or moved around a reactor.
    
    We are losing the randomness factor from the
    soaking AISM, but it's probably fine to have a
    more exact discharge time for this sort of item.

--- mods/nc_lantern/charge.lua
+++ mods/nc_lantern/charge.lua
@@ -1,34 +1,49 @@
 -- LUALOCALS < ---------------------------------------------------------
-local math, minetest, nodecore
-    = math, minetest, nodecore
+local math, minetest, nodecore, tostring
+    = math, minetest, nodecore, tostring
 local math_floor
     = math.floor
 -- LUALOCALS > ---------------------------------------------------------
 
 local modname = minetest.get_current_modname()
 
-local charge_per_level = 500
 local discharge_rate = 30
+local charge_per_level = discharge_rate * 300
+local max_charge = charge_per_level * 8 - 1
 
-nodecore.register_soaking_aism({
+nodecore.register_aism({
 		label = "lantern charge",
-		fieldname = "charge",
-		interval = 10,
+		interval = 2,
 		arealoaded = 14,
 		itemnames = "group:" .. modname,
-		soakrate = function(_, aismdata)
-			local pos = aismdata.pos or aismdata.player and aismdata.player:get_pos()
-			local wet = minetest.get_item_group(minetest.get_node(pos), "moist") > 0
-			return (nodecore.lux_soak_rate(pos) or 0)
+		action = function(stack, data)
+			local pos = data.pos or data.player and data.player:get_pos()
+			local wet = minetest.get_item_group(minetest.get_node(pos).name,
+			"water") > 0
+			local rate = (nodecore.lux_soak_rate(pos) or 0)
 			- discharge_rate * (wet and 3 or 1)
-		end,
-		soakcheck = function(data, stack)
-			if data.total < 0 then data.total = 0 end
-			if data.total >= charge_per_level * 8 then
-				data.total = charge_per_level * 8 - 1
-			end
-			stack:set_name(modname .. ":lamp" .. math_floor(
-					data.total / charge_per_level))
-			return data.total, stack
+
+			local oldname = stack:get_name()
+			local meta = stack:get_meta()
+			local oldrate = meta:get_float("rate")
+			local oldqty = meta:get_float("qty")
+			local oldtime = meta:get_float("time")
+
+			local now = nodecore.gametime
+			local qty = (oldtime == 0)
+			and (minetest.get_item_group(oldname, modname) * charge_per_level)
+			or (oldqty + oldrate * (now - oldtime))
+			if qty < 0 then qty = 0 end
+			if qty > max_charge then qty = max_charge end
+			print(tostring(wet) .. qty)
+			local name = modname .. ":lamp" .. math_floor(qty / charge_per_level)
+
+			if name == oldname and rate == oldrate then return end
+
+			stack:set_name(name)
+			meta:set_float("rate", rate)
+			meta:set_float("qty", qty)
+			meta:set_float("time", now)
+			return stack
 		end
 	})
