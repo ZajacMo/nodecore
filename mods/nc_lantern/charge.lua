9764680fc5e7a8c402156f21fd9395b5b66a5f70
Aaron Suen <warr1024@gmail.com>
2022-10-19 22:06:21 -0400

    Fixes and debug/tracing
    - Check for water in same node (carried by player) and
      one node above (esp. for placed stack)
    - Slightly optimize node group check
    - Fix float rounding error, since meta floats are not
      perfectly round-tripped, to reduce spurious rate changes
    - Add debugging info to trace phase transitions on all
      lantern items
    - Correct initial charge setting (for /giveme'd items)

--- mods/nc_lantern/charge.lua
+++ mods/nc_lantern/charge.lua
@@ -1,8 +1,8 @@
 -- LUALOCALS < ---------------------------------------------------------
-local math, minetest, nodecore
-    = math, minetest, nodecore
-local math_floor
-    = math.floor
+local math, minetest, nodecore, pairs, string
+    = math, minetest, nodecore, pairs, string
+local math_floor, string_format
+    = math.floor, string.format
 -- LUALOCALS > ---------------------------------------------------------
 
 local modname = minetest.get_current_modname()
@@ -11,6 +11,36 @@ local discharge_rate = 30
 local charge_per_level = discharge_rate * 300
 local max_charge = charge_per_level * 8 - 1
 
+local waters = {}
+minetest.after(0, function()
+		for k, v in pairs(minetest.registered_nodes) do
+			if v.groups and v.groups.water and v.groups.water > 0 then
+				waters[k] = true
+			end
+		end
+	end)
+local function iswater(pos) return waters[minetest.get_node(pos).name] end
+local function iswet(pos)
+	if iswater(pos) then return true end
+	local p = {x = pos.x, y = pos.y + 1, z = pos.z}
+	return iswater(p)
+end
+
+local function floateq(a, b)
+	if not (a and b) then return true end
+	if a == 0 and b == 0 then return a == b end
+	local ratio = a / b
+	return ratio > 0.999 and ratio < 1.001
+end
+local function floatrpt(a, b)
+	if floateq(a, b) then return string_format("%0.2f", a) end
+	return string.format("%0.2f -> %0.2f", a, b)
+end
+
+local function getlevel(name)
+	return minetest.get_item_group(name, modname) - 1
+end
+
 nodecore.register_aism({
 		label = "lantern charge",
 		interval = 2,
@@ -18,10 +48,8 @@ nodecore.register_aism({
 		itemnames = "group:" .. modname,
 		action = function(stack, data)
 			local pos = data.pos or data.player and data.player:get_pos()
-			local wet = minetest.get_item_group(minetest.get_node(pos).name,
-			"water") > 0
 			local rate = (nodecore.lux_soak_rate(pos) or 0)
-			- discharge_rate * (wet and 3 or 1)
+			- discharge_rate * (iswet(pos) and 3 or 1)
 
 			local oldname = stack:get_name()
 			local meta = stack:get_meta()
@@ -31,13 +59,25 @@ nodecore.register_aism({
 
 			local now = nodecore.gametime
 			local qty = (oldtime == 0)
-			and (minetest.get_item_group(oldname, modname) * charge_per_level)
+			and (getlevel(oldname) * charge_per_level)
 			or (oldqty + oldrate * (now - oldtime))
-			if qty < 0 then qty = 0 end
-			if qty > max_charge then qty = max_charge end
-			local name = modname .. ":lamp" .. math_floor(qty / charge_per_level)
+			if qty <= 0 then
+				qty = 0
+				if rate < 0 then rate = 0 end
+			end
+			if qty >= max_charge then
+				qty = max_charge
+				if rate > 0 then rate = 0 end
+			end
+			local level = math_floor(qty / charge_per_level)
+			local name = modname .. ":lamp" .. level
 
-			if name == oldname and rate == oldrate then return end
+			if name == oldname and floateq(rate, oldrate) then return end
+
+			nodecore.log("action", string_format("lantern level %s rate %s charge %s at %s",
+					floatrpt(getlevel(oldname), level),
+					floatrpt(oldrate, rate), floatrpt(oldqty, qty),
+					minetest.pos_to_string(pos, 0)))
 
 			stack:set_name(name)
 			meta:set_float("rate", rate)
