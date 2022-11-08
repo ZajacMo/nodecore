ce245b8d5e17209f5134bd4196f54b7ca6726b82
Aaron Suen <warr1024@gmail.com>
2022-11-08 06:35:15 -0500

    Abstract firestick spark/ignite out as a public API

--- mods/nc_fire/firestarting.lua
+++ mods/nc_fire/firestarting.lua
@@ -1,6 +1,6 @@
 -- LUALOCALS < ---------------------------------------------------------
-local ItemStack, math, minetest, nodecore
-    = ItemStack, math, minetest, nodecore
+local ItemStack, math, minetest, nodecore, vector
+    = ItemStack, math, minetest, nodecore, vector
 local math_random
     = math.random
 -- LUALOCALS > ---------------------------------------------------------
@@ -25,6 +25,30 @@ local firedirs = {
 	{x = 0, y = -1, z = 0},
 }
 
+function nodecore.firestick_spark_ignite(pos, ignite)
+	minetest.add_particlespawner({
+			amount = 50,
+			time = 0.02,
+			minpos = {x = pos.x, y = pos.y - 0.25, z = pos.z},
+			maxpos = {x = pos.x, y = pos.y + 0.5, z = pos.z},
+			minvel = {x = -2, y = -3, z = -2},
+			maxvel = {x = 2, y = 1, z = 2},
+			minacc = {x = 0, y = -0.5, z = 0},
+			maxacc = {x = 0, y = -0.5, z = 0},
+			minxeptime = 0.4,
+			maxexptime = 0.5,
+			minsize = 0.4,
+			maxsize = 0.5,
+			texture = "nc_fire_spark.png",
+			collisiondetection = true,
+			glow = 7
+		})
+
+	if not ignite then return end
+	local dir = firedirs[math_random(1, #firedirs)]
+	return nodecore.fire_check_ignite(vector.add(pos, dir))
+end
+
 nodecore.register_craft({
 		label = "stick fire starting",
 		action = "pummel",
@@ -38,46 +62,17 @@ nodecore.register_craft({
 		consumewield = 1,
 		duration = 5,
 		before = function(pos, data)
-			local w = data.wield and ItemStack(data.wield):get_name() or ""
-			local wd = minetest.registered_items[w] or {}
-			local wg = wd.groups or {}
-			local fs = wg.firestick or 1
-			local nd = minetest.registered_items[data.node.name] or {}
-			local ng = nd.groups or {}
-			fs = fs * (ng.firestick or 1)
+			local fs = minetest.get_item_group(data.node.name, "firestick")
+			* minetest.get_item_group(ItemStack(data.wield):get_name(), "firestick")
 
-			local r = math_random(1, 4)
-			if r > fs then
+			if math_random(1, 4) > fs then
 				nodecore.smokeburst(pos)
 				nodecore.sound_play("nc_api_toolbreak", {pos = pos, gain = 1})
 				return
 			end
 
 			nodecore.fire_ignite(pos)
-			minetest.add_particlespawner({
-					amount = 50,
-					time = 0.02,
-					minpos = {x = pos.x, y = pos.y - 0.25, z = pos.z},
-					maxpos = {x = pos.x, y = pos.y + 0.5, z = pos.z},
-					minvel = {x = -2, y = -3, z = -2},
-					maxvel = {x = 2, y = 1, z = 2},
-					minacc = {x = 0, y = -0.5, z = 0},
-					maxacc = {x = 0, y = -0.5, z = 0},
-					minxeptime = 0.4,
-					maxexptime = 0.5,
-					minsize = 0.4,
-					maxsize = 0.5,
-					texture = "nc_fire_spark.png",
-					collisiondetection = true,
-					glow = 7
-				})
-
-			if math_random(1, 4) > fs then return end
-			local dir = firedirs[math_random(1, #firedirs)]
-			return nodecore.fire_check_ignite({
-					x = pos.x + dir.x,
-					y = pos.y + dir.y,
-					z = pos.z + dir.z
-				})
+			return nodecore.firestick_spark_ignite(pos,
+				math_random(1, 4) > fs)
 		end
 	})
