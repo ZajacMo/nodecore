a003784c22275cbcde491940b8f253a1c9f89e9c
Aaron Suen <warr1024@gmail.com>
2023-01-01 12:37:17 -0500

    Fix cloudstone mix recipe

--- mods/nc_concrete/register.lua
+++ mods/nc_concrete/register.lua
@@ -135,7 +135,7 @@ nodecore.register_concrete({
 		groups_powder = {crumbly = 1},
 		swim_color = {r = 210, g = 210, b = 210},
 		craft_from_keys = {"nc_optics:glass_crude"},
-		craft_from = {"nc_optics:glass_crude"},
+		craft_from = "nc_optics:glass_crude",
 		to_crude = "nc_terrain:dirt",
 		to_washed = "nc_terrain:dirt",
 		to_molded = localpref .. "cloudstone_blank_ply"
