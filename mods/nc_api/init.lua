33827c6f9bd22fac2b82728606e9548f184e6d1f
Aaron Suen <warr1024@gmail.com>
2023-11-26 10:13:15 -0500

    Rename workaround with official issue number

--- mods/nc_api/init.lua
+++ mods/nc_api/init.lua
@@ -80,7 +80,7 @@ include("compat_issue10127")
 include("compat_legacyent")
 include("compat_nodealpha")
 include("compat_authcache")
-include("compat_resourceid")
+include("compat_issue14044")
 
 include("util_settings")
 include("util_privs")
