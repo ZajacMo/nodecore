9c778cb26f383bb1f714787e731a57ed9ea5fea0
Aaron Suen <warr1024@gmail.com>
2023-11-30 22:23:13 -0500

    Remove debugging hack

--- mods/nc_api/setup_clientversion.lua
+++ mods/nc_api/setup_clientversion.lua
@@ -26,8 +26,7 @@ nodecore.translate_inform(announce)
 minetest.register_on_joinplayer(function(player)
 		local pname = player:get_player_name()
 		local pinfo = minetest.get_player_information(pname)
-		if (not pinfo) or (pinfo.protocol_version < minproto)
-		or pname == "Warr1025" then
+		if (not pinfo) or (pinfo.protocol_version < minproto) then
 			rejected[pname] = true
 			minetest.kick_player(pname, kickmsg)
 			return minetest.after(0, function()
