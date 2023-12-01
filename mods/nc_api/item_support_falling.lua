0af6670f28ad384b12b3162de70c142eab520795
Aaron Suen <warr1024@gmail.com>
2023-11-30 22:49:10 -0500

    Add on_node_fallen API
    If a node falls, this will be triggered AFTER the
    node has fallen, with the pos and the OLD
    node from that position.

--- mods/nc_api/item_support_falling.lua
+++ mods/nc_api/item_support_falling.lua
@@ -13,14 +13,17 @@ function minetest.check_single_for_falling(pos, ...)
 		if not def then return n end
 		return (not (def and def.groups and def.groups.support_falling)) and n or nil
 	end
+	local node = minetest.get_node(pos)
+	local def = minetest.registered_nodes[node.name] or {}
 	local function helper(fell, ...)
 		minetest.get_node_or_nil = gnon
-		if not fell then
-			local node = minetest.get_node(pos)
-			local def = minetest.registered_nodes[node.name] or {}
-			if def.on_falling_check then
-				return def.on_falling_check(pos, node)
+		if fell then
+			if def.on_node_fallen then
+				local ret = def.on_node_fallen(pos, node)
+				if ret ~= nil then return ret, ... end
 			end
+		elseif def.on_falling_check then
+			return def.on_falling_check(pos, node)
 		end
 		return fell, ...
 	end
