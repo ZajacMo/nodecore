1de8503ecd02f706d9b3c601e2ca85e9245af261
Aaron Suen <warr1024@gmail.com>
2024-08-24 13:21:58 -0400

    Remove rejected player announcements by default
    There's a setting to reenable them if people really want them,
    but they create a lot of chat noise and almost no value in most
    circumstances.

--- mods/nc_api/setup_clientversion.lua
+++ mods/nc_api/setup_clientversion.lua
@@ -20,8 +20,20 @@ local kickmsg = string_format("\n\n%s\n%s",
 	nodecore.translate("Your Minetest version is outdated, please update!"),
 	nodecore.translate("Version @1 or higher is required.", minrelease))
 
-local announce = "@1 rejected. (protocol version @2)"
-nodecore.translate_inform(announce)
+local announcetext = "@1 rejected. (protocol version @2)"
+nodecore.translate_inform(announcetext)
+
+local announce = nodecore.setting_bool(
+	minetest.get_current_modname() .. "_client_version_announce",
+	false,
+	"Announce players kicked due to client version",
+	[[Players with outdated client versions cannot be rejected by game/mod
+	code before allowing them to join, so we have to allow them to join, and
+	then kick them. We suppress the normal join/leave messages to cut down
+	on noise. Enabling this option produces a single short announcement
+	about player rejections, as these "partial" joins may have privacy
+	implications.]]
+)
 
 minetest.register_on_joinplayer(function(player)
 		local pname = player:get_player_name()
@@ -29,9 +41,9 @@ minetest.register_on_joinplayer(function(player)
 		if (not pinfo) or (pinfo.protocol_version < minproto) then
 			rejected[pname] = true
 			minetest.kick_player(pname, kickmsg)
-			return minetest.after(0, function()
+			return announce and minetest.after(0, function()
 					return minetest.chat_send_all("*** "
-						.. nodecore.translate(announce, pname,
+						.. nodecore.translate(announcetext, pname,
 							pinfo and pinfo.protocol_version or "unknown"))
 				end)
 		else
