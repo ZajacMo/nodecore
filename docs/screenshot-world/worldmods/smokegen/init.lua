7849c87026a6f921a057223cd54cb838f4dd25ca
Aaron Suen <warr1024@gmail.com>
2024-08-25 11:38:27 -0400

    Fully stabilize screenshot smoke generators
    Instead of using limited-lifetime particles that need to be
    refreshed, use sprite entities, which never need to be replaced
    as long as they stay loaded.
    
    There was a short "glitch" when replacing the particles where
    they could appear twice, or be absent, for a frame or two.  If
    we're taking the screenshot at insanely high resolution/AA, which
    the automation in this mod enables, we may have fractional FPS,
    and the time window when the particles are in an invalid state
    may expand to the point where it's impossible to capture a
    screenshot with the correct smoke particles.
    
    Making the smoke particles stable this way makes it possible to
    reliably capture a screenshot with correct smoke particles at
    insanely high quality / low framerate.

--- docs/screenshot-world/worldmods/smokegen/init.lua
+++ docs/screenshot-world/worldmods/smokegen/init.lua
@@ -1,11 +1,26 @@
 -- LUALOCALS < ---------------------------------------------------------
-local PcgRandom, minetest, nodecore
-    = PcgRandom, minetest, nodecore
+local PcgRandom, math, minetest, nodecore, pairs
+    = PcgRandom, math, minetest, nodecore, pairs
+local math_floor
+    = math.floor
 -- LUALOCALS > ---------------------------------------------------------
 
 local modname = minetest.get_current_modname()
 
+local entname = modname .. ":ent"
 local genname = modname .. ":gen"
+
+minetest.register_entity(entname, {
+		description = "Smoke",
+		initial_properties = {
+			visual = "sprite",
+			visual_size = {x = 1/4, y = 1/4, z = 1/4},
+			textures = {"nc_api_craft_smoke.png"},
+			physical = false,
+			pointable = false,
+			static_save = false,
+		}
+	})
 
 minetest.register_node(genname, {
 		description = "Smoke Generator",
@@ -18,10 +33,16 @@ minetest.register_node(genname, {
 		groups = {snappy = 1}
 	})
 
+local smokepuffs = {}
+
+local function myround(n)
+	return math_floor(n * 256) / 256
+end
+
 nodecore.register_dnt({
 		name = genname,
 		nodenames = {genname},
-		time = 2,
+		time = 1,
 		loop = true,
 		ignore_stasis = true,
 		autostart = true,
@@ -31,16 +52,35 @@ nodecore.register_dnt({
 			local rng = function() return pcg:next() / 2 ^ 32 + 0.5 end
 			for _ = 1, 10 do
 				local p = {
-					x = pos.x + rng() - 0.5,
-					y = pos.y + rng() * 2 - 0.5,
-					z = pos.z + rng() - 0.5,
+					x = myround(pos.x + rng() - 0.5),
+					y = myround(pos.y + rng() * 2 - 0.5),
+					z = myround(pos.z + rng() - 0.5),
+					src = pos
 				}
-				minetest.add_particle({
-						pos = p,
-						texture = "nc_api_craft_smoke.png",
-						size = rng() * 2 + 1,
-						expirationtime = 2.1
-					})
+				smokepuffs[minetest.pos_to_string(p)] = p
 			end
 		end
 	})
+
+nodecore.interval(1, function()
+		local found = {}
+		for _, ent in pairs(minetest.luaentities) do
+			if ent.name == entname then
+				local key = ent.key
+				local puff = key and smokepuffs[key]
+				if not (puff and puff.src
+					and minetest.get_node(puff.src).name == genname) then
+					ent.object:remove()
+					smokepuffs[key] = nil
+				end
+				found[key] = ent
+			end
+		end
+		for key, pos in pairs(smokepuffs) do
+			if not found[key] then
+				local obj = minetest.add_entity(pos, entname)
+				local ent = obj and obj:get_luaentity()
+				if ent then ent.key = key end
+			end
+		end
+	end)
