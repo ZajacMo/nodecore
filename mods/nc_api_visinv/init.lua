b77fb35338574ba99e0baa209630f48df76356ab
Aaron Suen <warr1024@gmail.com>
2024-09-23 20:11:46 -0400

    Stack caching in visinv checks
    The ItemStack constructor is surprisingly expensive, and calling
    it thousands of times per globalstep for visinv checks is pricey.

--- mods/nc_api_visinv/init.lua
+++ mods/nc_api_visinv/init.lua
@@ -1,6 +1,6 @@
 -- LUALOCALS < ---------------------------------------------------------
-local math, minetest, nodecore, pairs, vector
-    = math, minetest, nodecore, pairs, vector
+local ItemStack, math, minetest, nodecore, pairs, vector
+    = ItemStack, math, minetest, nodecore, pairs, vector
 local math_random
     = math.random
 -- LUALOCALS > ---------------------------------------------------------
@@ -48,16 +48,32 @@ function nodecore.visinv_tween_from(nodepos, frompos)
 	tweenfrom[hash(vector.round(nodepos))] = frompos
 end
 
+local stackcache = {}
+nodecore.register_on_nodeupdate({
+		ignore = {
+			add_node_level = true,
+			liquid_transformed = true,
+		},
+		getnode = false,
+		func = function(pos)
+			stackcache[hash(pos)] = nil
+		end
+	})
+
 local function itemcheck(self)
 	local obj = self.object
 	if not (obj and obj:get_pos()) then return end
 
-	local rp = self.pos
 	local tf = tweenfrom[self.poskey]
-	local stack = nodecore.stack_get(rp)
-	local sstr = stack:to_string()
+	local rp = self.pos
+	local sstr = stackcache[self.poskey]
+	if not sstr then
+		sstr = nodecore.stack_get(rp):to_string()
+		stackcache[self.poskey] = sstr
+	end
 	if (not tf) and self.stackstring == sstr then return end
 	self.stackstring = sstr
+	local stack = ItemStack(sstr)
 
 	if stack:is_empty() then return self.object:remove() end
 
