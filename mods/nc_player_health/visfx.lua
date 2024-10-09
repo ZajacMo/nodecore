103d557b8d4c5a520ef869ee88ad965295aa5b5e
Aaron Suen <warr1024@gmail.com>
2024-10-09 17:24:52 -0400

    Make pain blur effect non-linear

--- mods/nc_player_health/visfx.lua
+++ mods/nc_player_health/visfx.lua
@@ -7,16 +7,22 @@ local math_abs
 
 local granularity = 0.02
 
+local function clamp(x)
+	if x > 1 then return 1 end
+	if x < 0 then return 0 end
+	return x
+end
+
 nodecore.register_playerstep({
 		label = "health visual effects",
 		action = function(player, data, dtime)
 			local old = data.phealth_fx_old or 0
-			local new = 1 - nodecore.getphealth(player) / 8
+			local new = clamp(1 - nodecore.getphealth(player) / 8)
+			new = clamp(new ^ 0.5)
 			local blend = 0.5 ^ dtime
 			local cur = old * blend + new * (1 - blend)
 			if cur - new < granularity then cur = new end
-			if cur > 1 then cur = 1 end
-			if cur < 0 then cur = 0 end
+			cur = clamp(cur)
 			data.phealth_fx_old = cur
 
 			local setto = data.phealth_fx_setto or 0
