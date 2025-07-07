04ea3470912a5ff455f25b355b677fd07f755011
Aaron Suen <warr1024@gmail.com>
2025-07-07 09:16:18 -0400

    Code refactors and denesting
    Convert code more toward "early return" style instead
    of "deep nesting".

--- mods/nc_player_setup/step_jumphigh.lua
+++ mods/nc_player_setup/step_jumphigh.lua
@@ -14,39 +14,40 @@ nc.register_playerstep({
 				hijump = {cooldown = 0}
 				data.hijump = hijump
 			end
-			if hijump.cooldown <= nodecore.gametime then
-				local ctl = data.control
-				local vel = player:get_velocity()
-				if vel.y > 0 then
-					if ctl.sneak then
-						if ctl.jump then
-							hijump.ready = true
-						end
-					elseif hijump.ready then
-						if (not ctl.jump) then
-							hijump.cooldown = nodecore.gametime + hijump_cooldown
-							player:add_velocity(hijump_amount)
-							minetest.add_particlespawner({
-									amount = 25,
-									time = 0.05,
-									size = 1/4,
-									minexptime = 0.125,
-									maxexptime = 0.25,
-									texture = {
-										name = "nc_player_setup_particle.png",
-										blend = "add",
-									},
-									playername = player:get_player_name(),
-									attached = player,
-									glow = 14,
-									minpos = vector.new(-0.5, 0, -0.5),
-									maxpos = vector.new(0.5, 2, 0.5),
-									velocity = vector.multiply(hijump_amount, -3)
-								})
-						end
-						hijump.ready = nil
-					end
-				end
+
+			if hijump.cooldown > nodecore.gametime then return end
+
+			local vel = player:get_velocity()
+			if vel.y <= 0 then return end
+
+			local ctl = data.control
+			if ctl.sneak then
+				if ctl.jump then hijump.ready = true end
+				return
 			end
+
+			if not hijump.ready then return end
+			hijump.ready = nil
+			if ctl.jump then return end
+
+			hijump.cooldown = nodecore.gametime + hijump_cooldown
+			player:add_velocity(hijump_amount)
+			minetest.add_particlespawner({
+					amount = 25,
+					time = 0.05,
+					size = 1/4,
+					minexptime = 0.125,
+					maxexptime = 0.25,
+					texture = {
+						name = "nc_player_setup_particle.png",
+						blend = "add",
+					},
+					playername = player:get_player_name(),
+					attached = player,
+					glow = 14,
+					minpos = vector.new(-0.5, 0, -0.5),
+					maxpos = vector.new(0.5, 2, 0.5),
+					velocity = vector.multiply(hijump_amount, -3)
+				})
 		end
 	})
--- mods/nc_player_setup/step_jumplong.lua
+++ mods/nc_player_setup/step_jumplong.lua
@@ -18,50 +18,56 @@ nc.register_playerstep({
 				}
 				data.longjump = longjump
 			end
-			if longjump.cooldown <= nodecore.gametime then
-				local ctl = data.control
-				if not ctl.up then
-					longjump.minspeedtime = 0
-					longjump.maxspeed = 0
-					longjump.ready = nil
-					return
-				end
-				local speed = data.physics.speed
-				if speed >= longjump_minspeed then
-					if speed > longjump.maxspeed then longjump.maxspeed = speed end
-					longjump.minspeedtime = nodecore.gametime + longjump_cooldown
-				end
-				if nodecore.gametime >= longjump.minspeedtime then return end
-				if ctl.sneak and not ctl.jump then
-					longjump.ready = true
-				elseif longjump.ready and ctl.jump
-					and player:get_velocity().y > 0 then
-						longjump.ready = nil
-						longjump.cooldown = nodecore.gametime + longjump_cooldown
-						local dir = player:get_look_dir()
-						dir.y = 0
-						dir = vector.normalize(dir)
-						player:add_velocity(vector.multiply(dir, 8 * longjump.maxspeed))
-						minetest.add_particlespawner({
-								amount = 50,
-								time = 0.05,
-								size = 1/4,
-								minexptime = 0.125,
-								maxexptime = 0.25,
-								texture = {
-									name = "nc_player_setup_particle.png",
-									blend = "add",
-								},
-								playername = player:get_player_name(),
-								attached = player,
-								glow = 14,
-								minpos = vector.new(-1, 1, -1),
-								maxpos = vector.new(1, 3, 8 * longjump.maxspeed),
-								velocity = vector.new(0, 0, -10 * longjump.maxspeed)
-							})
-						-- force reset run speed
-						data.autoruntime = nodecore.gametime
-					end
-				end
+
+			if longjump.cooldown > nodecore.gametime then return end
+
+			local ctl = data.control
+			if not ctl.up then
+				longjump.minspeedtime = 0
+				longjump.maxspeed = 0
+				longjump.ready = nil
+				return
 			end
-		})
+
+			local speed = data.physics.speed
+			if speed >= longjump_minspeed then
+				if speed > longjump.maxspeed then longjump.maxspeed = speed end
+				longjump.minspeedtime = nodecore.gametime + longjump_cooldown
+			end
+			if nodecore.gametime >= longjump.minspeedtime then return end
+
+			if ctl.sneak and not ctl.jump then
+				longjump.ready = true
+				return
+			end
+
+			if not (longjump.ready and ctl.jump and player:get_velocity().y > 0)
+			then return end
+
+			longjump.ready = nil
+			longjump.cooldown = nodecore.gametime + longjump_cooldown
+			local dir = player:get_look_dir()
+			dir.y = 0
+			dir = vector.normalize(dir)
+			player:add_velocity(vector.multiply(dir, 8 * longjump.maxspeed))
+			minetest.add_particlespawner({
+					amount = 50,
+					time = 0.05,
+					size = 1/4,
+					minexptime = 0.125,
+					maxexptime = 0.25,
+					texture = {
+						name = "nc_player_setup_particle.png",
+						blend = "add",
+					},
+					playername = player:get_player_name(),
+					attached = player,
+					glow = 14,
+					minpos = vector.new(-1, 1, -1),
+					maxpos = vector.new(1, 3, 8 * longjump.maxspeed),
+					velocity = vector.new(0, 0, -10 * longjump.maxspeed)
+				})
+			-- force reset run speed
+			data.autoruntime = nodecore.gametime
+		end
+	})
