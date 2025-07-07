43cc33727f0e41d45d60562c9795bf005f09cc92
Aaron Suen <warr1024@gmail.com>
2025-07-07 09:11:22 -0400

    More longjump bugfixes
    - Fix trigger condition that was making it happen with normal jumps.
    - Use run speed multiplier instead of measured velocity, to account
      for some network jank and make it less exploitable.

--- mods/nc_player_setup/step_jumplong.lua
+++ mods/nc_player_setup/step_jumplong.lua
@@ -3,8 +3,8 @@ local nc, vector
     = nc, vector
 -- LUALOCALS > ---------------------------------------------------------
 
-local longjump_cooldown = 3
-local longjump_minvel = 6
+local longjump_cooldown = 4
+local longjump_minspeed = 1.3
 
 nc.register_playerstep({
 		label = "longjump",
@@ -13,55 +13,55 @@ nc.register_playerstep({
 			if not longjump then
 				longjump = {
 					cooldown = 0,
-					minveltime = 0,
-					maxvel = 0
+					minspeedtime = 0,
+					maxspeed = 0
 				}
 				data.longjump = longjump
 			end
 			if longjump.cooldown <= nodecore.gametime then
 				local ctl = data.control
 				if not ctl.up then
-					longjump.minveltime = 0
-					longjump.maxvel = 0
-					longjump.ready = false
+					longjump.minspeedtime = 0
+					longjump.maxspeed = 0
+					longjump.ready = nil
 					return
 				end
-				local vel = player:get_velocity()
-				local xzvel = vector.length({x = vel.x, y = 0, z = vel.z})
-				if xzvel >= longjump_minvel then
-					if xzvel > longjump.maxvel then longjump.maxvel = xzvel end
-					longjump.minveltime = nodecore.gametime + longjump_cooldown
+				local speed = data.physics.speed
+				if speed >= longjump_minspeed then
+					if speed > longjump.maxspeed then longjump.maxspeed = speed end
+					longjump.minspeedtime = nodecore.gametime + longjump_cooldown
 				end
-				if nodecore.gametime >= longjump.minveltime then return end
+				if nodecore.gametime >= longjump.minspeedtime then return end
 				if ctl.sneak and not ctl.jump then
 					longjump.ready = true
-				elseif ctl.jump and vel.y > 0 then
-					longjump.ready = false
-					longjump.cooldown = nodecore.gametime + longjump_cooldown
-					local dir = player:get_look_dir()
-					dir.y = 0
-					dir = vector.normalize(dir)
-					player:add_velocity(vector.multiply(dir, 2 * longjump.maxvel))
-					minetest.add_particlespawner({
-							amount = 50,
-							time = 0.05,
-							size = 1/4,
-							minexptime = 0.125,
-							maxexptime = 0.25,
-							texture = {
-								name = "nc_player_setup_particle.png",
-								blend = "add",
-							},
-							playername = player:get_player_name(),
-							attached = player,
-							glow = 14,
-							minpos = vector.new(-1, 1, -1),
-							maxpos = vector.new(1, 3, 2 * longjump.maxvel),
-							velocity = vector.new(0, 0, -4 * longjump.maxvel)
-						})
-					-- force reset run speed
-					data.autoruntime = nodecore.gametime
+				elseif longjump.ready and ctl.jump
+					and player:get_velocity().y > 0 then
+						longjump.ready = nil
+						longjump.cooldown = nodecore.gametime + longjump_cooldown
+						local dir = player:get_look_dir()
+						dir.y = 0
+						dir = vector.normalize(dir)
+						player:add_velocity(vector.multiply(dir, 8 * longjump.maxspeed))
+						minetest.add_particlespawner({
+								amount = 50,
+								time = 0.05,
+								size = 1/4,
+								minexptime = 0.125,
+								maxexptime = 0.25,
+								texture = {
+									name = "nc_player_setup_particle.png",
+									blend = "add",
+								},
+								playername = player:get_player_name(),
+								attached = player,
+								glow = 14,
+								minpos = vector.new(-1, 1, -1),
+								maxpos = vector.new(1, 3, 8 * longjump.maxspeed),
+								velocity = vector.new(0, 0, -10 * longjump.maxspeed)
+							})
+						-- force reset run speed
+						data.autoruntime = nodecore.gametime
+					end
 				end
 			end
-		end
-	})
+		})
