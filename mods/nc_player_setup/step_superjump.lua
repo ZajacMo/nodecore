93f40418995210aeb21305ec96a237c21c2741df
Aaron Suen <warr1024@gmail.com>
2025-07-06 20:54:38 -0400

    Experimental player high-jump
    Hold sneak, tap jump, release sneak while still
    traveling upward, and you should get a small extra
    burst of upward speed, with accompanying particle
    effect.  The intent is, similar to auto-run,
    scaling, and other movement mechanics, to give the
    player a bit more freedom of movement without
    making movement totally free, so safety and
    planning of paths is still a relevant mechanic.

---
+++ mods/nc_player_setup/step_superjump.lua
@@ -0,0 +1,52 @@
+-- LUALOCALS < ---------------------------------------------------------
+local nc, vector
+    = nc, vector
+-- LUALOCALS > ---------------------------------------------------------
+
+local hijump_cooldown = 1
+local hijump_amount = vector.new(0, 5, 0)
+
+nc.register_playerstep({
+		label = "jumping",
+		action = function(player, data)
+			local hijump = data.hijump
+			if not hijump then
+				hijump = {cooldown = 0}
+				data.hijump = hijump
+			end
+			if hijump.cooldown <= nodecore.gametime then
+				local ctl = data.control
+				local vel = player:get_velocity()
+				if vel.y > 0 then
+					if ctl.sneak then
+						if ctl.jump then
+							hijump.ready = true
+						end
+					elseif hijump.ready then
+						if (not ctl.jump) then
+							hijump.cooldown = nodecore.gametime + hijump_cooldown
+							player:add_velocity(hijump_amount)
+							minetest.add_particlespawner({
+									amount = 25,
+									time = 0.05,
+									size = 1/4,
+									minexptime = 0.125,
+									maxexptime = 0.25,
+									texture = {
+										name = "nc_player_setup_particle.png",
+										blend = "add",
+									},
+									playername = player:get_player_name(),
+									attached = player,
+									glow = 14,
+									minpos = vector.new(-0.5, 0, -0.5),
+									maxpos = vector.new(0.5, 2, 0.5),
+									velocity = vector.multiply(hijump_amount, -3)
+								})
+						end
+						hijump.ready = nil
+					end
+				end
+			end
+		end
+	})
